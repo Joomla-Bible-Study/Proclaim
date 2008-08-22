92c15ac282900138d1b4e6bfd3341551973c1135
tomfuller2 <tomfuller2@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2008-08-22 13:55:56 +0000

    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@63 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- components/site/views/studydetails/view.pdf.php
+++ components/site/views/studydetails/view.pdf.php
@@ -141,6 +141,11 @@ $scripture2 = format_scripture($booknumber, $ch_b, $ch_e, $v_b, $v_e);
   $options = "include-passage-references=false";
   $url = "http://www.esvapi.org/v2/rest/passageQuery?key=$key&passage=$passage&$options";
 
+  $p = (get_extension_funcs("curl")); // This tests to see if the curl functions are there. It will return false if curl not installed
+  if ($p) { // If curl is installed then we go on
+  $ch = curl_init($url); // This will return false if curl is not enabled
+  if ($ch) { //This will return false if curl is not enabled
+
   $ch = curl_init($url); 
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
   $response = curl_exec($ch);
@@ -148,7 +153,8 @@ $scripture2 = format_scripture($booknumber, $ch_b, $ch_e, $v_b, $v_e);
   echo '<br /><hr /><br />';
   echo "".$scripture1." (ESV)";
   print $response;
-
+  } // End of if ($ch)
+  } // End if ($p)
 		
 	 } 
 	} // end of if show_passage_view 
