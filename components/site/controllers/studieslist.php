0a755dbb3a8f0b756ea5eeb03ab4d30ab1039290
genu <genu@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2008-11-25 16:58:22 +0000

    Added function to generate the Object for the inline flash player
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@108 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- components/site/controllers/studieslist.php
+++ components/site/controllers/studieslist.php
@@ -16,27 +16,43 @@ class biblestudyControllerstudieslist extends JController
 		parent::display();
 	}
 
-function download() {
-	$abspath    = JPATH_SITE;
-	require_once($abspath.DS.'components/com_biblestudy/class.biblestudydownload.php');
-	$task = JRequest::getVar('task');
-	if ($task == 'download')
+	function download() {
+		$abspath    = JPATH_SITE;
+		require_once($abspath.DS.'components/com_biblestudy/class.biblestudydownload.php');
+		$task = JRequest::getVar('task');
+		if ($task == 'download')
 		{
-		$downloader = new Dump_File();
-		$downloader->download();
+			$downloader = new Dump_File();
+			$downloader->download();
 		 die;
-  		}
+		}
 	}
 
-	function avplayer() 
+	function avplayer()
 	{
 		$task = JRequest::getVar('task');
 		if ($task == 'avplayer')
-			{
+		{
 			$mediacode = JRequest::getVar('code');
 			echo $mediacode;
 			return;
-			}
+		}
 	}
+
+	function inlinePlayer() {
+		$mediaPath = JRequest::getVar('url');
+		$player =
+					'<script language="JavaScript" src="'.JURI::base().'components/com_biblestudy/audio-player.js"></script>
+<object type="application/x-shockwave-flash" data="'.JURI::base().'components/com_biblestudy/player.swf" id="audioplayer0" height="24" width="290">
+<param name="movie" value="'.JURI::base().'components/com_biblestudy/player.swf">
+<param name="FlashVars" value="playerID=0&amp;soundFile='.$mediaPath.'">
+<param name="quality" value="high">
+<param name="menu" value="false">
+<param name="wmode" value="transparent">
+</object> ';
+		echo $player;
+	}
+		
 }
+
 ?>
