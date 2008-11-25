3ffed9249ff2749cc14a30bc0d0e65a7aca835e9
genu <genu@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2008-11-25 17:01:42 +0000

    Tweaked the way that the inline appears....
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@111 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- administrator/admin/js/biblestudy.js
+++ administrator/admin/js/biblestudy.js
@@ -4,7 +4,11 @@ $j(document).ready( function() {
 	$j('.btnPlay').click( function() {
 		var mediaId = $j(this).attr('alt');
 		var url = $j(this).attr('href');
+		
+		$j('.inlinePlayer:not(#media-'+mediaId+')').hide();
+		$j('.inlinePlayer').html('');
 		$j('#media-' + mediaId).toggle();
+		$j('#media-' + mediaId).load('index.php?option=com_biblestudy&url='+url+'&view=studieslist&controller=studieslist&task=inlinePlayer&tmpl=component');
 		return false;
 	});
 });
