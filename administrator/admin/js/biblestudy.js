61cfbb7b1e88974eb635739089e8cd62470141c7
genu <genu@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2008-12-10 05:02:56 +0000

    To be used, perhaps later
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@115 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- administrator/admin/js/biblestudy.js
+++ administrator/admin/js/biblestudy.js
@@ -8,7 +8,7 @@ $j(document).ready( function() {
 		$j('.inlinePlayer:not(#media-'+mediaId+')').hide();
 		$j('.inlinePlayer').html('');
 		$j('#media-' + mediaId).toggle();
-		$j('#media-' + mediaId).load('index.php?option=com_biblestudy&url='+url+'&view=studieslist&controller=studieslist&task=inlinePlayer&tmpl=component');
+		$j('#media-' + mediaId).load('index.php?option=com_biblestudy&view=studieslist&controller=studieslist&task=inlinePlayer&tmpl=component');
 		return false;
 	});
 });
