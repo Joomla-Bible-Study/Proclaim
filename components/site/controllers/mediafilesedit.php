16387f79d02f861423b261e3b313905c8339c11d
genu <genu@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2008-12-13 05:32:43 +0000

    Checks to see if there is a "filename", if NOT than it assigns it the value of the uploaded file's name. NOTE: Did not test intensively.
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@121 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- components/site/controllers/mediafilesedit.php
+++ components/site/controllers/mediafilesedit.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Media Edit Controller for Bible Study Component
- * 
- 
+ *
+
  */
 
 // Check to ensure this file is included in Joomla!
@@ -20,19 +20,19 @@ class biblestudyControllermediafilesedit extends JController
 	 */
 	function __construct()
 	{
-		
-			$user =& JFactory::getUser();
-			global $mainframe, $option;
-			$params =& $mainframe->getPageParameters();
-			$entry_user = $user->get('gid');
-			$entry_access = ($params->get('entry_access')) ;
-			$allow_entry = $params->get('allow_entry_study');
-			if (!$allow_entry) {$allow_entry = 0;}
-			if ($allow_entry < 1) {return JError::raiseError('403', JText::_('Access Forbidden')); }
-			if (!$entry_user) { $entry_user = 0; }
-			if ($allow_entry > 0) {
-				if ($entry_user < $entry_access){return JError::raiseError('403', JText::_('Access Forbidden')); }
-			}
+
+		$user =& JFactory::getUser();
+		global $mainframe, $option;
+		$params =& $mainframe->getPageParameters();
+		$entry_user = $user->get('gid');
+		$entry_access = ($params->get('entry_access')) ;
+		$allow_entry = $params->get('allow_entry_study');
+		if (!$allow_entry) {$allow_entry = 0;}
+		if ($allow_entry < 1) {return JError::raiseError('403', JText::_('Access Forbidden')); }
+		if (!$entry_user) { $entry_user = 0; }
+		if ($allow_entry > 0) {
+			if ($entry_user < $entry_access){return JError::raiseError('403', JText::_('Access Forbidden')); }
+		}
 		parent::__construct();
 
 		// Register Extra tasks
@@ -46,8 +46,8 @@ class biblestudyControllermediafilesedit extends JController
 	 */
 	function edit()
 	{
-		
-		
+
+
 		JRequest::setVar( 'view', 'mediafilesedit' );
 		JRequest::setVar( 'layout', 'form'  );
 		JRequest::setVar('hidemainmenu', 1);
@@ -62,42 +62,44 @@ class biblestudyControllermediafilesedit extends JController
 	 */
 	function save()
 	{
-		
+
 		$model = $this->getModel('mediafilesedit');
 		$file = JRequest::getVar('file', null, 'files', 'array' );
 		$filename_upload = $file['name'];
+		$data = JRequest::get( 'post' );
+		if(empty($data['filename'])) $data['filename'] = $file['name'];
 		if (isset($filename_upload)){
-		$uploadFile=$this->upload();}
-		if ($model->store($post)) {
-			$msg = JText::_( 'Media Saved!' );
-		} else {
-			$msg = JText::_( 'Error Saving Media' );
-		}
-		
-		global $mainframe, $option;
-		/*$db=& JFactory::getDBO(); 
-		$query = "SELECT id"
+			$uploadFile=$this->upload();}
+			if ($model->store($data)) {
+				$msg = JText::_( 'Media Saved!' );
+			} else {
+				$msg = JText::_( 'Error Saving Media' );
+			}
+
+			global $mainframe, $option;
+			/*$db=& JFactory::getDBO();
+			 $query = "SELECT id"
+			 . "\nFROM #__menu"
+			 . "\nWHERE link ='index.php?option=com_biblestudy&view=studieslist' and published = 1";
+			 $db->setQuery($query);
+			 $menuid = $db->loadResult();
+			 $menureturn='';
+			 if ($menuid) {$menureturn = '&Itemid='.$menuid;}
+			 //$link = 'index.php?option=com_biblestudy&view=studieslist&Itemid='.$params->get('alt_link').'&msg='.$msg;
+			 $link = JRoute::_('index.php?option='.$option.'&view=studieslist&msg='.$msg.$menureturn);*/
+			$database	= & JFactory::getDBO();
+			$query = "SELECT id"
 			. "\nFROM #__menu"
 			. "\nWHERE link ='index.php?option=com_biblestudy&view=studieslist' and published = 1";
-		$db->setQuery($query);
-		$menuid = $db->loadResult();
-		$menureturn='';
-		if ($menuid) {$menureturn = '&Itemid='.$menuid;}
-    	//$link = 'index.php?option=com_biblestudy&view=studieslist&Itemid='.$params->get('alt_link').'&msg='.$msg;
-		$link = JRoute::_('index.php?option='.$option.'&view=studieslist&msg='.$msg.$menureturn);*/
-		$database	= & JFactory::getDBO();
-		$query = "SELECT id"
-			. "\nFROM #__menu"
-			. "\nWHERE link ='index.php?option=com_biblestudy&view=studieslist' and published = 1";
-		$database->setQuery($query);
-		$menuid = $database->loadResult();
-	if ($menuid){
-    $link = JRoute::_('index.php?option=com_biblestudy&view=studieslist&Itemid='.$menuid.'&msg='.$msg);}
-		// Check the table in so it can be edited.... we are done with it anyway
-		$mainframe->redirect ($link);
-		// Check the table in so it can be edited.... we are done with it anyway
-		//$link = 'index.php?option=com_biblestudy&view=studieslist';
-		//$this->setRedirect($link, $msg);
+			$database->setQuery($query);
+			$menuid = $database->loadResult();
+			if ($menuid){
+				$link = JRoute::_('index.php?option=com_biblestudy&view=studieslist&Itemid='.$menuid.'&msg='.$msg);}
+				// Check the table in so it can be edited.... we are done with it anyway
+				$mainframe->redirect ($link);
+				// Check the table in so it can be edited.... we are done with it anyway
+				//$link = 'index.php?option=com_biblestudy&view=studieslist';
+				//$this->setRedirect($link, $msg);
 	}
 
 	/**
@@ -112,10 +114,10 @@ class biblestudyControllermediafilesedit extends JController
 		} else {
 			$msg = JText::_( 'Media Item(s) Deleted' );
 		}
-		$db=& JFactory::getDBO(); 
+		$db=& JFactory::getDBO();
 		$query = "SELECT id"
-			. "\nFROM #__menu"
-			. "\nWHERE link ='index.php?option=com_biblestudy&view=studieslist' and published = 1";
+		. "\nFROM #__menu"
+		. "\nWHERE link ='index.php?option=com_biblestudy&view=studieslist' and published = 1";
 		$db->setQuery($query);
 		$menuid = $db->loadResult();
 		$menureturn='';
@@ -124,12 +126,12 @@ class biblestudyControllermediafilesedit extends JController
 		$params =& $mainframe->getPageParameters();
 		$link = 'index.php?option=com_biblestudy&view=studieslist&msg='.$msg.$menureturn;
 		$link = 'index.php?option=com_biblestudy&view=studieslist&Itemid='.$menureturn.'&msg='.$msg;
-		
+
 		// Check the table in so it can be edited.... we are done with it anyway
 		$mainframe->redirect ($link);
 		//$this->setRedirect( 'index.php?option=com_biblestudy&view=studieslist', $msg );
 	}
-function publish()
+	function publish()
 	{
 		global $mainframe;
 
@@ -144,10 +146,10 @@ function publish()
 			echo "<script> alert('".$model->getError(true)."'); window.history.go(-1); </script>\n";
 		}
 		global $mainframe, $option;
-		$db=& JFactory::getDBO(); 
+		$db=& JFactory::getDBO();
 		$query = "SELECT id"
-			. "\nFROM #__menu"
-			. "\nWHERE link ='index.php?option=com_biblestudy&view=studieslist' and published = 1";
+		. "\nFROM #__menu"
+		. "\nWHERE link ='index.php?option=com_biblestudy&view=studieslist' and published = 1";
 		$db->setQuery($query);
 		$menuid = $db->loadResult();
 		$menureturn='';
@@ -155,7 +157,7 @@ function publish()
 		//$params =& $mainframe->getPageParameters();
 		$link = JRoute::_('index.php?option=com_biblestudy&view=studieslist&msg='.$msg.$menureturn);
 		//$link = 'index.php?option=com_biblestudy&view=studieslist&Itemid='.$menureturn.'&msg='.$msg;
-		
+
 		// Check the table in so it can be edited.... we are done with it anyway
 		$mainframe->redirect ($link);
 		//$this->setRedirect( 'index.php?option=com_biblestudy&view=studieslist' );
@@ -165,10 +167,10 @@ function publish()
 	function unpublish()
 	{
 		global $mainframe;
-		$db=& JFactory::getDBO(); 
+		$db=& JFactory::getDBO();
 		$query = "SELECT id"
-			. "\nFROM #__menu"
-			. "\nWHERE link ='index.php?option=com_biblestudy&view=studieslist' and published = 1";
+		. "\nFROM #__menu"
+		. "\nWHERE link ='index.php?option=com_biblestudy&view=studieslist' and published = 1";
 		$db->setQuery($query);
 		$menuid = $db->loadResult();
 		$menureturn='';
@@ -187,7 +189,7 @@ function publish()
 		$params =& $mainframe->getPageParameters();
 		$link = JRoute::_('index.php?option=com_biblestudy&view=studieslist&msg='.$msg.$menureturn);
 		//$link = 'index.php?option=com_biblestudy&view=studieslist'.$menureturn.'&msg='.$msg;
-		
+
 		// Check the table in so it can be edited.... we are done with it anyway
 		$mainframe->redirect ($link);
 		//$this->setRedirect( 'index.php?option=com_biblestudy&view=mediafileslist' );
@@ -199,10 +201,10 @@ function publish()
 	 */
 	function cancel()
 	{
-		$db=& JFactory::getDBO(); 
+		$db=& JFactory::getDBO();
 		$query = "SELECT id"
-			. "\nFROM #__menu"
-			. "\nWHERE link ='index.php?option=com_biblestudy&view=studieslist' and published = 1";
+		. "\nFROM #__menu"
+		. "\nWHERE link ='index.php?option=com_biblestudy&view=studieslist' and published = 1";
 		$db->setQuery($query);
 		$menuid = $db->loadResult();
 		$menureturn='';
@@ -212,65 +214,63 @@ function publish()
 		$params =& $mainframe->getPageParameters();
 		$link = JRoute::_('index.php?option=com_biblestudy&view=studieslist&msg='.$msg.$menureturn);
 		//$link = 'index.php?option=com_biblestudy&view=studieslist'.$menureturn.'&msg='.$msg;
-	
+
 		// Check the table in so it can be edited.... we are done with it anyway
 		$mainframe->redirect ($link);
 		//$this->setRedirect( 'index.php?option=com_biblestudy&view=mediafileslist', $msg );
 	}
-	
-function upload()
+
+	function upload()
 	{
 		global $mainframe, $option;
 
-	$db=& JFactory::getDBO();   
-	$file = JRequest::getVar('file', null, 'files', 'array' );
-	$filename = '';
-	$path = JRequest::getVar('path', null, 'POST', 'INT');
-	$query = 'SELECT id, folderpath FROM #__bsms_folders WHERE id = '.$path.' LIMIT 1';
-	$db->setQuery($query);
-	$folder = $db->loadObject();
-	$folderpath = $folder->folderpath;
-	
-	$query = "SELECT id"
-			. "\nFROM #__menu"
-			. "\nWHERE link ='index.php?option=com_biblestudy&view=studieslist' and published = 1";
+		$db=& JFactory::getDBO();
+		$file = JRequest::getVar('file', null, 'files', 'array' );
+		$filename = '';
+		$path = JRequest::getVar('path', null, 'POST', 'INT');
+		$query = 'SELECT id, folderpath FROM #__bsms_folders WHERE id = '.$path.' LIMIT 1';
+		$db->setQuery($query);
+		$folder = $db->loadObject();
+		$folderpath = $folder->folderpath;
+
+		$query = "SELECT id"
+		. "\nFROM #__menu"
+		. "\nWHERE link ='index.php?option=com_biblestudy&view=studieslist' and published = 1";
 		$db->setQuery($query);
 		$menuid = $db->loadResult();
 		$menureturn='';
 		if ($menuid) {$menureturn = '&Itemid='.$menuid;}
-	$filename = $file['name'];
-	if ($filename == 'index.htm'){
-		$mainframe->redirect("index.php?option=$option&view=mediafileslist".$menureturn, "File of this type not allowed.");
-			  return;
+		$filename = $file['name'];
+		if ($filename == 'index.htm'){
+			$mainframe->redirect("index.php?option=$option&view=mediafileslist".$menureturn, "File of this type not allowed.");
+			return;
 		}
-	if ($filename == 'index.html'){
-		$mainframe->redirect("index.php?option=$option&view=mediafileslist".$menureturn, "File of this type not allowed.");
-			  return;
+		if ($filename == 'index.html'){
+			$mainframe->redirect("index.php?option=$option&view=mediafileslist".$menureturn, "File of this type not allowed.");
+			return;
 		}
-	if ($filename == 'index.php'){
-		$mainframe->redirect("index.php?option=$option&view=mediafileslist".$menureturn, "File of this type not allowed.");
-			  return;
+		if ($filename == 'index.php'){
+			$mainframe->redirect("index.php?option=$option&view=mediafileslist".$menureturn, "File of this type not allowed.");
+			return;
 		}
-	if(isset($file) && is_array($file) && $file['name'] != '')
+		if(isset($file) && is_array($file) && $file['name'] != '')
 		{
-		   $fullfilename = JPATH_SITE.$folderpath. $file['name'];
-		   $filename = $file['name'];
-		   jimport('joomla.filesystem.file');
-		   
-		   
-		   if (JFile::exists($fullfilename)) {
-			  $mainframe->redirect("index.php?option=$option&view=mediafileslist".$menureturn, "Upload failed, file already exists.");
-			  return;
-		   }
-	
-		   if (!JFile::upload($file['tmp_name'], $fullfilename)) {
-			  $mainframe->redirect("index.php?option=$option&view=mediafileslist".$menureturn, "Upload failed, check to make sure that /components/$option/calendars exists.");
-			  return;
-		   }
-		   
-		}
+			$fullfilename = JPATH_SITE.$folderpath. $file['name'];
+			$filename = $file['name'];
+			jimport('joomla.filesystem.file');
+			 
+			 
+			if (JFile::exists($fullfilename)) {
+				$mainframe->redirect("index.php?option=$option&view=mediafileslist".$menureturn, "Upload failed, file already exists.");
+				return;
+			}
 
-		
+			if (!JFile::upload($file['tmp_name'], $fullfilename)) {
+				$mainframe->redirect("index.php?option=$option&view=mediafileslist".$menureturn, "Upload failed, check to make sure that /components/$option/calendars exists.");
+				return;
+			}
+			 
+		}
 	}
 
 }
