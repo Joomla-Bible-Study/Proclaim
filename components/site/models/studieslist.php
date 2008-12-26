309597b8b6ca28a7d25d5c8e8a637b74c065f964
genu <genu@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2008-12-26 22:02:52 +0000

    Moved queries from View into the model
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@130 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- components/site/models/studieslist.php
+++ components/site/models/studieslist.php
@@ -1,23 +1,29 @@
 <?php
-
- // studieslist Model for Bible Study Component
- 
-
-// Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die();
 global $mainframe, $option;
 jimport( 'joomla.application.component.model' );
 
 $params = &JComponentHelper::getParams($option);
 $default_order = $params->get('default_order');
- 
+
 class biblestudyModelstudieslist extends JModel
 {
-	var $_data;
+
 	var $_total = null;
 	var $_pagination = null;
-	//var $_limit = null;
 	
+	/**
+	 * @desc From database
+	 */
+	var $_data;
+	var $_teachers;
+	var $_series;
+	var $_messageTypes;
+	var $_studyYears;
+	var $_locations;
+	var $_topics;
+	var $_orders;
+
 	function __construct()
 	{
 		parent::__construct();
@@ -30,12 +36,12 @@ class biblestudyModelstudieslist extends JModel
 
 		// In case limit has been changed, adjust limitstart accordingly
 		$this->setState('limitstart', ($this->getState('limit') != 0 ? (floor($this->getState('limitstart') / $this->getState('limit')) * $this->getState('limit')) : 0));
-		
-		
+
+
 	}
 
 	/**
-	 * Returns the query
+	 * @desc Returns the query
 	 * @return string The query to be used to retrieve the rows from the database
 	 */
 	function _buildQuery()
@@ -43,28 +49,126 @@ class biblestudyModelstudieslist extends JModel
 		$where		= $this->_buildContentWhere();
 		$orderby	= $this->_buildContentOrderBy();
 		$query = 'SELECT #__bsms_studies.*, #__bsms_teachers.id AS tid, #__bsms_teachers.teachername, #__bsms_teachers.title AS teachertitle,'
-			. ' #__bsms_series.id AS sid, #__bsms_series.series_text, #__bsms_message_type.id AS mid,'
-			. ' #__bsms_message_type.message_type AS message_type, #__bsms_books.bookname,'
-			. ' #__bsms_topics.id AS tp_id, #__bsms_topics.topic_text, #__bsms_locations.id AS lid, #__bsms_locations.location_text'
-			. ' FROM #__bsms_studies'
-			. ' LEFT JOIN #__bsms_books ON (#__bsms_studies.booknumber = #__bsms_books.booknumber)'
-			. ' LEFT JOIN #__bsms_teachers ON (#__bsms_studies.teacher_id = #__bsms_teachers.id)'
-			. ' LEFT JOIN #__bsms_series ON (#__bsms_studies.series_id = #__bsms_series.id)'
-			. ' LEFT JOIN #__bsms_message_type ON (#__bsms_studies.messagetype = #__bsms_message_type.id)'
-			. '	LEFT JOIN #__bsms_topics ON (#__bsms_studies.topics_id = #__bsms_topics.id)'
-			. ' LEFT JOIN #__bsms_locations ON (#__bsms_studies.location_id = #__bsms_locations.id)'
-			. $where
-			. $orderby
-			;
+		. ' #__bsms_series.id AS sid, #__bsms_series.series_text, #__bsms_message_type.id AS mid,'
+		. ' #__bsms_message_type.message_type AS message_type, #__bsms_books.bookname,'
+		. ' #__bsms_topics.id AS tp_id, #__bsms_topics.topic_text, #__bsms_locations.id AS lid, #__bsms_locations.location_text'
+		. ' FROM #__bsms_studies'
+		. ' LEFT JOIN #__bsms_books ON (#__bsms_studies.booknumber = #__bsms_books.booknumber)'
+		. ' LEFT JOIN #__bsms_teachers ON (#__bsms_studies.teacher_id = #__bsms_teachers.id)'
+		. ' LEFT JOIN #__bsms_series ON (#__bsms_studies.series_id = #__bsms_series.id)'
+		. ' LEFT JOIN #__bsms_message_type ON (#__bsms_studies.messagetype = #__bsms_message_type.id)'
+		. '	LEFT JOIN #__bsms_topics ON (#__bsms_studies.topics_id = #__bsms_topics.id)'
+		. ' LEFT JOIN #__bsms_locations ON (#__bsms_studies.location_id = #__bsms_locations.id)'
+		. $where
+		. $orderby
+		;
 		return $query;
 	}
 
 	/**
-	 * Retrieves the data
-	 * @return array Array of objects containing the data from the database
+	 * @desc Returns teachers
+	 * @return Array
 	 */
+	function getTeachers() {
+		if (empty($this->_teachers)) {
+			$query = 'SELECT id AS value, teachername AS text, published'
+			. ' FROM #__bsms_teachers'
+			. ' WHERE published = 1'
+			. ' ORDER BY teachername ASC';
+			$this->_teachers = $this->_getList($query);
+		}
+		return $this->_teachers;
+	}
+
+	/**
+	 * @desc Returns teachers
+	 * @return Array
+	 */
+	function getSeries() {
+		if(empty($this->_series)) {
+			$query = 'SELECT id AS value, series_text AS text, published'
+			. ' FROM #__bsms_series'
+			. ' WHERE published = 1'
+			. ' ORDER BY series_text ASC';
+			$this->_series = $this->_getList($query);
+		}
+		return $this->_series;
+	}
+
+	/**
+	 * @desc Returns message types
+	 * @return Array
+	 */
+	function getMessageTypes() {
+		if (empty($this->_messageTypes)) {
+			$query = 'SELECT id AS value, message_type AS text, published'
+			. ' FROM #__bsms_message_type'
+			. ' WHERE published = 1'
+			. ' ORDER BY message_type ASC';
+			$this->_messageTypes = $this->_getList($query);
+		}
+		return $this->_messageTypes;
+	}
+
+	/**
+	 * @desc Returns years where studies exist
+	 * @return Array
+	 */
+	function getStudyYears() {
+		if (empty($this->_StudyYears)) {
+			$query = " SELECT DISTINCT date_format(studydate, '%Y') AS value, date_format(studydate, '%Y') AS text "
+			. ' FROM #__bsms_studies '
+			. ' ORDER BY value DESC';
+			$this->_StudyYears = $this->_getList($query);
+		}
+		return $this->_StudyYears;
+	}
+
+	/**
+	 * @desc Returns the locations
+	 * @return Array
+	 */
+	function getLocations() {
+		if (empty($this->_Locations)) {
+			$query = ' SELECT id AS value, location_text AS text, published FROM #__bsms_locations WHERE published = 1'
+			. ' ORDER BY location_text ASC ';
+			$this->_Locations = $this->_getList($query);
+		}
+		return $this->_Locations;
+	}
+
+	/**
+	 * @desc Returns message types
+	 * @return Array
+	 */
+	function getTopics() {
+		if (empty($this->_Topics)) {
+			$query = 'SELECT DISTINCT #__bsms_studies.topics_id AS value, #__bsms_topics.topic_text AS text'
+			. ' FROM #__bsms_studies'
+			. ' LEFT JOIN #__bsms_topics ON (#__bsms_topics.id = #__bsms_studies.topics_id)'
+			. ' WHERE #__bsms_topics.published = 1'
+			. ' ORDER BY #__bsms_topics.topic_text ASC';
+			$this->_Topics = $this->_getList($query);
+		}
+		return $this->_Topics;
+	}
+
+	/**
+	 * @desc Returns Orders
+	 * @return Array
+	 */
+	function getOrders() {
+		if (empty($this->_Orders)) {
+			$query = ' SELECT * FROM #__bsms_order '
+			. ' ORDER BY id ';
+			$this->_Orders = $this->_getList($query);
+		}
+		return $this->_Orders;
+	}
+
 	function getData()
 	{
+		//dump($data, 'Data from Model');
 		// Lets load the data if it doesn't already exist
 		if (empty( $this->_data ))
 		{
@@ -74,7 +178,7 @@ class biblestudyModelstudieslist extends JModel
 
 		return $this->_data;
 	}
-/**
+	/**
 	 * Method to get the total number of studies items
 	 *
 	 * @access public
@@ -105,14 +209,14 @@ class biblestudyModelstudieslist extends JModel
 		{
 			jimport('joomla.html.pagination');
 			$this->_pagination = new JPagination( $this->getTotal(), $this->getState('limitstart'), $this->getState('limit') );
-			
-			
-			
+
+
+
 		}
 
 		return $this->_pagination;
 	}
-function _buildContentWhere()
+	function _buildContentWhere()
 	{
 		global $mainframe, $option;
 		$params = &JComponentHelper::getParams($option);
@@ -128,7 +232,7 @@ function _buildContentWhere()
 		$topic_menu = $params->get('topic_id', 1);
 		$book_menu = $params->get('booknumber', 101);
 		$series_menu = $params->get('series_id', 1);
-		$messagetype_menu = $params->get('messagetype', 1); 
+		$messagetype_menu = $params->get('messagetype', 1);
 		$location_menu = $params->get('locations', 1);
 		$filter_orders		= $mainframe->getUserStateFromRequest( $option.'filter_orders',		'filter_orders',		'DESC',				'word' );
 
@@ -137,7 +241,7 @@ function _buildContentWhere()
 		$rightnow = date('Y-m-d H:i:s');
 		$where[] = ' #__bsms_studies.published = 1';
 		$where[] = " date_format(#__bsms_studies.studydate, '%Y-%m-%d %T') <= ".(int)$rightnow;
-		
+
 		if ($filter_topic > 0) {
 			$where[] = ' #__bsms_studies.topics_id = '.(int) $filter_topic;
 		}
@@ -183,14 +287,14 @@ function _buildContentWhere()
 		//$level_user = $user->usertype;
 		//dump ($level_user, 'Level_user: ');
 		$where[] = ' #__bsms_studies.show_level <= '.$level_user;
-		
+
 		//End for user level control
 
 		$where 		= ( count( $where ) ? ' WHERE '. implode( ' AND ', $where ) : '' );
-		
+
 		return $where;
 	}
-function _buildContentOrderBy()
+	function _buildContentOrderBy()
 	{
 		global $mainframe, $option;
 
