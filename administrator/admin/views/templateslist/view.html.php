766559b734a3fa597a757f2e1f097f29d959a1d2
genu <genu@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2009-01-12 21:34:04 +0000

    Added a tag summary function, and array of all tags
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@161 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- administrator/admin/views/templateslist/view.html.php
+++ administrator/admin/views/templateslist/view.html.php
@@ -4,6 +4,8 @@ defined('_JEXEC') or die('Restricted Access');
 jimport('joomla.application.component.view');
 class biblestudyViewtemplateslist extends JView {
 	
+	var $_tags;
+	
 	function display() {
 		JToolBarHelper::title(JText::_('Templates'), 'generic.png');
 		JToolBarHelper::publishList();
@@ -25,19 +27,34 @@ class biblestudyViewtemplateslist extends JView {
 			'tmplModule' => 'Module'
 		);
 		
+		//Creates array of all the tags for the 4 main tag categories
 		$tagsStudy = array('[studyDate]', '[studyTeacher]', '[studyNumber]', '[studyScripture1]', '[studyScripture2]', '[studyDVD]', '[studyCD]', '[studyTitle]', '[studyIntro]', '[studyComments]', '[studyHits]', '[studyUserAdded]', '[studyLocation]', '[studyMediaDuration]', '[studyMessageType]', '[studySeries]', '[studyTopic]', '[studyText]', '[studyMedia]');
 		$tagsStudyList = array('[filterLocation]', '[filterBook]', '[filterTeacher]', '[filterSeries]', '[filterType]', '[filterYear]', '[filterTopic]', '[filterOrder]', '[studiesList]', '[pagination]');
 		$tagsTeacher = array('[teacherName]', '[teacherTitle]', '[teacherPhone]', '[teacherEmail]', '[teacherWebsite]', '[teacherInformation]', '[teacherImage]', '[teacherShortDescription]');
 		$tagsTeacherList = array('[teachersList]');
 		
-		$this->assignRef('tagsStudy', $tagsStudy);
-		$this->assignRef('tagsStudyList', $tagsStudyList);
-		$this->assignRef('tagsTeacherer', $tagsTeacher);
-		$this->assignRef('tagsTeacherList', $tagsTeacherList);
+		//Creates an associative array of all the category tags and makes it available to the class
+		$this->_tags = array('tagsStudy' => $tagsStudy, 'tagsStudyList' => $tagsStudyList, 'tagsTeacher' => $tagsTeacher, 'tagsTeacherList' => $tagsTeacherList);
 		
 		$this->assignRef('tmplTypes', $tmplTypes);
 		$this->assignRef('templates', $templates);
 		parent::display();
+	}
+	
+	/**
+	 * @desc Generates a list of tags that are being used in the input template
+	 * @param $itemTmpl
+	 * @return Array
+	 */
+	function loadTagList($itemTmpl) {
+		foreach($this->_tags as $tagCategory) {
+			foreach($tagCategory as $tag) {
+				if(stristr($itemTmpl, $tag)) {
+					$tagArray[] = $tag;
+				}
+			}
+		}
+		return $tagArray;
 	}
 }
 ?>
