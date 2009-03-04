1d518cf76aae693d1b81305d98531a4589e94d19
genu <genu@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2009-03-04 19:00:51 +0000

    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@224 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- administrator/admin/js/biblestudy.js
+++ administrator/admin/js/biblestudy.js
@@ -61,4 +61,76 @@ $j(document).ready( function() {
 			}	
 		});
 	});
+	
+	/**
+	 * @title Templating Procedures
+	 */
+
+	//Determine the type of template, and route to that function
+	$j('#type').change(function() {
+		eval($j('#type option:selected').attr('value') + '()');
+	});
+	
+	
+	function canvasItemFunctions() {
+		$j('#canvasDeleteItem').click(function() {
+			//Delete Item, and update JSON string
+			
+			$j(this).parent('#canvasListItem').draggable(
+					{
+						handle: 'div#canvasDeleteItem'
+					}
+			);
+				
+		
+
+		});
+	}
+	
+	/**
+	 * @desc Creates Controls for a item on the canvas.
+	 */
+	function canvasItemControls(itemLabel) {
+		var itemOptions = '<div id="canvasItemOptions">&nbsp;</div>';
+		var moveItem = '<div id="canvasMoveItem">&nbsp;</div>';
+		var deleteItem = '<div id="canvasDeleteItem">&nbsp;</div>';
+		
+		$j('.canvasItem').append(itemOptions);
+		$j('.canvasItem').append(moveItem);
+		$j('.canvasItem').append(deleteItem);	
+		$j('.canvasItem').append('<div class="canvasItemName">' + itemLabel + '</div>');
+	
+		canvasItemFunctions();
+	}
+	
+
+	
+	
+	
+	function tmplList () {
+		var canvasListItem = '<div id="canvasListItem" class="canvasItem"></div>';
+		
+		$j('#tmplCanvas').append(canvasListItem);
+		canvasItemControls('List Items');
+	}
+	
+	function tmplListItem () {
+		alert ('this is the teacher list setup');
+	}
+	
+	function tmplSingleItem () {
+		alert ('this is the teacher list setup');
+	}
+	
+	function tmplModuleList () {
+		alert ('this is the teacher list setup');
+	}
+	
+	function tmplModuleItem () {
+		alert ('this is the teacher list setup');
+	}
+	
+	function tmplPopup () {
+		alert ('this is the teacher list setup');
+	}
 });
