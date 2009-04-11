0a37a6f3c2c7f3a30b5f5c28ea54e02546a20315
tomfuller2 <tomfuller2@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2009-04-11 15:59:21 +0000

    added store helper
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@283 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

---
+++ components/site/helpers/store.php
@@ -0,0 +1,47 @@
+<?php
+defined('_JEXEC') or die();
+
+function getStore($params, $rowid)
+{
+
+ 	global $mainframe;
+	$database	= & JFactory::getDBO();
+	$query = 'SELECT m.media_image_name, m.media_alttext, m.media_image_path, m.id AS mid, s.id AS sid,'
+    .' s.image_cd, s.prod_cd, s.server_cd, sr.id AS srid, sr.server_path
+                        FROM #__bsms_studies AS s
+                        LEFT JOIN #__bsms_media AS m ON ( m.id = s.image_cd )
+                        LEFT JOIN #__bsms_servers AS sr ON ( sr.id = s.server_cd )
+                        WHERE s.id ='.$rowid;
+    $database->setQuery($query);
+    $cd = $database->loadObject(); 
+	//dump ($cd, '$cd: ');
+	$query = 'SELECT m.media_image_name, m.media_alttext, m.media_image_path, m.id AS mid, s.id AS sid,'
+    .' s.image_dvd, s.prod_dvd, s.server_dvd, sr.id AS srid, sr.server_path
+                        FROM #__bsms_studies AS s
+                        LEFT JOIN #__bsms_media AS m ON ( m.id = s.image_dvd )
+                        LEFT JOIN #__bsms_servers AS sr ON ( sr.id = s.server_dvd )
+                        WHERE s.id ='.$rowid;
+    $database->setQuery($query);
+    $dvd = $database->loadObject();
+    
+	if (($cd->mid + $dvd->mid) > 0) 
+	{
+		$store = '<td width="'.$storewidth.'" <table><tr>'; //This td is for the store column
+    	if ($cd->mid > 0)
+		{
+      		$src = JURI::base().$cd->media_image_path;
+      		if ($imagew) {$width = $imagew;} else {$width = 24;}
+      		if ($imageh) {$height = $imageh;} else {$height= 24;}
+		$store .='<td><a href="'.$cd->server_path.$cd->prod_cd.'" title="'.$cd->media_alttext.'"><img src="'.JURI::base().$cd->media_image_path.'" width="'.$width.'" height="'.$height.'" alt="'.$cd->media_alttext.' "border="0" /></a></td>';
+    	}
+    
+		if ($dvd->mid > 0)
+		{
+		   $src = JURI::base().$dvd->media_image_path;
+		   if ($imagew) {$width = $imagew;} else {$width = 24;}
+		   if ($imageh) {$height = $imageh;} else {$height= 24;}
+		$store .='<td><a href="'.$dvd->server_path.$dvd->prod_dvd.'" title="'.$dvd->media_alttext.'"><img src="'.JURI::base().$dvd->media_image_path.'" width="'.$width.'" height="'.$height.'" alt="'.$dvd->media_alttext.' "border="0" /></a></td></tr><tr><td colspan="2" align="center"><span'.$params->get('store_span').$params->get('store_name').'</span></td></tr></table></td>';
+		}
+	}
+return $store;
+}
