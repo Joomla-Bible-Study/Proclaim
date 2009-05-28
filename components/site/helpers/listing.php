62a8ce48143be791e7c1ea78a80e258d5c24e2d6
tomfuller2 <tomfuller2@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2009-05-28 00:12:38 +0000

    Adding list.php which will be the master helper to create a listing row in studieslist
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@327 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

---
+++ components/site/helpers/listing.php
@@ -0,0 +1,15 @@
+<?php defined('_JEXEC') or die('Restriced Access');
+//Helper file - master list creater for study lists
+function getListing($row, $params)
+{
+	if ($params->get('row1cell1') {
+					 $listing = '<tr ';}
+					 if ($params->get('rowspanr1c1') > 1){$listing .='rowspan="'.$params->get('rowspanr1c1').'><td ';}
+					 	else $listing .='>';}
+					 if ($params->get('r1c1span') > 1) {$listing .= 'colspan="'.$params->get('r1c1span').'">';}
+						else {$listing .='>';}
+	
+	
+	
+return $listing;
+}
