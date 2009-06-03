594801f1b5932a24f63cab64f3a8b7aec8449d45
tomfuller2 <tomfuller2@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2009-06-03 05:40:27 +0000

    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@342 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- administrator/admin/views/seriesedit/tmpl/form.php
+++ administrator/admin/views/seriesedit/tmpl/form.php
@@ -3,17 +3,18 @@
 <form action="index.php" method="post" name="adminForm" id="adminForm">
 <div class="col100">
 	<fieldset class="adminform">
-		<legend><?php echo JText::_( 'Details' ); ?></legend>
+		<legend><?php echo JText::_( 'CSS' ); ?></legend>
 
 		<table class="admintable">
 		<tr>
 			<td width="100" align="right" class="key">
-				<label for="series">
-					<?php echo JText::_( 'Series' ); ?>:
+				<label for="css">
+					<?php echo JText::_( 'CSS' ); ?>:
 				</label>
 			</td>
 			<td>
-				<input class="text_area" type="text" name="series_text" id="series_text" size="100" maxlength="250" value="<?php echo $this->seriesedit->series_text;?>" />
+				
+				<textarea name="css_config" class="inputbox" rows="20" cols="92"><?php echo $this->lists['initstring'];?></textarea>
 			</td>
 		</tr>
 	</table>
@@ -22,7 +23,7 @@
 <div class="clr"></div>
 
 <input type="hidden" name="option" value="com_biblestudy" />
-<input type="hidden" name="id" value="<?php echo $this->seriesedit->id; ?>" />
+<input type="hidden" name="id" value="" />
 <input type="hidden" name="task" value="" />
-<input type="hidden" name="controller" value="seriesedit" />
+<input type="hidden" name="controller" value="cssedit" />
 </form>
