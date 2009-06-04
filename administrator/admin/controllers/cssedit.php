c820eac44dbe19cbf091dbbd0caaa719bae8efb9
tomfuller2 <tomfuller2@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2009-06-04 23:14:03 +0000

    workinfg on studydetails
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@346 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- administrator/admin/controllers/cssedit.php
+++ administrator/admin/controllers/cssedit.php
@@ -43,83 +43,8 @@ class biblestudyControllercssedit extends JController
 	 * save a record (and redirect to main page)
 	 * @return void
 	 */
-	function save2()
-	 {
-		 global $mainframe, $option; 
-		//$filecontent = $podhead.$episodedetail.$podfoot;
-  		$filecontent = JRequest::getVar('config_css', '', 'post', 'string', JREQUEST_ALLOWRAW);
-		dump ($filecontent, 'filecontent: ');
-  // Initialize some variables
-  $option = JRequest::getCmd('option');
-
-  if (!$filecontent) {
-   $mainframe->redirect('index.php?option='.$option.'&view=studieslist', JText::_('Operation Failed').': '.JText::_('Content empty.'));
-  }
-
-  // Set FTP credentials, if given
-  jimport('joomla.client.helper');
-  jimport('joomla.filesystem.file');
-  JClientHelper::setCredentialsFromRequest('ftp');
-  $ftp = JClientHelper::getCredentials('ftp');
-  $client =& JApplicationHelper::getClientInfo(JRequest::getVar('client', '0', '', 'int'));
-  //$file = $client->path.DS.'templates'.DS.$template.DS.'index.php';
-  $file = JPATH_ROOT.DS.'components'.DS.'com_biblestudy'.DS.'assets'.DS.'css'.DS.'biblestudy.css';
-
-
-  // Try to make the template file writeable
-  if (JFile::exists($file) && !$ftp['enabled'] && !JPath::setPermissions($file, '0755')) {
-   JError::raiseNotice('SOME_ERROR_CODE', 'Could not make the file writable');
-  }
-
-  $return = JFile::write($file, $filecontent);
-
-  // Try to make the template file unwriteable
-  if (!$ftp['enabled'] && !JPath::setPermissions($file, '0555')) {
-   JError::raiseNotice('SOME_ERROR_CODE', 'Could not make the file unwritable');
-  }
-
-  if ($return)
-  {
-   $task = JRequest::getCmd('task');
-   switch($task)
-   {
-    case 'apply_source':
-     $mainframe->redirect('index.php?option='.$option.'&view=studielist', JText::_($podinfo->filename.' saved'));
-     break;
-
-    case 'save_source':
-    default:
-     $mainframe->redirect('index.php?option='.$option.'&view=studieslist', JText::_($podinfo->filename.' saved'));
-     break;
-   }
-  }
-  else {
-   $mainframe->redirect('index.php?option='.$option.'&view=studieslist', JText::_('Operation Failed').': '.JText::_('Failed to open file for writing.'));
-  }
   
-	//$config_css = mosGetParam( $_POST, 'config_css' );
-	//$config_css = JRequest::getVar('config_css', 'POST');
-	//$config_css = JRequest::getVar('config_css', '', 'post', 'string', JREQUEST_ALLOWRAW);
 	
-	//dump ($config_css, 'config_css');
-		//$configcss=str_replace("[CR][NL]","\n",$config_css);
-		//$configcss=str_replace("[ES][SQ]","'",$configcss);
-		//$configcss=nl2br($configcss);
-		//$configcss=str_replace("<br />"," ",$configcss);
-		//$filename = JPATH_ROOT.DS.'components'.DS.'com_biblestudy'.DS.'assets'.DS.'css'.DS.'biblestudy.css';
-		//jimport('joomla.filesystem.file');
-		//$filecontent = $configcss;
-		//$return = JFile::write($filename, $filecontent);
-		//$cssfilein=fopen($filename,"w+") or die("Can't open file $filename");
-		//$filecontent=fread($cssfilein,filesize($filename));
-		//$cssfileout=fwrite($cssfilein,$configcss);
-		//fclose($cssfilein);
-		
-//if ($return) {$msg = 'CSS Saved';}
-//else { $msg = 'Problem writing file';
-//$mainframe->redirect('index.php?option=com_biblestudy&view=studieslist', JText::_('File Saved'));}
- // $link = 'index.php?option=com_biblestudy&view=studieslist';
-		//$this->setRedirect($link, $msg);
 }
 
 function reset() {
@@ -159,60 +84,18 @@ if ($return)
 
 		
 
-		function editCSS()
-	{
-		global $mainframe;
-
-		// Initialize some variables
-		$option		= JRequest::getCmd('option');
-		$client		=& JApplicationHelper::getClientInfo(JRequest::getVar('client', '0', '', 'int'));
-		$template	= JRequest::getVar('id', '', 'method', 'cmd');
-		$filename	= JRequest::getVar('filename', '', 'method', 'cmd');
-
-		jimport('joomla.filesystem.file');
-
-		if (JFile::getExt($filename) !== 'css') {
-			$msg = JText::_('Wrong file type given, only CSS files can be edited.');
-			$mainframe->redirect('index.php?option='.$option.'&client='.$client->id.'&task=choose_css', $msg, 'error');
-		}
-
-		$content = JFile::read($client->path.DS.'administrator'.DS.'components'.DS.'com_biblestudy'.DS.'assets'.DS.'css'.DS.$filename);
-
-		if ($content !== false)
-		{
-			// Set FTP credentials, if given
-			jimport('joomla.client.helper');
-			$ftp =& JClientHelper::setCredentialsFromRequest('ftp');
-
-			$content = htmlspecialchars($content, ENT_COMPAT, 'UTF-8');
-			require_once (JPATH_COMPONENT.DS.'admin.templates.html.php');
-			TemplatesView::editCSSSource($template, $filename, $content, $option, $client, $ftp);
-		}
-		else
-		{
-			$msg = JText::sprintf('Operation Failed Could not open', $client->path.$filename);
-			$mainframe->redirect('index.php?option='.$option.'&client='.$client->id, $msg);
-		}
-	}
+		
 
 	function save()
 	{
 		global $mainframe;
 
-		// Check for request forgeries
-		//JRequest::checkToken() or jexit( 'Invalid Token' );
 
 		// Initialize some variables
 		$option			= JRequest::getCmd('option');
 		$client			=& JApplicationHelper::getClientInfo(JRequest::getVar('client', '0', '', 'int'));
-		//$template		= JRequest::getVar('id', '', 'post', 'cmd');
-		//$filename		= JRequest::getVar('filename', '', 'post', 'cmd');
 		$filename		= 'biblestudy.css';
 		$filecontent	= JRequest::getVar('filecontent', '', 'post', 'string', JREQUEST_ALLOWRAW);
-
-		//if (!$template) {
-			//$mainframe->redirect('index.php?option='.$option.'&client='.$client->id, JText::_('Operation Failed').': '.JText::_('No template specified.'));
-		//}
 
 		if (!$filecontent) {
 			$mainframe->redirect('index.php?option='.$option.'&view=cssedit', JText::_('Operation Failed').': '.JText::_('Content empty.'));
@@ -223,20 +106,12 @@ if ($return)
 		JClientHelper::setCredentialsFromRequest('ftp');
 		$ftp = JClientHelper::getCredentials('ftp');
 
-		//$file = $client->path.DS.'administrator'.DS.'components'.DS.'com_biblestudy'.DS.'assets'.DS.'css'.DS.$filename;
 		$file = JPATH_ROOT.DS.'components'.DS.'com_biblestudy'.DS.'assets'.DS.'css'.DS.$filename;
 		// Try to make the css file writeable
-		//if (!$ftp['enabled'] && JPath::isOwner($file) && !JPath::setPermissions($file, '0755')) {
-			//JError::raiseNotice('SOME_ERROR_CODE', JText::_('Could not make the css file writable'));
-		//}
 
 		jimport('joomla.filesystem.file');
 		$return = JFile::write($file, $filecontent);
 
-		// Try to make the css file unwriteable
-		//if (!$ftp['enabled'] && JPath::isOwner($file) && !JPath::setPermissions($file, '0555')) {
-			//JError::raiseNotice('SOME_ERROR_CODE', JText::_('Could not make the css file unwritable'));
-		//}
 
 		if ($return)
 		{
--- components/site/views/studydetails/tmpl/default.php
+++ components/site/views/studydetails/tmpl/default.php
@@ -15,171 +15,39 @@ else { document.getElementById(d).style.display = "none"; }
 global $mainframe, $option;
 $params = $mainframe->getPageParameters();
 $document =& JFactory::getDocument();
-$type = 'text/css';
-$css_call = JView::loadHelper('css');
-$styles = getCss($params);
-$document->addStyleDeclaration($styles, $type);
-$document->addStyleSheet(JURI::base().'components'.DS.'com_biblestudy'.DS.'biblestudyviews.css');
+$document->addStyleSheet(JURI::base().'components'.DS.'com_biblestudy'.DS.'assets'.DS.'css'.DS.'biblestudy.css');
 $url = $params->get('stylesheet');
 if ($url) {$document->addStyleSheet($url);}
-$pageclass_sfx = $params->get('pageclass_sfx');
 $row = $this->studydetails;
+$listingcall = JView::loadHelper('listing');
 ?>
-<div class="detailspagecontainer<?php echo $pageclass_sfx;?>"> <!-- This div is the container for the whole page -->
+  <div id="biblestudy" class="noRefTagger"> <!-- This div is the container for the whole page -->
+ <div id="header">
+ <h1 class="componentheading">
+ <?php echo $row->studytitle;?>
+  </h1>
+   </div><!-- header -->
+ 
+ <table id="bslisttable" cellspacing="0">
+     <?php 
+	 if ($params->get('use_headers') > 0){
+     $headerCall = JView::loadHelper('header');
+     $header = getHeader($row, $params);
+     echo $header;}
+     ?>
+      <tbody>
 
-<div id="detailstitlecontainer<?php echo $pageclass_sfx;?>">		
-<div class="buttonheading<?php echo $pageclass_sfx;?>">
-
-	<?php 
-	if ($this->params->get('show_print_view') > 0) 
-	{
-		$text = JHTML::_('image.site',  'printButton.png', '/images/M_images/', NULL, NULL, JText::_( 'Print' ) );
-        echo '<a href="#&tmpl=component" onclick="window.print();return false;">'.$text.'</a>';
-	}
-	if ($this->params->get('show_pdf_view') > 0 ) 
-    { 
-        $url = 'index.php?option=com_biblestudy&view=studydetails&id='.$this->studydetails->id.'&format=pdf';
-        $status = 'status=no,toolbar=no,scrollbars=yes,titlebar=no,menubar=no,resizable=yes,width=640,height=480,directories=no,location=no';
-        $text = JHTML::_('image.site', 'pdf24.png', '/components/com_biblestudy/images/', NULL, NULL, JText::_('PDF'), JText::_('PDF'));
-        $attribs['title']	= JText::_( 'PDF' );
-        $attribs['onclick'] = "window.open(this.href,'win2','".$status."'); return false;";
-        $attribs['rel']     = 'nofollow';
-        $link = JHTML::_('link', JRoute::_($url), $text, $attribs);
-        echo $link; 
-    } ?>
-
-</div>
-<?php
-	if ($params->get('show_teacher_view') > 0)
-	{	?>        
-<div class="detailsteacher<?php echo $pageclass_sfx;?>">
-    
-    <?php	
-	$teacher_call = JView::loadHelper('teacher');
-	$teacher = getTeacher($params, $row->teacher_id);
-	if ($teacher) {echo $teacher;}
-	?>
-</div><!--end of detailsteacher div-->
-<?php } ?>
-<?php if ($params->get('title_line_1') + $params->get('title_line_2') > 0) 
-	{
-		$title_call = JView::loadHelper('title');
-		$title = getTitle($params, $row);
-		echo $title;
-	}?> 
-</div><!--end of titlecontainer div-->
-<?php if ($params->get('use_headers') >0) { ?>
-	
-	<div class="detailsheadercontainer<?php echo $pageclass_sfx;?>" >
-	<?php 
-    $header_call = JView::loadHelper('header');
-    $header = getHeader($this->params);
-    echo $header;
-	echo '</div>';
-	}?>
-    
-
-    
-   <?php  
-    $listarraycall = JView::loadHelper('listarray');
-	$a = getListarray($params, $row);
-		
-		  //This calls the helper once that will process each column's array, coming from the $a variable. We will then call a function in each column from this helper file
-		  $array_call = JView::loadHelper('columnarray');
-		echo '<div class="detailslistingcontainer'.$pageclass_sfx.'">';
-        
-		$columnnumber = 1;
-		$column1 = getColumnarray($a, $row, $columnnumber, $this->params);
-		if ($column1) { echo '<div class="column'.$columnnumber.$pageclass_sfx.'">';}
-		echo $column1; 
-		
-		if ($column1) {echo '</div>';}
-		$columnnumber = 2;
-		$column2 = getColumnarray($a, $row, $columnnumber, $this->params);
-		if ($column2) { echo '<div class="column'.$columnnumber.$pageclass_sfx.'">';}
-		echo $column2; 
-		
-		if ($column2) {echo '</div>';}
-        $columnnumber = 3;
-		$column3 = getColumnarray($a, $row, $columnnumber, $this->params);
-		if ($column3) { echo '<div class="column'.$columnnumber.$pageclass_sfx.'">';}
-       	echo $column3; 
-		
-		if ($column3) {echo '</div>';}
-		$columnnumber = 4;
-		$column4 = getColumnarray($a, $row, $columnnumber, $this->params);
-		if ($column4) { echo '<div class="column'.$columnnumber.$pageclass_sfx.'">';}
-		echo $column4;
-		
-		if ($column4) {echo '</div>';}
-		
-	
-		//Store section
-		
-		if ($params->get('show_store') > 0) {
-			$store_call = JView::loadHelper('store');
-			$store = getStore($params, $row->id);
-			echo '<div class="detailsstore'.$pageclass_sfx.'">'.$store.'</div>';
-			
-		} //end store
-		//show media section
-		
-		if ($params->get('show_media') > 0) {
-				echo '<div class="detailsmediatable'.$pageclass_sfx.'">';
-        		$ismodule = 0;
-				$filesize_call = JView::loadHelper('filesize');
-				$call_filepath = JView::loadHelper('filepath');
-				$call_mediatable = JView::loadHelper('mediatable');
-				$mediatable = getMediatable($params, $row);
-				echo $mediatable.'</div>';
-				
-		}//End of bsmediatable div
-		
-		//column for description
-		
-		if ($params->get('show_description') > 0) {
-	        echo '<div class="detailsbottomlisting'.$pageclass_sfx.'">'.$row->studyintro.'</div>';
-			}//End of bsbottomlisting
-          ?>      
-        
-        </div><!--end of detailslistingcontainer-->
-        
-		<div class="detailstext<?php echo $pageclass_sfx;?>">
-        <p><?php echo $this->article->studytext;?></p>
-        
-         <?php if ($params->get('show_comments') > 0)
-		{?>
-        <div class="commentstable<?php echo $params->get('pageclass_sfx');?>">
-		<?php $Itemid = JRequest::getVar('Itemid');
-		$comments_call = JView::loadHelper('comments');
-        $comments = getComments($params, $row, $Itemid);
-		echo $comments;
-		?>
-               
-        </div><!--end of div for comments-->
-        
-        <?php } //end of if comments param?>
-
-        <?php if ($this->params->get('show_passage_view') > 0) { ?>
-		<div id="passagecontainer<?php echo $pageclass_sfx;?>">
-          <strong><a class="heading" href="javascript:ReverseDisplay('scripture')">>><?php echo JText::_('Show/Hide Scipture Passage');?><<</a>
-
-        <div id="scripture" style="display:none;"></strong>
-          <?php 
-		  $passage_call = JView::loadHelper('passage');
-          $response = getPassage($params, $row);
-          echo $response;?>
-        </div>
-
-        
-        </div>			
-		<?php } //end of if passage?>
-
-        </div><!--end of detailsstudytext-->
-        
-       
-
-    <div class="detailsfooter<?php echo $pageclass_sfx;?>">
+        <?php 
+ //This sets the alternativing colors for the background of the table cells
+$oddeven = 'bsodd';
+ $listing = getListing($row, $params, $oddeven);
+ echo $listing;?>
+ </tbody></table>
+ <table id="studydetailstable" cellspacing="0">
+ <tr><td id="studydetailstext">
+ <?php echo $this->article->studytext;?>
+ </td></tr></table>
+    <div class="listingfooter">
     <?php $link_text = $this->params->get('link_text');
 			if (!$link_text) {
 				$link_text = JText::_('Return to Studies List');
@@ -195,4 +63,5 @@ $row = $this->studydetails;
 						$link = JRoute::_('index.php?option=com_biblestudy&view=studieslist&Itemid='.$item);}?>
 			<a href="<?php echo $link;?>"> <?php echo $link_text; ?> </a> <?php } //End of if view_link not 0?>
     </div><!--end of footer div-->
+
 </div><!--End of page container div-->
