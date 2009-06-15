87da10286dbfcddc27cda98fba99f35385a55a4b
tomfuller2 <tomfuller2@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2009-06-15 05:19:59 +0000

    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@365 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- components/site/helpers/translated.php
+++ components/site/helpers/translated.php
@@ -21,15 +21,15 @@ function getTranslated($result)
 		//$key = $result2->value;
 		//if ($format == $result->text)
 		//{
-			unset($result[$key]);
-			array_push($result,$output);	
+			unset($value[$key]);
+			array_push($value,$output);	
 		//}
 		//$array1['id'] = $key;
 		//$array2['text'] = $output;
 	}
 	//$translated = array_combine($array1, $array2);
 //return $translated;
-return $result;
+return $value;
 }
 
 
