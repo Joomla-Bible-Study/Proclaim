3c7f1799aaf82653d0aa1815cf0f510bc77469d7
tomfuller2 <tomfuller2@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2009-06-15 18:43:31 +0000

    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@366 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- components/site/helpers/translated.php
+++ components/site/helpers/translated.php
@@ -8,27 +8,23 @@
 defined('_JEXEC') or die('Restriced Access');
 
 function getTranslated($result)
+
 {
-	
-	
-	//$array1 = array('id' => '');
-	//$array2 = array( 'text' => '');
-	foreach ($result as $key => $value)
-	{
-		$format = $value->text;
-		$output->text = JText::_($format);
-		$output->id = $key;
-		//$key = $result2->value;
-		//if ($format == $result->text)
-		//{
-			unset($value[$key]);
-			array_push($value,$output);	
-		//}
-		//$array1['id'] = $key;
-		//$array2['text'] = $output;
-	}
-	//$translated = array_combine($array1, $array2);
-//return $translated;
+    $output = array();
+	//$arr = (array) $result;
+	foreach ($result as $value)
+    {
+      $format = $value->text;
+        $output=>text = JText::_($format);
+		$output=>value = $value->value;
+		$output=>id = $value->id;
+		$output=>published = $value->published;
+        //$value->id = $key;
+          //unset($result->text);
+         array_push($value,$output);   
+
+    }
+   
 return $value;
 }
 
