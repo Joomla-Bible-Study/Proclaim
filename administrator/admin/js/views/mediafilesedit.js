78b7249848afaa8b30e6efd523009b68fcb49b6e
genu <genu@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2009-08-12 03:23:23 +0000

    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@452 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

---
+++ administrator/admin/js/views/mediafilesedit.js
@@ -0,0 +1,79 @@
+$j(document).ready( function() {
+	$j('#loading').ajaxStart( function() {
+		$j(this).show();
+	});
+	$j('#loading').ajaxStop( function() {
+		$j(this).hide();
+	});
+	
+	// Docman integration
+	$j('#docManCategories').change(function() {
+			docManItems = $j('#docManItems');
+			docManItems.removeOption(/./);
+			var catId = $j('#docManCategories option:selected').attr(
+					'value');
+			var url = 'index.php?option=com_biblestudy&controller=mediafilesedit&task=docmanCategoryItems&format=raw&catId=';
+			// request the items
+			$j.ajax( {
+				dataType : "json",
+				url : url + catId,
+				success : function(data) {
+					$j.each(data, function(entryIndex, entry) {
+						docManItems.addOption(entry['id'], entry['name']);
+						$j('#docManItemsContainer').show();
+						
+						$j('#articlesCategoriesContainer').hide();
+						$j('#articlesItemsContainer').hide();
+						
+						$j('#articleSectionCategories').removeOption(/./);
+						$j('#categoryItems').removeOption(/./);
+						
+					})
+				}
+			});
+		});
+	
+	// Articles Integration
+	$j('#articlesSections').change(function() {
+		$j('#articleSectionCategories').removeOption(/./);
+		var secId = $j('#articlesSections option:selected').attr(
+				'value');
+		var url = 'index.php?option=com_biblestudy&controller=mediafilesedit&task=articlesSectionCategories&format=raw&secId=' + secId;
+		$j.ajax( {
+			dataType : "json",
+			url : url,
+			success : function(data) {
+				$j.each(data, function(entryIndex, entry) {
+					$j('#articleSectionCategories').addOption(entry['id'], entry['title']);
+					$j('#articlesCategoriesContainer').show();	
+					$j('#docManItemsContainer').hide();
+					$j('#docManItems').removeOption(/./);
+				})
+			}
+		});
+		$j('#categoryItems').removeOption(/./);
+	});
+	
+	$j('#articleSectionCategories')
+			.change(
+					function() {
+						$j('#categoryItems').removeOption(/./);
+						var catId = $j('#articleSectionCategories option:selected')
+								.attr('value');
+						var url = 'index.php?option=com_biblestudy&controller=mediafilesedit&task=articlesCategoryItems&format=raw&catId=' + catId;
+						$j.ajax( {
+							dataType : "json",
+							url : url,
+							success : function(data) {
+								$j.each(data, function(entryIndex, entry) {
+									$j('#categoryItems').addOption(entry['id'],	entry['title']);
+									$j('#articlesItemsContainer').show();
+								})
+							}
+						});
+					});
+	$j('#categoryItems').change( function() {
+		$j('#docManItemsContainer').hide();
+		$j('#docManItems').removeOption(/./);
+	});
+});
