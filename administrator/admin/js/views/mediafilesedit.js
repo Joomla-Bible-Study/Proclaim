558b3473b3bd17ce920cbf3037213b01ed3b8a5d
genu <genu@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2009-08-12 22:27:29 +0000

    Fixed bug with having only one category in a section
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@460 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- administrator/admin/js/views/mediafilesedit.js
+++ administrator/admin/js/views/mediafilesedit.js
@@ -36,32 +36,37 @@ $j(document).ready( function() {
 	// Articles Integration
 	$j('#articlesSections').change(function() {
 		$j('#articleSectionCategories').removeOption(/./);
-		var secId = $j('#articlesSections option:selected').attr(
-				'value');
+		var secId = $j('#articlesSections option:selected').attr('value');
 		var url = 'index.php?option=com_biblestudy&controller=mediafilesedit&task=articlesSectionCategories&format=raw&secId=' + secId;
-		$j.ajax( {
-			dataType : "json",
-			url : url,
-			success : function(data) {
-				$j.each(data, function(entryIndex, entry) {
-					$j('#articleSectionCategories').addOption(entry['id'], entry['title']);
-					$j('#articlesCategoriesContainer').show();	
-					$j('#docManItemsContainer').hide();
-					$j('#docManItems').removeOption(/./);
-				})
-			}
-		});
-		$j('#categoryItems').removeOption(/./);
+		if(secId !== "") {
+			$j.ajax( {
+				dataType : "json",
+				url : url,
+				success : function(data) {
+					$j.each(data, function(entryIndex, entry) {
+						$j('#articleSectionCategories').addOption(entry['id'], entry['title']);
+						$j('#articlesCategoriesContainer').show();	
+						$j('#docManItemsContainer').hide();
+						$j('#docManItems').removeOption(/./);
+					})	
+					refreshArticleItems();
+				}
+			});
+			$j('#categoryItems').removeOption(/./);
+		}else{
+			//Hide all others
+			$j('#articlesCategoriesContainer').hide();
+			$j('#articlesItemsContainer').hide();
+		}
 	});
 	
 	$j('#articleSectionCategories')
 			.change(
 					function() {
 						$j('#categoryItems').removeOption(/./);
-						var catId = $j('#articleSectionCategories option:selected')
-								.attr('value');
+						var catId = $j('#articleSectionCategories option:selected').attr('value');
 						var url = 'index.php?option=com_biblestudy&controller=mediafilesedit&task=articlesCategoryItems&format=raw&catId=' + catId;
-						$j.ajax( {
+						$j.ajax({
 							dataType : "json",
 							url : url,
 							success : function(data) {
@@ -76,4 +81,19 @@ $j(document).ready( function() {
 		$j('#docManItemsContainer').hide();
 		$j('#docManItems').removeOption(/./);
 	});
+	
+	function refreshArticleItems() {
+		var catId = $j('#articleSectionCategories option:selected').attr('value');
+		var url = 'index.php?option=com_biblestudy&controller=mediafilesedit&task=articlesCategoryItems&format=raw&catId=' + catId;
+		$j.ajax({
+			dataType : "json",
+			url : url,
+			success : function(data) {
+				$j.each(data, function(entryIndex, entry) {
+					$j('#categoryItems').addOption(entry['id'],	entry['title']);
+					$j('#articlesItemsContainer').show();
+				})
+			}
+		});
+	}
 });
