7a80e9f76d28b5c3c4fe2ba5499a5ce01566a9c4
du8die <du8die@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2009-08-27 03:04:04 +0000

    Added classes and Id's for Tables, TD's, and TR.
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@495 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- components/site/views/teacherlist/tmpl/default.php
+++ components/site/views/teacherlist/tmpl/default.php
@@ -17,7 +17,7 @@ if (!$templatemenuid){$templatemenuid = 1;}
 <table width="100%">
     <tr><td align="center"><h1><?php echo $this->params->get('teacher_title');?></h1></td></tr>
     <tr>
-        <td>
+        <td class='bsm_separator'>
             <img src="<?php echo JURI::base().'components/com_biblestudy/images/square.gif'?>" height="3" width="100%" />
         </td>
     </tr>
@@ -28,38 +28,38 @@ if ($entry_access <= $entry_user){ ?>
 <table><tr><td><a href="index.php?option=com_biblestudy&view=teacheredit&layout=form"><strong><?php echo JText::_('Add a Teacher');?></strong></a></td></tr></table><?php } }?>
 <table width="100%" class="contentpaneopen<?php echo $this->params->get( 'pageclass_sfx' ); ?>">
 <tr><td>
-<?php foreach ($this->items as $item) { 
+<?php foreach ($this->items as $item) {
 if (!$item->teacher_thumbnail) { $image->path = $item->thumb; $image->height = $item->thumbh; $image->width = $item->thumbw; }
-	else 
+	else
 	{
 		if ($item->teacher_thumbnail && !$admin_params->get('teachers_imagefolder')) { $i_path = 'components/com_biblestudy/images/stories/'.$item->teacher_thumbnail; }
 		if ($item->teacher_thumbnail && $admin_params->get('teachers_imagefolder')) { $i_path = 'images'.DS.$admin_params->get('teachers_imagefolder').DS.$item->teacher_thumbnail;}
 		$image = getImage($i_path);
 	}
 ?>
-	
-    <table cellpadding="1" cellspacing="1">
+
+    <table cellpadding="1" cellspacing="1" class='bsm_teachertable' id='bsm_teacher_<?php echo $this>id?>'>
     <tr>
     <?php if ($allow_entry > 0) {
 if ($entry_access <= $entry_user){ ?>
   <td><a href="index.php?option=com_biblestudy&view=teacheredit&layout=form&controller=teacheredit&cid[]=<?php echo $item->id?>"><?php echo '['.JText::_('Edit').']';?></a></td>
   <?php } }?>
-        <td ><?php if ($item->thumb || $item->teacher_thumbnail){?>
+        <td class='bsm_teacherthumbnail' id='bsm_teacher_thumbnail_<?php echo $this>id?>'><?php if ($item->thumb || $item->teacher_thumbnail){?>
         	<img src="<?php echo $image->path;?>" border="1" title="<?php echo $item->teachername;?>" alt="<?php echo $item->teachername;?>" width="<?php echo $image->width;?>" height="<?php echo $image->height;?>" /><?php } ?>
         </td>
-        <td width="150" align="left">
+        <td width="150" align="left" class='bsm_teachername' id='bsm_teacher_name_<?php echo $this>id?>'>
             <a href="index.php?option=com_biblestudy&view=teacherdisplay&id=<?php echo $item->id.'&templatemenuid='.$templatemenuid;?>"><?php echo $item->teachername;?></a><?php echo JText::_(' - ');?>
             <?php echo $item->title;?>
         </td>
         <td width="5">  </td>
-        <td align="left">
+        <td align="left" class='bsm_short' id='bsm_teacher_short_<?php echo $this>id?>'>
 			<?php echo $item->short;?>
         </td>
      </tr>
     </table>
-    
+
 <table >
-    	<tr><td><img src="<?php echo JURI::base().'components/com_biblestudy/images/square.gif'?>" height="1" width="100%" /></td></tr>
+    	<tr><td class='bsm_separator'><img src="<?php echo JURI::base().'components/com_biblestudy/images/square.gif'?>" height="1" width="100%" /></td></tr>
 </table>
     <?php } //end of foreach ?>
 </td></tr>
