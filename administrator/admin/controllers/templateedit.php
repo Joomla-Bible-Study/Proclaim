8c25aa25474a3950aa81a0bd6eb6196cd3dfa54d
genu <genu@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2009-11-03 18:22:48 +0000

    Incorporated Copy routine
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@571 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- administrator/admin/controllers/templateedit.php
+++ administrator/admin/controllers/templateedit.php
@@ -20,6 +20,20 @@ class biblestudyControllertemplateedit extends JController {
 		parent::display();
 	}
 
+	function copy() {
+		$cid =  JRequest::getVar( 'cid', array(), 'post', 'array');
+		JArrayHelper::toInteger($cid);
+
+		$model	=& $this->getModel('templateedit');
+
+		if ($model->copy($cid)) {
+			$msg = JText::sprintf('Template(s) have been copied');
+		} else {
+			$msg = $model->getError();
+		}
+		$this->setRedirect('index.php?option=com_biblestudy&view=templateslist', $msg);
+	}
+
 	function save() {
 		$model = $this->getModel('templateedit');
 		$data = JRequest::get('post');
@@ -29,22 +43,21 @@ class biblestudyControllertemplateedit extends JController {
 			$msg = JText::_( 'Error Saving Template' );
 		}
 
+		switch ($this->_task) {
+			case 'apply':
+				$msg = JText::_( 'Changes to Template Updated! (by Apply)' );
+				$cid 	= JRequest::getVar( 'id', 1, 'post', 'int' );
+				$link = 'index.php?option=com_biblestudy&view=templateedit&layout=form&task=edit&cid[]='. $cid;
+				break;
 
-switch ($this->_task) {
-         case 'apply':   
-            $msg = JText::_( 'Changes to Template Updated! (by Apply)' );
-			$cid 	= JRequest::getVar( 'id', 1, 'post', 'int' );
-            $link = 'index.php?option=com_biblestudy&view=templateedit&layout=form&task=edit&cid[]='. $cid;
-           break;
+			case 'save':
+			default:
+				//$msg = JText::_( 'Data Saved!' );
+				//$link = 'index.php?option=com_driver';
 
-         case 'save':
-         default:         
-            //$msg = JText::_( 'Data Saved!' );
-            //$link = 'index.php?option=com_driver';
-            
-		// Check the table in so it can be edited.... we are done with it anyway
-		$link = 'index.php?option=com_biblestudy&view=templateslist';
-		break;
+				// Check the table in so it can be edited.... we are done with it anyway
+				$link = 'index.php?option=com_biblestudy&view=templateslist';
+				break;
 		}
 		$this->setRedirect($link, $msg);
 	}
@@ -74,7 +87,7 @@ switch ($this->_task) {
 			if (!is_array( $cid ) || count( $cid ) < 1) {
 				JError::raiseError(500, JText::_( 'Select an item to unpublish' ) );
 			}
-	
+
 			$model = $this->getModel('templateedit');
 			if(!$model->publish($cid, 0)) {
 				echo "<script> alert('".$model->getError(true)."'); window.history.go(-1); </script>\n";
