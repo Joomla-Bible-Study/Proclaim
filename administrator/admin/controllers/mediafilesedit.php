e4cc9b4f65d776409a4c7481b522ff2c35579525
bcordis <bcordis@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2010-10-05 19:03:49 +0000

    Added file size lookup
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@1016 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- administrator/admin/controllers/mediafilesedit.php
+++ administrator/admin/controllers/mediafilesedit.php
@@ -376,4 +376,15 @@ function resetPlays()
 			}
 	}
 }
+//New File Size System
+function getSizeFile($url) {
+    if (substr($url,0,4)=='http') {
+        $x = array_change_key_case(get_headers($url, 1),CASE_LOWER);
+        if ( strcasecmp($x[0], 'HTTP/1.1 200 OK') != 0 ) { $x = $x['content-length'][1]; }
+        else { $x = $x['content-length']; }
+    }
+    else { $x = @filesize($url); }
+
+    return $x;
+}
 ?>
