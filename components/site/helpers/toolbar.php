92423dbd02191b53636c4416023811bb9f9667e5
santon <santon@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2010-12-07 17:40:47 +0000

    copy toolsbar.php from admin
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@1159 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

---
+++ components/site/helpers/toolbar.php
@@ -0,0 +1,21 @@
+<?php
+defined('_JEXEC') or die();
+jimport('joomla.html.toolbar');
+ 
+ class biblestudyHelperToolbar extends JObject
+ {        
+        function getToolbar() {
+ 
+ 				$directory='images';
+                $bar = new JToolBar( 'Toolbar' );
+                $bar->appendButton( 'Standard', 'save', 'Save', 'save', false );
+                $bar->appendButton( 'Standard', 'cancel', 'Cancel', 'cancel', false );
+ 				$toolview = JRequest::getVar('view');
+ 				if ($toolview == 'mediafilesedit')
+				 {$bar->appendButton( 'Popup', 'upload', 'Upload', "index.php?option=com_media&tmpl=component&task=popupUpload&directory=$directory", 600, 400 );}
+ 
+                return $bar->render();
+ 
+        }
+ 
+ }
