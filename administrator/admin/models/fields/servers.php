3eb6e1a21af26a52f66c19b6337c224380f827bc
genu <genu@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2010-12-29 10:29:21 +0000

    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@1247 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

---
+++ administrator/admin/models/fields/servers.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @version		$Id: filters.php 19730 2010-12-02 13:06:49Z chdemko $
+ * @package		Joomla.Framework
+ * @subpackage	Form
+ * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('JPATH_BASE') or die;
+
+jimport('joomla.html.html');
+jimport('joomla.access.access');
+jimport('joomla.form.formfield');
+
+/**
+ * Form Field class for the Joomla Framework.
+ *
+ * @package		Joomla.Framework
+ * @subpackage	Form
+ * @since		1.6
+ */
+class JFormFieldServers extends JFormField
+{
+	public $type = 'Servers';
+
+	protected function getInput()
+	{
+		$db = JFactory::getDBO();
+		$db->setQuery("SELECT id as value, server_name as text FROM #__bsms_servers WHERE published=1 ORDER BY server_name ASC");
+		$html[] = JHTML::_('select.option', '0', '- '. JText::_('JBS_CMN_SELECT_SERVER') .' -' );
+		$html = array_merge($html, $db->loadObjectList());
+		return JHTML::_('select.genericlist', $html, 'study_id', 'class="inputbox" size="1" ', 'value', 'text', 0);
+	}
+}
+
+
