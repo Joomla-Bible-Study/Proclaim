ce3f6b2f4fc13cbd319ca99f3135bc7222edd20a
genu <genu@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2011-01-04 22:46:55 +0000

    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@1302 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- administrator/admin/views/mediafileslist/tmpl/default_16.php
+++ administrator/admin/views/mediafileslist/tmpl/default_16.php
@@ -7,93 +7,113 @@
 
 //No Direct Access
 defined('_JEXEC') or die();
+
+JHtml::_('script', 'system/multiselect.js', false, true);
+$listOrder = $this->state->get('list.ordering');
+$listDirn = $this->state->get('list.direction');
+$saveOrder = $listOrder == 'mediafile.ordering';
 ?>
 
-<form
-	action="<?php echo $this->request_url; ?>" method="post"
-	name="adminForm"><?php //echo $this->lists['studyid']; ?>
-<div id="editcell">
-<table class="adminlist">
-	<thead>
-		<tr>
-			<th width="5"><?php echo JText::_( 'JBS_CMN_ROW' ); ?></th>
-			<th width="20"><input type="checkbox" name="toggle" value=""
-				onclick="checkAll(<?php echo count( $this->items ); ?>);" /></th>
+<form action="<?php echo JRoute::_('index.php?option=com_biblestudy&view=mediafileslist'); ?>" method="post" name="adminForm" id="adminForm">
+    <fieldset id="filter-bar">
+        <div class="filter-search fltlft">
+            <label class="filter-search-lbl" for="filter_search">Filter here</label>
+        </div>
+    </fieldset>
+    <div class="clr"></div>
 
-			<th width="20" align="center"><?php echo JHTML::_('grid.sort',  'JBS_CMN_PUBLISHED', 'published', $this->lists['order_Dir'], $this->lists['order'] ); ?>
-			</th>
-			<th width="20"><?php echo JHTML::_('grid.sort',  'JBS_CMN_ID', 'id', $this->lists['order_Dir'], $this->lists['order'] ); ?>
-			</th>
-			<th width="8%" nowrap="nowrap"><?php echo JHTML::_('grid.sort',  'JBS_CMN_ORDER', 'ordering', $this->lists['order_Dir'], $this->lists['order'] ); ?>
-			<?php echo JHTML::_('grid.order',  $this->items ); ?></th>
-                        <th align="left"><?php echo JHTML::_('grid.sort',  'JBS_MED_FILENAME', 'filename', $this->lists['order_Dir'], $this->lists['order'] ); ?>
-			</th>
-			<th><?php  echo JHTML::_('grid.sort',  'JBS_CMN_STUDY_TITLE', 'studytitle', $this->lists['order_Dir'], $this->lists['order'] ); ?></th>
-			<th><?php echo JHTML::_('grid.sort',  'JBS_MED_MEDIA_TYPE', 'media_image_name', $this->lists['order_Dir'], $this->lists['order'] ); ?>
-			</th>
-			<th><?php echo JHTML::_('grid.sort',  'JBS_CMN_MEDIA_CREATE_DATE', 'createdate', $this->lists['order_Dir'], $this->lists['order'] ); ?>
-			</th>
-            	<th><?php echo JHTML::_('grid.sort',  'JBS_CMN_PLAYS', 'plays', $this->lists['order_Dir'], $this->lists['order'] ); ?>
-			</th>
-            	<th><?php echo JHTML::_('grid.sort',  'JBS_CMN_DOWNLOADS', 'downloads', $this->lists['order_Dir'], $this->lists['order'] ); ?>
-			</th>
-		</tr>
-	</thead>
-	<?php
-	$k = 0;
-	for ($i=0, $n=count( $this->items ); $i < $n; $i++)
-	{
-		$row = &$this->items[$i];
-		$checked 	= JHTML::_('grid.id',   $i, $row->id );
-		$link 		= JRoute::_( 'index.php?option=com_biblestudy&controller=mediafilesedit&task=edit&id='. $row->id );
-		$published 	= JHTML::_('grid.published', $row, $i );
-		$ordering = ($this->lists['order'] == 'ordering');
-		?>
-	<tr class="<?php echo "row$k"; ?>">
-		<td><?php echo $this->pagination->getRowOffset( $i ); ?></td>
-		<td width="20"><?php echo $checked; ?></td>
-		<td align="center" width="20"><?php echo $published; ?></td>
-		<td width="20"><?php echo $row->id; ?></td>
-
-		<td width="8%" class="order">
-		<span><?php echo $this->pagination->orderUpIcon( $i, $row->study_id == @$this->items[$i-1]->study_id, 'orderup', 'Move Up', $ordering ); ?></span>
-		<span><?php echo $this->pagination->orderDownIcon( $i, $n, ($row->study_id == @$this->items[$i+1]->study_id), 'orderdown', 'Move Down', $ordering ); ?></span>
-		<?php $disabled = $ordering ?  '' : 'disabled="disabled"'; ?>
-		<input
-			type="text" name="order[]" size="5"
-			value="<?php echo $row->ordering;?>" <?php echo $disabled ?>
-			class="text_area" style="text-align: center"
-		/>
-		</td>
-
-		<td><a href="<?php echo $link; ?>"><?php if (!$row->filename) { echo $row->createdate;} else {echo $row->filename;} ?></a>
-		</td>
-		<td><?php echo $row->studytitle; ?></td>
-		<td><?php echo $row->media_image_name; ?></td>
-		<td><?php echo $row->createdate; ?></td>
-        <td><?php echo $row->plays; ?></td>
-        <td><?php echo $row->downloads; ?></td>
-
-	</tr>
-	<?php
-	$k = 1 - $k;
-	}
-	?>
-
-	<tfoot>
-		<tr>
-			<td colspan="10"><?php echo $this->pagination->getListFooter(); ?></td>
-		</tr>
-	</tfoot>
-</table>
-
-
-</div>
-<!--<table>    <tr><td>Pagination: <?php //print_r($this->pagination);?></td></tr></table>-->
-	<input type="hidden" name="option" value="com_biblestudy" />
-	<input type="hidden" name="task" value="" />
-	<input type="hidden" name="boxchecked" value="0" />
-	<input type="hidden" name="controller" value="mediafilesedit" />
-	<input type="hidden" name="filter_order" value="<?php echo $this->lists['order']; ?>" />
-	<input type="hidden" name="filter_order_Dir" value="<?php echo $this->lists['order_Dir']; ?>" />
+    <table class="adminlist">
+        <thead>
+            <tr>
+                <th width="1%">
+                    <input type="checkbox" name="checkall-toggle" value="" onclick="checkAll(this)"/>
+                </th>
+                <th width="5%">
+                    <?php echo JHtml::_('grid.sort', 'JPUBLISHED', 'mediafile.published', $listDirn, $listOrder); ?>
+                </th>
+                <th width="10%">
+                    <?php
+                        echo JHtml::_('grid.sort', 'JGRID_HEADING_ORDERING', 'mediafile.ordering', $listDirn, $listOrder);
+                        echo JHtml::_('grid.order', $this->items, 'filesave.png', 'mediafileslist.saveorer');
+                    ?>
+                </th>
+                <th width="20%">
+                    <?php echo JHtml::_('grid.sort', 'JBS_MED_FILENAME', 'mediafile.filename', $listDirn, $listOrder); ?>
+                </th>
+                <th width="20%">
+                    <?php echo JHtml::_('grid.sort', 'JBS_CMN_STUDY_TITLE', 'study.studytitle', $listDirn, $listOrder); ?>
+                </th>
+                <th width="20%">
+                    <?php echo JHtml::_('grid.sort', 'JBS_MED_MEDIA_TYPE', 'mediaType.media_text', $listDirn, $listOrder); ?>
+                </th>
+                <th width="15%">
+                    <?php echo JHtml::_('grid.sort', 'JBS_CMN_MEDIA_CREATE_DATE', 'mediafile.createdate', $listDirn, $listOrder); ?>
+                </th>
+                <th width="5%">
+                    <?php echo JHtml::_('grid.sort', 'JBS_CMN_PLAYS', 'mediafile.plays', $listDirn, $listOrder); ?>
+                </th>
+                <th width="5%">
+                    <?php echo JHtml::_('grid.sort', 'JBS_CMN_DOWNLOADS', 'mediafile.downloads', $listDirn, $listOrder); ?>
+                </th>
+            </tr>
+        </thead>
+        <tfoot>
+            <tr>
+                <td colspan="9">
+                    <?php echo $this->pagination->getListFooter(); ?>
+                </td>
+            </tr>
+        </tfoot>
+        <?php
+        foreach($this->items as $i => $item) :
+            $ordering = ($listOrder == 'mediafile.ordering');
+        ?>
+        <tr class="row<?php echo $i % 2; ?>">
+            <td class="center">
+                <?php echo JHtml::_('grid.id', $i, $item->id); ?>
+            </td>
+            <td class="center">
+                <?php echo JHtml::_('jgrid.published', $item->published, $i, 'mediafileslist', true, 'cb', '', ''); ?>
+            </td>
+            <td class="order">
+                <?php if($listDirn == 'asc') : ?>
+                    <span><?php echo $this->pagination->orderUpIcon($i, ($item->studytitle == @$this->items[$i-1]->studytitle), 'mediafileslist.orderup', 'JLIB_HTML_MOVE_UP', $ordering); ?></span>
+                    <span><?php echo $this->pagination->orderDownIcon($i, $this->pagination->total, ($item->studytitle == @$this->items[$i-1]->studytitle), 'mediafileslist.orderdown', 'JLIB_HTML_MOVE_DOWN', $ordering); ?></span>
+                <?php elseif ($listDirn == 'desc') : ?>
+                    <span><?php echo $this->pagination->orderUpIcon($i, ($item->studytitle == @$this->items[$i-1]->studytitle), 'mediafileslist.orderup', 'JLIB_HTML_MOVE_DOWN', $ordering); ?></span>
+                    <span><?php echo $this->pagination->orderDownIcon($i, $this->pagination->total, ($item->studytitle == @$this->items[$i-1]->studytitle), 'mediafileslist.orderdown', 'JLIB_HTML_MOVE_UP', $ordering); ?></span>
+                <?php endif; ?>
+                <?php $disabled = $saveOrder ?  '' : 'disabled="disabled"'; ?>
+		<input type="text" name="order[]" size="5" value="<?php echo $item->ordering;?>" <?php echo $disabled ?> class="text-area-order" />
+            </td>
+            <td class="center">
+                <a href="<?php echo JRoute::_('index.php?option=com_biblestudy&task=mediafilesedit.edit&id='.(int)$item->id); ?>">
+                    <?php echo $this->escape($item->filename); ?>
+                </a>
+            </td>
+            <td class="center">
+                <?php echo $this->escape($item->studytitle); ?>
+            </td>
+            <td class="center">
+                <?php echo $this->escape($item->mediaType); ?>
+            </td>
+            <td class="center">
+                <?php echo JHtml::_('date', $item->createdate, JText::_('DATE_FORMAT_LC4')); ?>
+            </td>
+            <td class="center">
+                <?php echo $this->escape($item->plays);?>
+            </td>
+            <td class="center">
+                <?php echo $this->escape($item->downloads);?>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+    <div>
+        <input type="hidden" name="task" value=""/>
+        <input type="hidden" name="boxchecked" value="0"/>
+        <input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>"/>
+        <input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>"/>
+        <?php echo JHtml::_('form.token'); ?>
+    </div>
 </form>
