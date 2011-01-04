b0fd68dff7438cffc7c75f36705186d2b75f5317
genu <genu@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2011-01-04 08:01:06 +0000

    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@1287 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- administrator/admin/views/mediafilesedit/viewj15.html.php
+++ administrator/admin/views/mediafilesedit/viewj15.html.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * @version     $Id$
+ * @package     com_biblestudy
+ * @license     GNU/GPL
+ */
+
+//No Direct Access
 defined('_JEXEC') or die();
 
 jimport( 'joomla.application.component.view' );
