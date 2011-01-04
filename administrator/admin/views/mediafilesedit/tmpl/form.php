29908e1eafe2d75faf8f551a7a00d10de09863e4
genu <genu@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2011-01-04 22:55:14 +0000

    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@1306 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

---
+++ administrator/admin/views/mediafilesedit/tmpl/form.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @version     $Id$
+ * @package     com_biblestudy
+ * @license     GNU/GPL
+ */
+
+//No Direct Access
+defined('_JEXEC') or die();
+
+require_once (JPATH_ROOT . DS . 'administrator' . DS . 'components' . DS . 'com_biblestudy' . DS . 'lib' . DS . 'biblestudy.defines.php');
+if (JOOMLA_VERSION == '5') {
+    echo $this->loadTemplate('15');
+} else {
+    echo $this->loadTemplate('16');
+}
+?>
