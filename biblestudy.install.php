8d8592561e87db7ad746928e3d9dec658599e5e4
bcordis <bcordis@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2011-01-07 03:00:54 +0000

    Update to Install Trunk
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@1338 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- biblestudy.install.php
+++ biblestudy.install.php
@@ -4,80 +4,74 @@
  * Bible Study Component
  * @package Bible Study
  *
-* @Copyright (C) 2007 - 2010 Joomla Bible Study Team All rights reserved
-* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
-* @link http://www.JoomlaBibleStudy.org
-*
-* Install Based on Kunena Component
-
+ * @Copyright (C) 2007 - 2010 Joomla Bible Study Team All rights reserved
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link http://www.JoomlaBibleStudy.org
+ *
+ * Install Based on Kunena Component
  **/
-//
-// Dont allow direct linking
-defined( '_JEXEC' ) or die('Restricted access');
+ //
+ // Dont allow direct linking
+ defined( '_JEXEC' ) or die('Restricted access');
 ?>
-	<script language=JavaScript>
-			function showDetail(srcElement) {
-				var targetID, srcElement, targetElement, imgElementID, imgElement;
-				targetID = srcElement.id + "_details";
-				imgElementID = srcElement.id + "_img";
-
-				targetElement = document.getElementById(targetID);
-				imgElement = document.getElementById(imgElementID);
-				if (targetElement.style.display == "none") {
-					targetElement.style.display = "";
-					imgElement.src = "images/collapseall.png";
-				} else {
-					targetElement.style.display = "none";
-					imgElement.src = "images/expandall.png";
-				}
-			}
-			</script>
-			<style>
-			.details {
-				font-family: courier;
-				background-color: #EEEEEE;
-				border: 1px dashed #BBBBBB;
-				padding-left: 10px;
-				margin-left: 20px;
-				margin-top: 5px;
-.fbscs {
-	margin: 0;
-	padding: 0;
-	list-style: none;
-}
-
-.fbscslist {
-	list-style: none;
-	padding: 5px 10px;
-	margin: 3px 0;
-	border: 1px solid #66CC66;
-	background: #D6FEB8;
-	display: block;
-	font-family: Arial, Helvetica, sans-serif;
-	font-size: 12px;
-	color: #333;
-}
-
-.fbscslisterror {
-	list-style: none;
-	padding: 5px 10px;
-	margin: 3px 0;
-	border: 1px solid #FF9999;
-	background: #FFCCCC;
-	display: block;
-	font-family: Arial, Helvetica, sans-serif;
-	font-size: 12px;
-	color: #333;
-}
+<script language=JavaScript>
+	function showDetail(srcElement) {
+	var targetID, srcElement, targetElement, imgElementID, imgElement;
+	targetID = srcElement.id + "_details";
+	imgElementID = srcElement.id + "_img";
+	targetElement = document.getElementById(targetID);
+	imgElement = document.getElementById(imgElementID);
+	if (targetElement.style.display == "none") {
+	targetElement.style.display = "";
+	imgElement.src = "images/collapseall.png";
+	} else {
+	targetElement.style.display = "none";
+	imgElement.src = "images/expandall.png";
+	}
+	}
+	</script>
+	<style type="text/css">
+	.details {
+		font-family: courier;
+		background-color: #EEEEEE;
+		border: 1px dashed #BBBBBB;
+		padding-left: 10px;
+		margin-left: 20px;
+		margin-top: 5px;
+	.fbscs {
+		margin: 0;
+		padding: 0;
+		list-style: none;
+	}
+	.fbscslist {
+		list-style: none;
+		padding: 5px 10px;
+		margin: 3px 0;
+		border: 1px solid #66CC66;
+		background: #D6FEB8;
+		display: block;
+		font-family: Arial, Helvetica, sans-serif;
+		font-size: 12px;
+		color: #333;
+	}
+	.fbscslisterror {
+		list-style: none;
+		padding: 5px 10px;
+		margin: 3px 0;
+		border: 1px solid #FF9999;
+		background: #FFCCCC;
+		display: block;
+		font-family: Arial, Helvetica, sans-serif;
+		font-size: 12px;
+		color: #333;
+	}
 </style>
 <?php
 @error_reporting(E_ERROR | E_WARNING | E_PARSE);
-
 // Help get past php timeouts if we made it that far
 // Joomla 1.5 installer can be very slow and this helps avoid timeouts
 @set_time_limit(3600);
 $kn_maxTime = @ini_get('max_execution_time');
-
 $maxMem = trim(@ini_get('memory_limit'));
 if ($maxMem) {
 	$unit = strtolower($maxMem{strlen($maxMem) - 1});
@@ -100,44 +94,34 @@ if ($maxMem) {
 	}
 }
 ignore_user_abort(true);
-
 // Bible Study wide defines
 require_once (JPATH_ROOT  .DS. 'components' .DS. 'com_biblestudy' .DS. 'lib' .DS. 'biblestudy.defines.php');
-
 include_once(BIBLESTUDY_PATH_ADMIN_LIB .DS. 'fx.upgrade.class.php');
 // Install Bible Study Component
 function com_install()
 {
 	$biblestudy_db = JFactory::getDBO();
-
 	// Determine MySQL version from phpinfo
 	$biblestudy_db->setQuery("SELECT VERSION() as mysql_version");
 	$mysqlversion = $biblestudy_db->loadResult();
-
 	//before we do anything else we want to check for minimum system requirements
 	if (version_compare(phpversion(), BIBLESTUDY_MIN_PHP, ">=") && version_compare($mysqlversion, BIBLESTUDY_MIN_MYSQL, ">"))
+	{				
+	//install & upgrade class
+	$bsmsupgrade = new fx_Upgrade("com_biblestudy", "biblestudy.install.upgrade.xml", "bsms_", "install", false);
+	// Start Installation/Upgrade
+	$bsmsupgrade->doUpgrade();
+	//For some reason in version 7.0 the last phpfile doesn't run so we check here
+	$query = "SELECT `player` FROM #__bsms_mediafiles WHERE `player` IS NOT NULL";
+	$biblestudy_db->setQuery($query);
+	$biblestudy_db->query();
+	$results = $biblestudy_db->loadObjectList();
+	$count = count($results);
+	if (!$count)
 	{
-				
-		
-
-
-
-		//install & upgrade class
-		$bsmsupgrade = new fx_Upgrade("com_biblestudy", "biblestudy.install.upgrade.xml", "bsms_", "install", false);
-
-		// Start Installation/Upgrade
-		$bsmsupgrade->doUpgrade();
-  //For some reason in version 7.0 the last phpfile doesn't run so we check here
-  $query = "SELECT `player` FROM #__bsms_mediafiles WHERE `player` IS NOT NULL";
-  $biblestudy_db->setQuery($query);
-  $biblestudy_db->query();
-  $results = $biblestudy_db->loadObjectList();
-  $count = count($results);
-  if (!$count)
-  {
-    include_once(JPATH_ADMINISTRATOR.DS.'components'.DS.'com_biblestudy'.DS.'install'.DS.'biblestudy.700.upgrade.php');
-  }   
-  //Check for presence of css or backup
+		include_once(JPATH_ADMINISTRATOR.DS.'components'.DS.'com_biblestudy'.DS.'install'.DS.'biblestudy.700.upgrade.php');
+	}   
+	//Check for presence of css or backup
     jimport('joomla.filesystem.file');
     $src = JPATH_SITE.DS.'components'.DS.'com_biblestudy'.DS.'assets'.DS.'css'.DS.'biblestudy.css.dist';
     $dest = JPATH_SITE.DS.'components'.DS.'com_biblestudy'.DS.'assets'.DS.'css'.DS.'biblestudy.css';
@@ -164,20 +148,15 @@ function com_install()
         }
     }    
     }
-
 	?>
-<br />
-
 <div style="border: 1px solid #ccc; background: #FBFBFB; padding: 10px; text-align: left; margin: 10px 0;">
-<table width="100%" border="0" cellpadding="0" cellspacing="0">
+<table width="100%" border="0" cellpadding="0" cellspacing="0" summary="">
 	<tr>
 		<td width="10%" valign="top" style="padding: 10px;"><a
 			href="index2.php?option=com_biblestudy"><img
 			src="components/com_biblestudy/images/openbible.png" alt="Bible Study"
 			border="0"></a></td>
-
 		<td width="90%" valign="top" style="padding: 10px;">
-		
 		<div
 			style="border: 1px solid #FFCC99; background: #FFFFCC; padding: 20px; margin: 20px; clear: both;">
 		<strong>I N S T A L L : <font color="green">Successful</font> </strong>
@@ -187,14 +166,16 @@ function com_install()
 		<br />
 		<strong>mysql version: <font color="green"><?php echo $mysqlversion; ?></font> (Required &gt; <?php echo BIBLESTUDY_MIN_MYSQL; ?>)</strong>
 		</div>
-
-</div>		<?php
+		</td>
+	</tr>
+	</table>
+</div>
+<?php
 	}
 	else
 	{
 		// Minimum version requirements not satisfied
 		?>
-
 <div style="border: 1px solid #ccc; background: #FBFBFB; padding: 10px; text-align: left; margin: 10px 0;">
 <table width="100%" border="0" cellpadding="0" cellspacing="0">
 	<tr>
@@ -202,9 +183,7 @@ function com_install()
 			href="index2.php?option=com_biblestudy"><img
 			src="components/com_biblestudy/images/openbible.png" alt="Bible Study Management System"
 			border="0"></a></td>
-
 		<td width="80%" valign="top" style="padding: 10px;">
-
 		<div
 			style="border: 1px solid #FFCC99; background: #FFFFCC; padding: 20px; margin: 20px; clear: both;">
 		<strong>I N S T A L L : <font color="red">F A I L E D - Minimum Version Requirements not satisfied</font> </strong>
@@ -213,26 +192,19 @@ function com_install()
 		<strong>php version: <font color="<?php echo version_compare(phpversion(), BIBLESTUDY_MIN_PHP, '>=')?'green':'red'; ?>"><?php echo phpversion(); ?></font> (Required &gt;= <?php echo BIBLESTUDY_MIN_PHP; ?>)</strong>
 		<br />
 		<strong>mysql version: <font color="<?php echo version_compare($mysqlversion, BIBLESTUDY_MIN_MYSQL, '>')?'green':'red'; ?>"><?php echo $mysqlversion; ?></font> (Required &gt; <?php echo BIBLESTUDY_MIN_MYSQL; ?>)</strong>
-  	</div>
-
-		<?php
-	}
-    
-
-
-    
-  
+		</td>
+	</tr>
+</table>
+ 	</div>
+<?php
+	}  
 	// Rest of footer
-	?>
-		<div
-			style="border: 1px solid #99CCFF; background: #D9D9FF; padding: 20px; margin: 20px; clear: both;">
-		<strong>Thank you for using Joomla Bible Study!</strong> <br />
-<?php 
-$mainframe =& JFactory::getApplication(); ?>
-
+?>
+<div style="border: 1px solid #99CCFF; background: #D9D9FF; padding: 20px; margin: 20px; clear: both;">
+<strong>Thank you for using Joomla Bible Study!</strong>
+<br />
+<?php $mainframe =& JFactory::getApplication(); ?>
 <img src = "<?php echo $mainframe->getCfg("live_site"); ?>/components/com_biblestudy/images/openbible.png" alt = "Joomla Bible Study" title="Joomla Bible Study" border = "0" /> Congratulations, Bible Study Message Manager has been installed successfully. </div>
-<p>
-
 <p>
 Welcome to Joomla Bible Study. Please note if there are any error messages above signified by a red X. You can click on that line to see what went wrong. Please copy it into your clipboard for future reference. This component is designed to help your church communicate the gospel and teachings in the Word of God. Joomla Bible Study allows you to enter detailed information about the studies given and links to multimedia content you have uploaded to your server. You can also display full text or notes. All this is searchable in many different ways and you have a lot of control over how much information is displayed on the front end. </p>
 <p>
@@ -243,18 +215,16 @@ It is very important that you do a couple of things when you first install the c
  2. Go back to Components | Bible Study. Click on the Servers and add your server. Remember, you are building a url that someone could paste into their browser - this isn't to your web root, but your web site address. Be sure to follow the instructions of how to put in that entry. Then go to the Folders link and add a folder under that server. Now you are set to add your first real study (be sure to delete the samples once you are familiar with how the component works). </p>
  <p>
  3. Click on Studies. Just put in some text to add a new study. Then go to the Media Files tab and enter a media file associated with that study. </p>
-
  <p><a href="http://www.joomlabiblestudy.org/forums.html" target="_blank">Visit our forum with your questions</a></p>
  <p><a href="http://www.joomlabiblestudy.org" target="_blank">Get more help and information at JoomlaBibleStudy.org</a></p>
  <p><a href="http://www.JoomlaBibleStudy.org/jbsdocs" target="_blank">Visit our Documentation Site</a></p>
-		Bible Study Component <em>for Joomla! </em> &copy; by <a
+		<p>Bible Study Component <em>for Joomla! </em> &copy; by <a
 			href="http://www.JoomlaBibleStudy.org" target="_blank">www.JoomlaBibleStudy.org</a>.
-		All rights reserved.</div>
+		All rights reserved.</p></div>
 		</td>
 	</tr>
 </table>
 </div>
 	<?php
-
 }
 ?>
