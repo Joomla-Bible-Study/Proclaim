830161cec07685f09949333313f58d648c12ff11
genu <genu@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2011-01-08 21:30:40 +0000

    Made the necessary files to form the views
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@1346 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

---
+++ administrator/admin/views/commentsedit/viewj16.html.php
@@ -0,0 +1,9 @@
+o<?php
+/**
+ * @version     $Id$
+ * @package     com_biblestudy
+ * @license     GNU/GPL
+ */
+
+//No Direct Access
+defined('_JEXEC') or die();
---
+++ administrator/admin/views/locationsedit/viewj16.html.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @version     $Id$
+ * @package     com_biblestudy
+ * @license     GNU/GPL
+ */
+
+//No Direct Access
+defined('_JEXEC') or die();
