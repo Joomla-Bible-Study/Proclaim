147554993d81e2ac787ad3501c03931691fc0420
bcordis <bcordis@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2011-01-30 19:50:45 +0000

    Adding j1.5 exception calls
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@1460 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- administrator/admin/admin.biblestudy.php
+++ administrator/admin/admin.biblestudy.php
@@ -7,16 +7,71 @@
 //No Direct Access
 defined('_JEXEC') or die();
 
+// Check for PHP4
+if(defined('PHP_VERSION')) {
+	$version = PHP_VERSION;
+} elseif(function_exists('phpversion')) {
+	$version = phpversion();
+} else {
+	// No version info. I'll lie and hope for the best.
+	$version = '5.0.0';
+}
+
+// Old PHP version detected. EJECT! EJECT! EJECT!
+if(!version_compare($version, '5.0.0', '>='))
+{
+	return JError::raise(E_ERROR, 500, 'PHP 4 is not supported by Joomla Bible Study');
+}
+
 define('JSTART', '$j(document).ready( function() {');
 define('JSTOP', '});');
 addLoadingDiv();
 addCSS();
 addJS();
 
+// Joomla! 1.6 detection
+jimport('joomla.filesystem.file');
+if(!version_compare( JVERSION, '1.6.0', 'ge' )) {
+	define('BIBLESTUDY_JVERSION','15');
+} else {
+	define('BIBLESTUDY_JVERSION','16');
+}
+
+if(!defined('BIBLESTUDYENGINE')) {
+	define('BIBLESTUDYENGINE', 1); // Required for accessing Akeeba Engine's factory class
+	define('BIBLESTUDYPLATFORM', 'joomla15'); // So that platform-specific stuff can get done!
+}
+
+// Setup Akeeba's ACLs, honoring laxed permissions in component's parameters, if set
+if(BIBLESTUDY_JVERSION == '15')
+{
+// Require the base controller
+require_once (JPATH_COMPONENT.DS.'controller.php');
+if($controller = JRequest::getWord('controller')) {
+	require_once (JPATH_COMPONENT.DS.'controllers'.DS.$controller.'.php');
+}
+
+$controller = JRequest::getVar( 'controller' );
+
+		
+		$classname	= 'biblestudyController'.$controller;
+		
+		$controller = new $classname( );
+		
+
+		// Perform the Request task 
+		$controller->execute( JRequest::getWord('task'));
+		//Redirect if set by the controller 
+		
+		$controller->redirect();
+}
+else
+{
 jimport('joomla.application.component.controller');
 $controller = JController::getInstance('biblestudy');
 $controller->execute(JRequest::getCmd('task'));
 $controller->redirect();
+}
 
 /**
  * Adds a loading div for any ajax requests via JQuery
