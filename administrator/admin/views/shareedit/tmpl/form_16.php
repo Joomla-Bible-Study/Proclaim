5654f2f2648df4e7c54cb5284596d38ff98e1ceb
tomfuller2 <tomfuller2@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2011-01-31 19:11:33 +0000

    Fixed Shareedit - pulled correct params over, then iterated over them in the form.
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@1463 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- administrator/admin/views/shareedit/tmpl/form_16.php
+++ administrator/admin/views/shareedit/tmpl/form_16.php
@@ -6,6 +6,7 @@
  */
 //No Direct Access
 defined('_JEXEC') or die('Restricted access'); 
+$params = $this->form->getFieldsets('params');
 ?>
 <form action="<?php echo JRoute::_('index.php?option=com_biblestudy&layout=form&id='.(int) $this->item->id); ?>" method="post" name="adminForm" id="adminForm">
 	<div class="width-60 fltlft">
@@ -27,6 +28,19 @@ defined('_JEXEC') or die('Restricted access');
 			</ul>
 		</fieldset>
 	</div>
+    <div class="width-35 fltrt">
+       <fieldset class="panelform" >
+       <legend><?php echo JText::_('JBS_CMN_PARAMETERS'); ?></legend>
+       
+           <ul class="adminformlist">
+           <?php foreach ($params as $name => $fieldset): ?>
+                <?php foreach ($this->form->getFieldset($name) as $field) : ?>
+                                <li><?php echo $field->label; ?><?php echo $field->input; ?></li>
+                <?php endforeach; ?>
+           <?php endforeach; ?>
+           </ul>
+       </fieldset>
+     </div>
   <input type="hidden" name="task" value="" />
   <?php echo JHtml::_('form.token'); ?>
 </form>
