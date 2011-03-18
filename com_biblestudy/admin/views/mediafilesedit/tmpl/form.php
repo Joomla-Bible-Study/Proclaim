26f8d063b6bc73715f09b654e9838b96fd0d9da1
tomfuller2 <tomfuller2@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2011-03-18 18:54:52 +0000

    Rules working now on mediafilesedit
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@1656 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- com_biblestudy/admin/views/mediafilesedit/tmpl/form.php
+++ com_biblestudy/admin/views/mediafilesedit/tmpl/form.php
@@ -171,7 +171,22 @@ $params = $this->form->getFieldsets('params');
 				<?php endforeach; ?>
         <?php echo JHtml::_('sliders.end'); ?>
 		</div>
+<div class="clr"></div>
+	
+	<?php if ($this->canDo->get('core.admin')): ?>
+		<div class="width-100 fltlft">
+			<?php echo JHtml::_('sliders.start','permissions-sliders-'.$this->item->id, array('useCookie'=>1)); ?>
 
+				<?php echo JHtml::_('sliders.panel',JText::_('JBS_CMN_FIELDSET_RULES'), 'access-rules'); ?>
+
+				<fieldset class="panelform">
+					<?php echo $this->form->getLabel('rules'); ?>
+					<?php echo $this->form->getInput('rules'); ?>
+				</fieldset>
+
+			<?php echo JHtml::_('sliders.end'); ?>
+		</div>
+	<?php endif; ?>
 		<input type="hidden" name="task" value="" />
     <?php echo JHtml::_('form.token'); ?>
 </form>
