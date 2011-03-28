6dccccda4f30f231d4c91af89cd5c85670465aab
tomfuller2 <tomfuller2@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2011-03-28 02:15:48 +0000

    Working on ACL assets migration. Just testing at this point. Problems have occured.
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@1682 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- com_biblestudy/admin/install/biblestudy.assets.php
+++ com_biblestudy/admin/install/biblestudy.assets.php
@@ -11,7 +11,21 @@ defined('_JEXEC') or die();
 
 class fixJBSAssets
 {
-    public function JBSAsset()
+    public function AssetEntry()
+    {
+        $objects = array(array(name=>'#__bsms_servers',titlefield=>'server_name',assetname=>'serversedit'),
+                        array(name=>'#__bsms_folders',titlefield=>'foldername',assetname=>'foldersedit'));
+
+        foreach ($objects AS $object)
+        {
+            $name = $object['name'];
+            $titlefield = $object['titlefield'];
+            $assetname =  $object['assetname'];
+            $doAsset = $this->JBSAsset($name, $titlefield, $assetname);
+        }
+        if ($doAsset){return true;}else{return false;}
+    }
+    public function JBSAsset($name, $titlefield, $assetname)
     {
         $jconfig = new JConfig();
 
@@ -22,40 +36,42 @@ class fixJBSAssets
 		$this->config['database'] = $jconfig->db;
 		$this->config['prefix']   = $jconfig->dbprefix;
         $this->db_new = JDatabase::getInstance($this->config);
+        
         $db = JFactory::getDBO();
         
         // Getting the asset table
-	//	$table = JTable::getInstance('Asset', 'JTable', array('dbo' => $this->db_new));
+	
         $query = "SELECT id FROM #__assets WHERE name = 'com_biblestudy'";
         $db->setQuery($query);
         $db->query();
         $parent_id = $db->loadResult();
         
         
-        $query = "SELECT id, server_name FROM #__bsms_servers";
+        $query = 'SELECT id, '.$titlefield.' AS ta FROM '.$name;
         $db->setQuery($query);
         $db->query();
-        $servers = $db->loadObjectList();
-        
-        foreach ($servers AS $server)
+        $oldtables = $db->loadObjectList(); 
+        if ($oldtables)
         {
-            $table = JTable::getInstance('Asset', 'JTable', array('dbo' => $this->db_new));
-            $table->name = 'com_biblestudy.serversedit.'.$server->id;
-            $table->parent_id = $parent_id;
-            $table->level = 2;
-            $table->rules = '{"core.create":[],"core.delete":[],"core.edit":[],"core.edit.state":[],"core.edit.own":[]}';
-            $table->title = mysql_real_escape_string($server->server_name);
-            
-            
-          //  echo $parent_id; echo $table->level;
-           	$table->store();
-         //print_r($table);
-           	$query = "UPDATE #__bsms_servers SET asset_id = {$table->id}"
-		." WHERE id = {$server->id}";
-		$this->db_new->setQuery($query);
-		$this->db_new->query();
-    
-        } 
+            foreach ($oldtables AS $oldtable)
+            {
+                $table = JTable::getInstance('Asset', 'JTable', array('dbo' => $this->db_new));
+                $table->name = 'com_biblestudy.'.$assetname.'.'.$oldtable->id;
+                $table->parent_id = $parent_id;
+                $table->level = '2';
+                $table->rules = '{"core.create":[],"core.delete":[],"core.edit":[],"core.edit.state":[],"core.edit.own":[]}';
+                $table->title = mysql_real_escape_string($oldtable->ta);
+        
+               	$table->store();
+             
+            $query = 'UPDATE '.$name.' SET asset_id = '.$table->id.' WHERE id = '.$oldtable->id;
+    		$this->db_new->setQuery($query);
+    		$this->db_new->query();
+        
+            }
+        }
+        else {return false;} 
+        return true;
     }
 }
 
--- com_biblestudy/admin/views/commentslist/tmpl/default.php
+++ com_biblestudy/admin/views/commentslist/tmpl/default.php
@@ -8,50 +8,13 @@
 //No Direct Access
 defined('_JEXEC') or die('Restricted access');
 
-        $jconfig = new JConfig();
+        
+require_once (JPATH_ADMINISTRATOR .DS. 'components' .DS. 'com_biblestudy' .DS. 'install' .DS. 'biblestudy.assets.php');
 
-		$this->config['driver']   = 'mysql';
-		$this->config['host']     = $jconfig->host;
-		$this->config['user']     = $jconfig->user;
-		$this->config['password'] = $jconfig->password;
-		$this->config['database'] = $jconfig->db;
-		$this->config['prefix']   = $jconfig->dbprefix;
-        $this->db_new = JDatabase::getInstance($this->config);
-        $db = JFactory::getDBO();
-        
-        // Getting the asset table
-	//	$table = JTable::getInstance('Asset', 'JTable', array('dbo' => $this->db_new));
-        $query = "SELECT id FROM #__assets WHERE name = 'com_biblestudy'";
-        $db->setQuery($query);
-        $db->query();
-        $parent_id = $db->loadResult();
-        
-        
-        $query = "SELECT id, server_name FROM #__bsms_servers";
-        $db->setQuery($query);
-        $db->query();
-        $servers = $db->loadObjectList();
-        
-        foreach ($servers AS $server)
-        {
-            $table = JTable::getInstance('Asset', 'JTable', array('dbo' => $this->db_new));
-            $table->name = 'com_biblestudy.serversedit.'.$server->id;
-            $table->parent_id = $parent_id;
-            $table->level = 2;
-            $table->rules = '{"core.create":[],"core.delete":[],"core.edit":[],"core.edit.state":[],"core.edit.own":[]}';
-            $table->title = mysql_real_escape_string($server->server_name);
-            
-            
-          //  echo $parent_id; echo $table->level;
-           	$table->store();
-         //print_r($table);
-           	$query = "UPDATE #__bsms_servers SET asset_id = {$table->id}"
-		." WHERE id = {$server->id}";
-		$this->db_new->setQuery($query);
-		$this->db_new->query();
-    
-        } 
-        
+$assetfix = new fixJBSAssets();
+$assetdofix = $assetfix->AssetEntry();
+      //  if ($assetdofix){echo 'it worked.';}else{echo 'if failed';}
+      print_r($assetdofix);
 require_once (JPATH_ADMINISTRATOR  .DS. 'components' .DS. 'com_biblestudy' .DS. 'lib' .DS. 'biblestudy.defines.php');
  ?>
 <form action="<?php echo JRoute::_('index.php?option=com_biblestudy&view=commentslist'); ?>" method="post" name="adminForm" id="adminForm">
