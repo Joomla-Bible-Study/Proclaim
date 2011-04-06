721bc4d2b2153ae00bee0256f1a1435418d25cdb
santon <santon@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2011-04-06 19:10:47 +0000

    bugfix formatting of studieslist
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@1722 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- com_biblestudy/admin/views/studieslist/tmpl/default.php
+++ com_biblestudy/admin/views/studieslist/tmpl/default.php
@@ -12,7 +12,7 @@ $listOrder = $this->state->get('list.ordering');
 $listDirn = $this->state->get('list.direction');
 ?>
 <form action="<?php echo JRoute::_('index.php?option=com_biblestudy&view=studieslist'); ?>" method="post" name="adminForm" id="adminForm">
-    <fieldset id="filter-bar">
+    <fieldset id="filter">
         <div class="filter-search fltlft">
             <label class="filter-search-lbl" for="filter_studytitle"><?php echo JText::_('JBS_CMN_STUDY_TITLE'); ?>: </label>
             <input type="text" name="filter_studytitle" id="filter_studytitle" value="<?php echo $this->escape($this->state->get('filter.studytitle')); ?>" title="<?php echo JText::_('JBS_CMN_FILTER_SEARCH_DESC'); ?>" />
@@ -20,7 +20,7 @@ $listDirn = $this->state->get('list.direction');
             <button type="submit" class="btn"><?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?></button>
             <button type="button" onclick="document.id('filter_studytitle').value='';this.form.submit();"><?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?></button>
         </div>
-        <div class="filter-select fltlft">
+        <div class="filter-select fltrt">
             <select name="filter_book" class="inputbox" onchange="this.form.submit()">
                 <option value=""><?php echo JText::_('JBS_CMN_SELECT_BOOK'); ?></option>
                 <?php echo JHtml::_('select.options', $this->books, 'value', 'text', $this->state->get('filter.book')); ?>
@@ -49,11 +49,9 @@ $listDirn = $this->state->get('list.direction');
 				<option value=""><?php echo JText::_('JOPTION_SELECT_PUBLISHED');?></option>
 				<?php echo JHtml::_('select.options', JHtml::_('jgrid.publishedOptions'), 'value', 'text', $this->state->get('filter.published'), true);?>
 			</select>
-           
         </div>
-
     </fieldset>
-    
+    <div class="clr"></div>
     <table class="adminlist">
         <thead>
             <tr>
