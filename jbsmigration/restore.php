5c3a696106c0b5ae8401579b5e35c523e870e52c
tomfuller2 <tomfuller2@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2011-04-13 23:09:22 +0000

    Fixed problem in migration with timeset table not having an id field.
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@1741 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- jbsmigration/restore.php
+++ jbsmigration/restore.php
@@ -166,14 +166,16 @@ class JBSImport
                     $db->query();
                     
                     //For some reason the auto_increment was dropped in the backup so we need to add it back
-                    $query = 'ALTER TABLE '.$newtable.' ADD PRIMARY KEY (id)';
-                    $db->setQuery($query);
-                    $db->query();
-                    
-                    $query = 'ALTER TABLE '.$newtable.' MODIFY id int(10) NOT NULL AUTO_INCREMENT';
-                    $db->setQuery($query);
-                    $db->query();
-                    
+                    if (!substr_count($newtable, 'timeset'))
+                    {
+                        $query = 'ALTER TABLE '.$newtable.' ADD PRIMARY KEY (id)';
+                        $db->setQuery($query);
+                        $db->query();
+                        
+                        $query = 'ALTER TABLE '.$newtable.' MODIFY id int(10) NOT NULL AUTO_INCREMENT';
+                        $db->setQuery($query);
+                        $db->query();
+                    }
                     if (substr_count($newtable,'studies'))
                         {
                             $query = 'ALTER TABLE '.$newtable.' MODIFY studytext TEXT';
