4d1d83e0135d9ea846607675ca70f59a09af2e75
tomfuller2 <tomfuller2@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2011-04-22 23:01:17 +0000

    Beginning to work on search plugin
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@1764 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- biblestudysearch/biblestudysearch.php
+++ biblestudysearch/biblestudysearch.php
@@ -1,4 +1,135 @@
 <?php defined('_JEXEC') or die('Restricted access'); ?>
+<?php 
+class plgSearchBiblestudy extends JPlugin
+{
+
+    /**
+	 * Constructor
+	 *
+	 * @access      protected
+	 * @param       object  $subject The object to observe
+	 * @param       array   $config  An array that holds the plugin configuration
+	 * @since       1.5
+     * based on com_weblinks
+	 */
+	public function __construct(& $subject, $config)
+	{
+		parent::__construct($subject, $config);
+		$this->loadLanguage();
+	}
+
+	/**
+	 * @return array An array of search areas
+	 */
+	function onContentSearchAreas() {
+		static $areas = array(
+			'biblestudy' => 'PLG_SEARCH_BIBLESTUDY'
+			);
+			return $areas;
+	}
+
+/**
+	 * Biblestudy Search method
+	 *
+	 * The sql must return the following fields that are used in a common display
+	 * routine: 
+	 * @param string Target search string
+	 * @param string mathcing option, exact|any|all
+	 * @param string ordering option, newest|oldest|popular|alpha|category
+	 * @param mixed An array if the search it to be restricted to areas, null if search all
+	 */
+	function onContentSearch($text, $phrase='', $ordering='', $areas=null)
+	{
+		$db		= JFactory::getDbo();
+		$app	= JFactory::getApplication();
+		$user	= JFactory::getUser();
+		$groups	= implode(',', $user->getAuthorisedViewLevels());
+
+        
+		$searchText = $text;
+
+		if (is_array($areas)) {
+			if (!array_intersect($areas, array_keys($this->onContentSearchAreas()))) {
+				return array();
+			}
+		}
+    
+    $state = array();
+		if ($sContent) {
+			$state[]=1;
+		}
+		if ($sArchived) {
+			$state[]=2;
+		}
+        
+    $text = trim($text);
+		if ($text == '') {
+			return array();
+		}
+		$section	= JText::_('PLG_SEARCH_WEBLINKS');
+
+		$wheres	= array();
+		switch ($phrase)
+		{
+			case 'exact':
+				$text		= $db->Quote('%'.$db->getEscaped($text, true).'%', false);
+				$wheres2	= array();
+				$wheres2[]	= 'a.url LIKE '.$text;
+				$wheres2[]	= 'a.description LIKE '.$text;
+				$wheres2[]	= 'a.title LIKE '.$text;
+				$where		= '(' . implode(') OR (', $wheres2) . ')';
+				break;
+
+			case 'all':
+			case 'any':
+			default:
+				$words	= explode(' ', $text);
+				$wheres = array();
+				foreach ($words as $word)
+				{
+					$word		= $db->Quote('%'.$db->getEscaped($word, true).'%', false);
+					$wheres2	= array();
+					$wheres2[]	= 'a.studytext LIKE '.$word;
+					$wheres2[]	= 'a.studyintro LIKE '.$word;
+					$wheres2[]	= 'a.teachername LIKE '.$word;
+                    $wheres2[]	= 'a.bookname LIKE '.$word;
+                    $wheres2[]	= 'a.series_text LIKE '.$word;
+                    $wheres2[]	= 'a.topic_text LIKE '.$word;
+					$wheres[]	= implode(' OR ', $wheres2);
+				}
+				$where	= '(' . implode(($phrase == 'all' ? ') AND (' : ') OR ('), $wheres) . ')';
+				break;
+		}
+        
+        switch ($ordering) {
+	case 'oldest':
+		$order = 'a.studydate ASC';
+		break;
+	case 'alpha':
+		$order = 'a.studytitle ASC';
+		break;
+	case 'newest':
+	default:
+		$order = 'a.studydate DESC';
+		break;
+   
+   if (!empty($state)) {
+			$query	= $db->getQuery(true);
+			$query->select('a.title AS title, a.description AS text, a.created AS created, a.url, '
+						.'CASE WHEN CHAR_LENGTH(a.alias) THEN CONCAT_WS(\':\', a.id, a.alias) ELSE a.id END as slug, '
+						.'CASE WHEN CHAR_LENGTH(c.alias) THEN CONCAT_WS(\':\', c.id, c.alias) ELSE c.id END as catslug, '
+						.'CONCAT_WS(" / ", '.$db->Quote($section).', c.title) AS section, "1" AS browsernav');
+			$query->from('#__bsms_studies AS a');
+			$query->innerJoin('#__categories AS c ON c.id = a.catid');
+			$query->where('('.$where.')' . ' AND a.state in ('.implode(',',$state).') AND  c.published=1 AND  c.access IN ('.$groups.')');
+			$query->order($order);
+   
+    //more here
+    
+    }
+}
+
+?>
 <?php $mainframe->registerEvent( 'onSearch', 'botSearchBiblestudies' );
 $mainframe->registerEvent( 'onSearchAreas', 'botSearchBiblestudiesAreas' );
 	static $areas = array(
