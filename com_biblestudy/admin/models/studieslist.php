38f7771489ab5d78f3afb7ea75c257ff66f21834
genu <genu@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2011-07-16 04:24:43 +0000

    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@1922 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- com_biblestudy/admin/models/studieslist.php
+++ com_biblestudy/admin/models/studieslist.php
@@ -8,13 +8,13 @@
 //No Direct Access
 defined('_JEXEC') or die();
 
- include_once (JPATH_COMPONENT_ADMINISTRATOR .DS. 'helpers' .DS. 'translated.php');
- 
-    jimport('joomla.application.component.modellist');
+include_once (JPATH_COMPONENT_ADMINISTRATOR . DS . 'helpers' . DS . 'translated.php');
 
-    abstract class modelClass extends JModelList {
-        
-    }
+jimport('joomla.application.component.modellist');
+
+abstract class modelClass extends JModelList {
+    
+}
 
 class biblestudyModelstudieslist extends modelClass {
 
@@ -35,10 +35,10 @@ class biblestudyModelstudieslist extends modelClass {
 
         // Get the pagination request variables
         $limit = $mainframe->getUserStateFromRequest('global.list.limit', 'limit', $mainframe->getCfg('list_limit'), 'int');
-       
+
         $limitstart = $mainframe->getUserStateFromRequest('com_biblestudy&view=studieslist.limitstart', 'limitstart', 0, 'int');
 
-       
+
         $this->setState('limit', $limit);
         $this->setState('limitstart', $limitstart);
     }
@@ -50,7 +50,7 @@ class biblestudyModelstudieslist extends modelClass {
     function _buildQuery() {
         $where = $this->_buildContentWhere();
         $orderby = $this->_buildContentOrderBy();
-        
+
         $query = 'SELECT #__bsms_studies.*, #__bsms_teachers.id AS tid, #__bsms_teachers.teachername,'
                 . ' #__bsms_series.id AS sid, #__bsms_series.series_text, #__bsms_message_type.id AS mid,'
                 . ' #__bsms_message_type.message_type AS message_type, #__bsms_books.bookname,'
@@ -80,14 +80,12 @@ class biblestudyModelstudieslist extends modelClass {
         return $result[0]->totalDownloads;
     }
 
-   
-
     /**
      * Retrieves the data
      * @return array Array of objects containing the data from the database
      */
     function getData() {
-		die('biblestudyModelstudieslist.getData is no more used');
+        die('biblestudyModelstudieslist.getData is no more used');
 //        // Lets load the data if it doesn't already exist
 //        if (empty($this->_data)) {
 //            $query = $this->_buildQuery();
@@ -227,32 +225,32 @@ class biblestudyModelstudieslist extends modelClass {
     /**
      * @since   7.0
      */
-    protected function  populateState() {
-        $studytitle = $this->getUserStateFromRequest($this->context.'.filter.studytitle', 'filter_studytitle');
+    protected function populateState() {
+        $studytitle = $this->getUserStateFromRequest($this->context . '.filter.studytitle', 'filter_studytitle');
         $this->setState('filter.studytitle', $studytitle);
-        
-        $published = $this->getUserStateFromRequest($this->context.'.filter.published', 'filter_published', '');
-		$this->setState('filter.published', $published);
 
-        $book = $this->getUserStateFromRequest($this->context.'.filter.book', 'filter_book');
+        $published = $this->getUserStateFromRequest($this->context . '.filter.published', 'filter_published', '');
+        $this->setState('filter.published', $published);
+
+        $book = $this->getUserStateFromRequest($this->context . '.filter.book', 'filter_book');
         $this->setState('filter.book', $book);
 
-        $teacher = $this->getUserStateFromRequest($this->context.'.filter.teacher', 'filter_teacher');
+        $teacher = $this->getUserStateFromRequest($this->context . '.filter.teacher', 'filter_teacher');
         $this->setState('filter.teacher', $teacher);
 
-        $series = $this->getUserStateFromRequest($this->context.'.filter.series', 'filter_series');
+        $series = $this->getUserStateFromRequest($this->context . '.filter.series', 'filter_series');
         $this->setState('filter.series', $series);
 
-        $messageType = $this->getUserStateFromRequest($this->context.'.filter.messageType', 'filter_message_type');
+        $messageType = $this->getUserStateFromRequest($this->context . '.filter.messageType', 'filter_message_type');
         $this->setState('filter.messageType', $messageType);
 
-        $year = $this->getUserStateFromRequest($this->context.'.filter.year', 'filter_year');
+        $year = $this->getUserStateFromRequest($this->context . '.filter.year', 'filter_year');
         $this->setState('filter.year', $year);
 
-        $topic = $this->getUserStateFromRequest($this->context.'.filter.topic', 'filter_topic');
+        $topic = $this->getUserStateFromRequest($this->context . '.filter.topic', 'filter_topic');
         $this->setState('filter.topic', $topic);
 
-        $state = $this->getUserStateFromRequest($this->context.'.filter.state', 'filter_state');
+        $state = $this->getUserStateFromRequest($this->context . '.filter.state', 'filter_state');
         $this->setState('filter.state', $state);
 
         parent::populateState('study.studydate', 'DESC');
@@ -270,8 +268,7 @@ class biblestudyModelstudieslist extends modelClass {
 
         $query->select(
                 $this->getState(
-                        'list.select',
-                        'study.id, study.published, study.studydate, study.studytitle, study.booknumber, study.chapter_begin,
+                        'list.select', 'study.id, study.published, study.studydate, study.studytitle, study.booknumber, study.chapter_begin,
                         study.verse_begin, study.chapter_end, study.verse_end, study.hits'));
         $query->from('#__bsms_studies AS study');
 
@@ -290,68 +287,67 @@ class biblestudyModelstudieslist extends modelClass {
         //Join over Topics
         $query->select('topic.topic_text, topic.params AS topic_params');
         $query->join('LEFT', '#__bsms_topics AS topic ON topic.id = study.topics_id');
-        
+
         //Join over Books
         $query->select('book.bookname');
         $query->join('LEFT', '#__bsms_books AS book ON book.booknumber = study.booknumber');
-        
+
         //Join over Plays/Downloads
         $query->select('SUM(mediafile.plays) AS totalplays, SUM(mediafile.downloads) as totaldownloads, mediafile.study_id');
-        $query->join('LEFT','#__bsms_mediafiles AS mediafile ON mediafile.study_id = study.id');
+        $query->join('LEFT', '#__bsms_mediafiles AS mediafile ON mediafile.study_id = study.id');
         $query->group('study.id');
-        
-      
-        
+
+
+
         //Filter by studytitle
         $studytitle = $this->getState('filter.studytitle');
-        if(!empty($studytitle))
-            $query->where('study.studytitle LIKE "'.$studytitle.'%"');
+        if (!empty($studytitle))
+            $query->where('study.studytitle LIKE "' . $studytitle . '%"');
 
         //Filter by book
         $book = $this->getState('filter.book');
-        if(!empty($book))
-            $query->where('study.booknumber = '.(int)$book.' OR study.booknumber2 = '.(int)$book);
-            $query->join('LEFT', '#__bsms_books AS books ON books.booknumber = study.booknumber');
+        if (!empty($book))
+            $query->where('study.booknumber = ' . (int) $book . ' OR study.booknumber2 = ' . (int) $book);
+        $query->join('LEFT', '#__bsms_books AS books ON books.booknumber = study.booknumber');
 
         //Filter by teacher
         $teacher = $this->getState('filter.teacher');
-        if(!empty($teacher))
-            $query->where('study.teacher_id = '.(int)$teacher);
+        if (!empty($teacher))
+            $query->where('study.teacher_id = ' . (int) $teacher);
 
         //Filter by series
         $series = $this->getState('filter.series');
-        if(!empty($series))
-            $query->where('study.series_id = '.(int)$series);
+        if (!empty($series))
+            $query->where('study.series_id = ' . (int) $series);
 
         //Filter by message type
         $messageType = $this->getState('filter.messageType');
-        if(!empty($messageType))
-            $query->where('study.messageType = '.(int)$messageType);
+        if (!empty($messageType))
+            $query->where('study.messageType = ' . (int) $messageType);
 
         //Filter by Year
         $year = $this->getState('filter.year');
         if (!empty($year))
-            $query->where('YEAR(study.studydate) = '.(int)$year );
-            
+            $query->where('YEAR(study.studydate) = ' . (int) $year);
+
         //Filter by topic
         $topic = $this->getState('filter.topic');
-        if(!empty($topic))
-            $query->where('study.topics_id = '.(int)$topic);
+        if (!empty($topic))
+            $query->where('study.topics_id = ' . (int) $topic);
 
         // Filter by published state
-		$published = $this->getState('filter.published');
-		if (is_numeric($published)) {
-			$query->where('study.published = ' . (int) $published);
-		}
-		else if ($published === '') {
-			$query->where('(study.published = 0 OR study.published = 1)');
-		}
+        $published = $this->getState('filter.published');
+        if (is_numeric($published)) {
+            $query->where('study.published = ' . (int) $published);
+        } else if ($published === '') {
+            $query->where('(study.published = 0 OR study.published = 1)');
+        }
 
         //Add the list ordering clause
         $orderCol = $this->state->get('list.ordering');
         $orderDirn = $this->state->get('list.direction');
-        $query->order($db->getEscaped($orderCol.' '.$orderDirn));
-        
+        $query->order($db->getEscaped($orderCol . ' ' . $orderDirn));
+
         return $query;
     }
 
@@ -361,13 +357,12 @@ class biblestudyModelstudieslist extends modelClass {
      */
 
     public function getTranslated($items = array()) {
-		foreach ($items as $item) {
-			$item->bookname = JText::_($item->bookname);
-			$item->topic_text = getTopicItemTranslated($item);
-		}
-		return $items;
-	}
-
+        foreach ($items as $item) {
+            $item->bookname = JText::_($item->bookname);
+            $item->topic_text = getTopicItemTranslated($item);
+        }
+        return $items;
+    }
 
     /*
      * @since 7.0
@@ -384,14 +379,12 @@ class biblestudyModelstudieslist extends modelClass {
         $query->order('book.booknumber');
 
         $db->setQuery($query->__toString());
-      
+
         $db_result = $db->loadAssocList();
-        foreach($db_result as $i => $value)
-        {
-                 $db_result[$i]['text'] = JText::_($value['text']);
+        foreach ($db_result as $i => $value) {
+            $db_result[$i]['text'] = JText::_($value['text']);
         }
         return $db_result;
-        
     }
 
     /*
@@ -465,43 +458,40 @@ class biblestudyModelstudieslist extends modelClass {
         $db->setQuery($query->__toString());
         $db_result = $db->loadObjectList();
         $output = array();
-        foreach($db_result as $i => $value)
-        {
-                 $value->text = getTopicItemTranslated($value);
-                 $output[] = $value;
+        foreach ($db_result as $i => $value) {
+            $value->text = getTopicItemTranslated($value);
+            $output[] = $value;
         }
         return $output;
     }
 
-    public function getYears(){
+    public function getYears() {
         $db = $this->getDBO();
         $query = $db->getQuery(true);
-        
+
         $query->select('DISTINCT YEAR(studydate) as value, YEAR(studydate) as text');
-        $query->from('#__bsms_studies' );
+        $query->from('#__bsms_studies');
         $query->order('value');
-        
+
         $db->setQuery($query->__toString());
         $year = $db->loadObjectList();
-      //  dump ($year);
+        //  dump ($year);
         return $year;
     }
-    
+
     public function getPlays($id) {
         $db = $this->getDBO();
         $query = $db->getQuery(true);
-        
+
         $query->select('SUM(plays) AS totalPlays');
         $query->from('#__bsms_mediafiles');
         $query->group('study_id');
-        $query->where('study_id = '.$id);
+        $query->where('study_id = ' . $id);
         $db->setQuery($query->__toString());
         $plays = $db->loadResult();
         return $plays;
-        
     }
-    
-     
+
 }
 
 ?>
