ee74b1444a8f24a6af24afbf7fa9a1591abc6a24
bcordis <bcordis@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2011-09-13 20:54:13 +0000

    Copy from 6.4 code will need to test for changes.
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@2039 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- com_biblestudy/site/router.php
+++ com_biblestudy/site/router.php
@@ -1,29 +1,72 @@
-<?php
-defined( '_JEXEC' ) or die( 'Restricted access' );
+<?php defined('_JEXEC') or die ('Restricted Access');
 
-function BibleStiudyBuildRoute(&$query)
+
+function biblestudyBuildRoute(&$query)
 {
-  $segments = array();
+	$segments = array();
 
-  if(isset($query['task'])) 
-  {
-    $segments[] = $query['task'];		
-    unset($query['task']);
-  };
+	if(isset($query['view'])) 
+	{
+		if(empty($query['Itemid'])) {
+			$segments[] = $query['view'];
+		}
+		else { 
+		$segments[] = $query['view'];
+		}
+		unset($query['view']);
+	};
 	
-  if(isset($query['id']))
-  {
-    $segments[] = $query['id'];
-    unset($query['id']);
-  };
-
-  return $segments;
+	if(isset($query['id']))
+	{
+		$segments[] = $query['id'];
+		unset($query['id']);
+	};
+	
+	
+	return $segments;
 }
 
-function BibleStiudyHelloParseRoute($segments)
+function biblestudyParseRoute($segments)
 {
-  $vars = array();
-  $vars['task']	= $segments[0];
-  $vars['id']		= $segments[1];
-  return $vars;
+	$vars = array();
+
+	//Get the active menu item
+	$menu =& JSite::getMenu();
+	$item =& $menu->getActive();
+
+	// Count route segments
+	//$count = count($segments);
+	
+	//Standard routing for articles
+	if(!isset($item)) 
+	{
+		$vars['view'] = $segments[0];
+		
+		$vars['id'] = $segments[1];
+		
+		
+
+		return $vars;
+	}
+
+	//Handle View and Identifier
+	switch($item->query['view'])
+	{
+		case 'studydetails'   :
+		{
+			
+			
+			$vars['id']  = $segments[1];
+			$vars['view'] = 'studydetails';
+
+		} break;
+		case 'studieslist' :
+		{
+			
+			$vars['id']   = $segments[1];
+			$vars['view'] = 'studieslist';
+		} break;
+	}
+
+	return $vars;
 }
