5d36718471227b9e545b99eabc194ecb06cb0688
tomfuller2 <tomfuller2@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2011-11-16 23:56:10 +0000

    Created new function to allow copying and migrating of db tables in place.
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@2099 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- jbsmigration/controller.php
+++ jbsmigration/controller.php
@@ -26,14 +26,18 @@ class jbsmigrationController extends JController
 	 */
 	function display()
 	{
-		$application = JFactory::getApplication();
+		
+        $application = JFactory::getApplication();
 		JRequest::setVar('migrationdone','0','get');
 		$task = JRequest::getWord('task','','get');
 		$run = 0;
-
+        $oldprefix = JRequest::getInt('oldprefix','','post');
+        
 		$run = JRequest::getInt('run','','get');
 
 		$import = JRequest::getVar('file','','post');
+        
+
 		if ($task == 'export' && $run == 1)
 		{
 			$export = new JBSExport();
@@ -46,9 +50,10 @@ class jbsmigrationController extends JController
 				$application->enqueueMessage( ''. JText::_('JBS_EI_FAILURE') .'' ) ;
 			}
 		}
-		if ($task == 'migrate' && $run == 1)
+		if ($task == 'migrate' && $run == 1 && !$oldprefix)
 		{
-			$migrate = new JBSMigrate();
+			
+            $migrate = new JBSMigrate();
 			$migration = $migrate->migrate();
 			if ($migration)
 			{
@@ -63,13 +68,32 @@ class jbsmigrationController extends JController
 		parent::display();
 	}
 
+
 	function doimport()
 	{
-
-		$application = JFactory::getApplication();
-		$import = new JBSImport();
-		$result = $import->importdb();
-		if ($result)
+        $application = JFactory::getApplication();
+        
+        //Add commands to move tables from old prefix to new
+        $oldprefix = JRequest::getWord('oldprefix','','post');
+        
+        if ($oldprefix) 
+            {
+                $tablescopied = $this->copyTables($oldprefix);
+                //if error
+                //check for empty array - if not, print results
+                if (empty($tablescopied)){$copysuccess = 1; print_r($tablescopied);}
+                else
+                {$copysuccess = false;}
+                
+            }
+       if (!$oldprefix)
+       {
+            
+    		$import = new JBSImport();
+    		$result = $import->importdb();
+       }
+		
+		if ($result || $copysuccess)
 		{
 			$migrate = new JBSMigrate();
 			$migration = $migrate->migrate();
@@ -93,4 +117,55 @@ class jbsmigrationController extends JController
 		parent::display();
 	}
 
-}
+function performdb($query)
+	{
+		$db = JFactory::getDBO();
+		$results = false;
+		$db->setQuery($query);
+		$db->query();
+		if ($db->getErrorNum() != 0)
+		{
+			$results = JText::_('JBS_EI_DB_ERROR').': '.$db->getErrorNum()."<br /><font color=\"red\">";
+			$results .= $db->stderr(true);
+			$results .= "</font>";
+			return $results;
+		}
+		else
+		{
+			$results = false; return $results;
+		}
+	}
+    
+ function copyTables($oldprefix)
+ {
+    //create table tablename_new like tablename; -> this will copy the structure...
+    //insert into tablename_new select * from tablename; -> this would copy all the data
+    $results = array();
+    $db = JFactory::getDBO();
+    $tables = $db->getTableList();
+	$prefix = $db->getPrefix();
+    foreach ($tables as $table)
+    {
+        $isjbs = substr_count($table,$oldprefix.'bsms');
+        if ($isjbs)
+        {
+            $oldlength = strlen($oldprefix);
+            $newsubtablename = substr($table,$oldlength );
+            $newtablename = $prefix.$newsubtablename;
+            $results = array();
+            $query = 'DROP TABLE IF EXISTS '.$newtablename;
+            $result = $this->performdb($query);
+                if ($result){$results[] = $result;}
+            $query = 'CREATE TABLE '.$newtablename.' LIKE '.$table;
+            $result = $this->performdb($query);
+                if ($result){$results[] = $result;}
+            $query = 'INSERT INTO '.$newtablename.' SELECT * FROM '.$table;
+            $result = $this->performdb($query);
+                if ($result){$results[] = $result;}
+            
+        }
+    }
+    return $results;
+    
+ }
+} // end of class
