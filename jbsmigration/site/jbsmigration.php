eb5efe7c58fbd709295c56f76abeb72ce6e9cdd2
bcordis <bcordis@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2012-01-15 22:58:28 +0000

    New jbsmigration system work j1.5 and j1.6+
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@2310 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

---
+++ jbsmigration/site/jbsmigration.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @version $Id: jbsmigration.php 1 $
+ * @package COM_JBSMIGRATION
+ * @Copyright (C) 2007 - 2011 Joomla Bible Study Team All rights reserved
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link http://www.JoomlaBibleStudy.org
+ * */
+defined('_JEXEC') or die();
+
+JError::raiseError(404,JText::_('COMPONENT NOT FOUND'));
