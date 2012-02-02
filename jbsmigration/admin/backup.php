6678d8bf4db46a8f640c9cd008a74a7d8a62251a
bcordis <bcordis@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2012-02-02 21:20:37 +0000

    Proper call for comments now.
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@2441 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- jbsmigration/admin/backup.php
+++ jbsmigration/admin/backup.php
@@ -49,7 +49,7 @@ class JBSExport {
         $db = JFactory::getDBO();
         //Get the prefix
         $prefix = $db->getPrefix();
-     //  $export = "---\n --- Table " . $table . "\n ---\n";
+        $export = "--\n -- Table structure for table `" . $table . "`\n --\n";
         //Drop the existing table
         $export .= 'DROP TABLE ' . $table . ";\n";
         //Create a new table defintion based on the incoming database
