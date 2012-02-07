2e9f402ed0f049b7f010b14df44f94be6c35978b
bcordis <bcordis@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2012-02-07 04:31:11 +0000

    update script.php
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@2483 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

---
+++ plg_biblestudy_backup/script.php
@@ -0,0 +1,62 @@
+<?php
+
+// No direct access to this file
+defined('_JEXEC') or die;
+
+/**
+ * Script file of JBSBACKUP component
+ */
+class PLG_JBSBACKUPInstallerScript {
+
+    /**
+     * method to install the component
+     *
+     * @return void
+     */
+    function install($parent) {
+
+    }
+
+    /**
+     * method to uninstall the component
+     *
+     * @return void
+     */
+    function uninstall($parent) {
+        // $parent is the class calling this method
+        echo '<p>' . JText::_('PLG_JBSBACKUP_UNINSTALL_TEXT') . '</p>';
+    }
+
+    /**
+     * method to update the component
+     *
+     * @return void
+     */
+    function update($parent) {
+        // $parent is the class calling this method
+        echo '<p>' . JText::_('PLG_JBSBACKUP_UPDATE_TEXT') . '</p>';
+    }
+
+    /**
+     * method to run before an install/update/uninstall method
+     *
+     * @return void
+     */
+    function preflight($type, $parent) {
+        // $parent is the class calling this method
+        // $type is the type of change (install, update or discover_install)
+        echo '<p>' . JText::_('PLG_JBSBACKUP_PREFLIGHT_' . $type . '_TEXT') . '</p>';
+    }
+
+    /**
+     * method to run after an install/update/uninstall method
+     *
+     * @return void
+     */
+    function postflight($type, $parent) {
+        // $parent is the class calling this method
+        // $type is the type of change (install, update or discover_install)
+        echo '<p>' . JText::_('PLG_JBSBACKUP_POSTFLIGHT_' . $type . '_TEXT') . '</p>';
+    }
+
+}
