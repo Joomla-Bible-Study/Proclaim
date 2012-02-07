f714fe17c1224c7d77c54c4af9079593d61019b2
bcordis <bcordis@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2012-02-07 04:23:49 +0000

    added sql update
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@2482 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- plg_biblestudy_backup/sql/install.mysql.utf8.sql
+++ plg_biblestudy_backup/sql/install.mysql.utf8.sql
@@ -1,3 +1,14 @@
+DROP TABLE IF EXISTS `#__jbsbackup_update`;
+
+CREATE TABLE IF NOT EXISTS `#__jbsbackup_update` (
+  id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  version VARCHAR(255) DEFAULT NULL,
+  PRIMARY KEY (id)
+) DEFAULT CHARSET=utf8;
+
+INSERT INTO `#__bsms_update` (id,version) VALUES
+(1,'7.0.7'),
+
 CREATE TABLE IF NOT EXISTS `#__jbsbackup_timeset` (
 	`timeset` varchar(14) NOT NULL DEFAULT '',
 	`backup` varchar(14) DEFAULT NULL,
@@ -8,5 +19,5 @@ CREATE TABLE IF NOT EXISTS `#__jbsbackup_timeset` (
 -- Dumping data for table `#__jbsbackup_timeset`
 --
 
-INSERT INTO `#__jbsbackup_timeset` (`timeset`, `backup`) VALUES 
+INSERT INTO `#__jbsbackup_timeset` (`timeset`, `backup`) VALUES
 	( '1281646339', '1281646339');
