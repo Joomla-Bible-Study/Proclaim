8a92294716db6e77f5145d43de2cb3f632069e0c
bcordis <bcordis@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2012-04-13 17:04:11 +0000

    Fixed recall
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@2776 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- com_biblestudy/admin/models/fields/teacherordering.php
+++ com_biblestudy/admin/models/fields/teacherordering.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @version		$Id: teacherOrdering.php 2025 2011-08-28 04:08:06Z genu $
  * @package		Joomla.Administrator
@@ -7,6 +6,7 @@
  * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
+
 //No Direct Access
 defined('_JEXEC') or die;
 
@@ -20,58 +20,57 @@ jimport('joomla.form.formfield');
  * @subpackage	com_weblinks
  * @since		1.6
  */
-class JFormFieldteacherOrdering extends JFormField {
+class JFormFieldteacherOrdering extends JFormField
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var		string
+	 * @since	1.6
+	 */
+	protected $type = 'teacherOrdering';
 
-    /**
-     * The form field type.
-     *
-     * @var		string
-     * @since	1.6
-     */
-    protected $type = 'TeacherOrdering';
+	/**
+	 * Method to get the field input markup.
+	 *
+	 * @return	string	The field input markup.
+	 * @since	1.6
+	 */
+	protected function getInput()
+	{
+		// Initialize variables.
+		$html = array();
+		$attr = '';
 
-    /**
-     * Method to get the field input markup.
-     *
-     * @return	string	The field input markup.
-     * @since	1.6
-     */
-    protected function getInput() {
-        // Initialize variables.
-        $html = array();
-        $attr = '';
+		// Initialize some field attributes.
+		$attr .= $this->element['class'] ? ' class="'.(string) $this->element['class'].'"' : '';
+		$attr .= ((string) $this->element['disabled'] == 'true') ? ' disabled="disabled"' : '';
+		$attr .= $this->element['size'] ? ' size="'.(int) $this->element['size'].'"' : '';
 
-        // Initialize some field attributes.
-        $attr .= $this->element['class'] ? ' class="' . (string) $this->element['class'] . '"' : '';
-        $attr .= ((string) $this->element['disabled'] == 'true') ? ' disabled="disabled"' : '';
-        $attr .= $this->element['size'] ? ' size="' . (int) $this->element['size'] . '"' : '';
+		// Initialize JavaScript field attributes.
+		$attr .= $this->element['onchange'] ? ' onchange="'.(string) $this->element['onchange'].'"' : '';
 
-        // Initialize JavaScript field attributes.
-        $attr .= $this->element['onchange'] ? ' onchange="' . (string) $this->element['onchange'] . '"' : '';
+		// Get some field values from the form.
+		$teacherId	= (int) $this->form->getValue('id');
 
-        // Get some field values from the form.
-        $weblinkId = (int) $this->form->getValue('id');
-        $categoryId = (int) $this->form->getValue('catid');
+		// Build the query for the ordering list.
+		$query = 'SELECT ordering AS value, teachername AS text' .
+				' FROM #__bsms_teachers' .
+				' WHERE catid = 1' .
+				' ORDER BY ordering';
 
-        // Build the query for the ordering list.
-        $query = 'SELECT ordering AS value, teachername AS text' .
-                ' FROM #__bsms_teachers' .
-                ' WHERE catid = ' . (int) $categoryId .
-                ' ORDER BY ordering';
+		// Create a read-only list (no name) with a hidden input to store the value.
 
-        // Create a read-only list (no name) with a hidden input to store the value.
+		if ((string) $this->element['readonly'] == 'true') {
+			$html[] = JHtml::_('list.ordering', '', $query, trim($attr), $this->value, $teacherId ? 0 : 1);
+			$html[] = '<input type="hidden" name="'.$this->teachername.'" value="'.$this->value.'"/>';
+		}
+		// Create a regular list.
+		else {
 
-        if ((string) $this->element['readonly'] == 'true') {
-            $html[] = JHtml::_('list.ordering', '', $query, trim($attr), $this->value, $weblinkId ? 0 : 1);
-            $html[] = '<input type="hidden" name="' . $this->teachername . '" value="' . $this->value . '"/>';
-        }
-        // Create a regular list.
-        else {
+			$html[] = JHtml::_('list.ordering', $this->teachername, $query, trim($attr), $this->value, $teacherId ? 0 : 1);
+		}
 
-            $html[] = JHtml::_('list.ordering', $this->teachername, $query, trim($attr), $this->value, $weblinkId ? 0 : 1);
-        }
-
-        return implode($html);
-    }
-
+		return implode($html);
+	}
 }
