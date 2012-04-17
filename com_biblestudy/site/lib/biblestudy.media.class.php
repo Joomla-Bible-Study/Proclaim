8aaf4a671f43edcff4dedecf3dc587922c9c3dca
tomfuller2 <tomfuller2@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2012-04-17 22:07:29 +0000

    added mimetype to query
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@2790 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- com_biblestudy/site/lib/biblestudy.media.class.php
+++ com_biblestudy/site/lib/biblestudy.media.class.php
@@ -170,7 +170,7 @@ class jbsMedia {
                 . ' #__bsms_media.media_image_name AS imname, #__bsms_media.path2 AS path2, s.studyintro, s.media_hours, s.media_minutes,'
                 . ' s.media_seconds, s.studytitle, s.studydate, s.teacher_id, s.booknumber, s.chapter_begin, s.chapter_end, s.verse_begin,'
                 . ' s.verse_end, t.teachername, t.id as tid, s.id as sid, s.studyintro,  #__bsms_media.media_alttext AS malttext,'
-                . ' #__bsms_mimetype.id AS mtid, #__bsms_mimetype.mimetext FROM #__bsms_mediafiles'
+                . ' #__bsms_mimetype.id AS mtid, #__bsms_mimetype.mimetext, #__bsms_mimetype.mimetype FROM #__bsms_mediafiles'
                 . ' LEFT JOIN #__bsms_media ON (#__bsms_media.id = #__bsms_mediafiles.media_image)'
                 . ' LEFT JOIN #__bsms_servers ON (#__bsms_servers.id = #__bsms_mediafiles.server)'
                 . ' LEFT JOIN #__bsms_folders ON (#__bsms_folders.id = #__bsms_mediafiles.path)'
