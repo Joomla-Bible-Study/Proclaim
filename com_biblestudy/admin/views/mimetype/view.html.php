4ae61cad2f7109e5ceb8f572e3651358142f090a
bcordis <bcordis@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2012-04-19 16:35:03 +0000

    Finished Title Block update.
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@2801 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- com_biblestudy/admin/views/mimetype/view.html.php
+++ com_biblestudy/admin/views/mimetype/view.html.php
@@ -6,46 +6,77 @@
  * @Copyright (C) 2007 - 2011 Joomla Bible Study Team All rights reserved
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
  * @link http://www.JoomlaBibleStudy.org
- **/
+ * */
 //No Direct Access
 defined('_JEXEC') or die;
 
-require_once (JPATH_ADMINISTRATOR  .DIRECTORY_SEPARATOR. 'components' .DIRECTORY_SEPARATOR. 'com_biblestudy' .DIRECTORY_SEPARATOR. 'helpers' .DIRECTORY_SEPARATOR. 'biblestudy.php');
+require_once (JPATH_ADMINISTRATOR . DIRECTORY_SEPARATOR . 'components' . DIRECTORY_SEPARATOR . 'com_biblestudy' . DIRECTORY_SEPARATOR . 'helpers' . DIRECTORY_SEPARATOR . 'biblestudy.php');
 
 jimport('joomla.application.component.view');
 
 class BiblestudyViewMimetype extends JView {
 
-	protected $form;
-	protected $item;
-	protected $state;
-	protected $defaults;
+    protected $form;
+    protected $item;
+    protected $state;
+    protected $defaults;
 
-	function display($tpl = null) {
-		$this->form = $this->get("Form");
-		$this->item = $this->get("Item");
-		$this->state = $this->get("State");
-		$this->canDo	= BibleStudyHelper::getActions($this->item->id, 'mimetype');
-		$this->setLayout("form");
-		$this->addToolbar();
-		parent::display($tpl);
-	}
+    function display($tpl = null) {
+        $this->form = $this->get("Form");
+        $this->item = $this->get("Item");
+        $this->state = $this->get("State");
+        $this->canDo = BibleStudyHelper::getActions($this->item->id, 'mimetype');
+        $this->setLayout("form");
+        // Set the toolbar
+        $this->addToolbar();
 
-	protected function addToolbar() {
-                JRequest::setVar('hidemainmenu', true);
-		$isNew = ($this->item->id < 1);
-		$title = $isNew ? JText::_('JBS_CMN_NEW') : JText::_('JBS_CMN_EDIT');
-		JToolBarHelper::title(JText::_('JBS_CMN_MIME_TYPES') . ': <small><small>[' . $title . ']</small></small>', 'mimetype.png');
+        // Display the template
+        parent::display($tpl);
 
-		if ($this->canDo->get('core.edit','com_biblestudy'))
-		{
-			JToolBarHelper::save('mimetype.save');
-			JToolBarHelper::apply('mimetype.apply');
-		}
-		JToolBarHelper::cancel('mimetype.cancel', 'JTOOLBAR_CANCEL');
+        // Set the document
+        $this->setDocument();
+    }
 
-		JToolBarHelper::divider();
-		JToolBarHelper::help('biblestudy', true);
-	}
+    protected function addToolbar() {
+        JRequest::setVar('hidemainmenu', true);
+        $isNew = ($this->item->id == 0);
+        $title = $isNew ? JText::_('JBS_CMN_NEW') : JText::_('JBS_CMN_EDIT');
+        JToolBarHelper::title(JText::_('JBS_CMN_MIME_TYPES') . ': <small><small>[' . $title . ']</small></small>', 'mimetype.png');
+
+        if ($isNew && $this->canDo->get('core.create', 'com_biblestudy')) {
+            JToolBarHelper::apply('mimetype.apply');
+            JToolBarHelper::save('mimetype.save');
+            JToolBarHelper::save2new('mimetype.save2new');
+            JToolBarHelper::cancel('mimetype.cancel');
+        } else {
+            if ($this->canDo->get('core.edit', 'com_biblestudy')) {
+                JToolBarHelper::apply('mimetype.apply');
+                JToolBarHelper::save('mimetype.save');
+
+                // We can save this record, but check the create permission to see if we can return to make a new one.
+                if ($this->canDo->get('core.create', 'com_biblestudy')) {
+                    JToolbarHelper::save2new('mimetype.save2new');
+                }
+            }
+            // If checked out, we can still save
+            if ($this->canDo->get('core.create', 'com_biblestudy')) {
+                JToolBarHelper::save2copy('mimetype.save2copy');
+            }
+            JToolBarHelper::cancel('mimetype.cancel', 'JTOOLBAR_CLOSE');
+        }
+        JToolBarHelper::divider();
+        JToolBarHelper::help('biblestudy', true);
+    }
+
+    /**
+     * Add the page title to browser.
+     *
+     * @since	7.1.0
+     */
+    protected function setDocument() {
+        $isNew = ($this->item->id < 1);
+        $document = JFactory::getDocument();
+        $document->setTitle($isNew ? JText::_('JBS_TITLE_MIME_TYPES_CREATING') : JText::sprintf('JBS_TITLE_MIME_TYPES_EDITING', $this->item->mimetext));
+    }
 
 }
