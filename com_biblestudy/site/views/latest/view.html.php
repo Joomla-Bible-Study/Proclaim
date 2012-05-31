8c1915cf29865b22208dc71cd1cf7d7be2ebc50c
tomfuller2 <tomfuller2@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2012-05-31 21:09:39 +0000

    Working on latests study link. NOt working yet.
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@2874 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

---
+++ com_biblestudy/site/views/latest/view.html.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * 
+ */
+//No Direct Access
+defined('_JEXEC') or die;
+class BiblestudyViewLatest extends JView {
+
+    function display($tpl = null) {
+     
+        $db = JFactory::getDBO();
+        $query = $db->getQuery('true');
+        $query->select('id');
+        $query->from('#__bsms_studies');
+        $query->where('published = 1');
+        $query->order('studydate DESC LIMIT 1');
+        $db->setQuery($query);
+        $db->query();
+        $id = $db->loadResult(); 
+        $t = JRequest::getInt('t','1','');
+        $link = JRoute('index.php?option=com_biblestudy&view=sermon&id='.$id.'&t='.$t);
+    }
+}
+?>
