ad940be8ddd3ffbef5f2723d5deb5ec64612b2cf
bcordis <bcordis@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2012-06-28 22:46:26 +0000

    DTD update, and js relocation. plus copyright updates.
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@2980 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

---
+++ com_biblestudy/media/js/mediafiles/submitbutton.js
@@ -0,0 +1,27 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+Joomla.submitbutton = function(task)
+{
+        if (task == '')
+        		{
+                return false;
+        		}
+       	else if (task == 'upload')
+        		{
+					if (document.adminForm.upload_folder.value == '') 
+						{
+							alert("<?php echo JText::_('JBS_MED_PATH_OR_FOLDER');?>");
+						} 
+					else if (document.adminForm.upload_server.value == '' ) 
+						{
+							alert("<?php echo JText::_('JBS_MED_SERVER');?>");
+						} 
+  					else {
+						Joomla.submitform(task);
+                  return true;
+						}
+		 		}
+}
