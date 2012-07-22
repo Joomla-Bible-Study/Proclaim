02d7ee1e04b6cb3cba892afb7285ccc898c2753d
tomfuller2 <tomfuller2@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2012-07-22 14:26:43 +0000

    Fixed problem with topics not trashing
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@3072 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

---
+++ com_biblestudy/admin/controllers/topics.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @package BibleStudy.Admin
+ * @Copyright (C) 2007 - 2011 Joomla Bible Study Team All rights reserved
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link http://www.JoomlaBibleStudy.org
+ * */
+//No Direct Access
+defined('_JEXEC') or die;
+
+jimport('joomla.application.component.controlleradmin');
+
+/**
+ * Teachers list controller class.
+ *
+ * @package BibleStudy.Admin
+ * @since 7.0.0
+ */
+class BiblestudyControllerTopics extends JControllerAdmin {
+
+    /**
+     * Proxy for getModel
+     *
+     * @param string $name    The name of the model
+     * @param string $prefix  The prefix for the PHP class name
+     *
+     * @return JModel
+     * @since 7.0.0
+     */
+    public function getModel($name = 'Topic', $prefix = 'BiblestudyModel', $config = array('ignore_request' => true)) {
+        $model = parent::getModel($name, $prefix, $config);
+        return $model;
+    }
+
+}
