589819c27909df77b56331d11ff2b87d132e1e84
bcordis <bcordis@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2012-08-15 19:30:37 +0000

    Bad Key file but moving it to 7.1.0.sql
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@3147 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- com_biblestudy/admin/install/sql/updates/mysql/7.0.1.sql
+++ com_biblestudy/admin/install/sql/updates/mysql/7.0.1.sql
@@ -1,6 +1,3 @@
 -- 7.0.1
 INSERT INTO #__bsms_update (id,version) VALUES (2, '7.0.1')
 ON DUPLICATE KEY UPDATE version= '7.0.1';
-
-ALTER TABLE `#__bsms_studytopics` DROP INDEX id;
-ALTER TABLE `#__bsms_studytopics` DROP INDEX id_2;
