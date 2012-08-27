2896bee8b981d90fa853be5c3926745d03dd7dad
tomfuller2 <tomfuller2@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2012-08-27 21:31:55 +0000

    Removed wrong db prefixes
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@3189 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- com_biblestudy/admin/migration/update701.php
+++ com_biblestudy/admin/migration/update701.php
@@ -51,8 +51,8 @@ class updatejbs701 {
                 }
             }
         }
-        $query = 'ALTER TABLE `r9s7a_bsms_studytopics` DROP INDEX id;
-                ALTER TABLE `r9s7a_bsms_studytopics` DROP INDEX id_2;';
+        $query = 'ALTER TABLE `#__bsms_studytopics` DROP INDEX id;
+                ALTER TABLE `#__bsms_studytopics` DROP INDEX id_2;';
         $msg = $this->performdb($query);
         if (!$msg) {
             $messages[] = '<font color="green">' . JText::_('JBS_IBM_QUERY_SUCCESS') . ': ' . $query . ' </font><br /><br />';
