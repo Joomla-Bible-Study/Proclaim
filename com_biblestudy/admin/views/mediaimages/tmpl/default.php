7c7eb2398717ab482c06d0ed4ec51a48c4beb9ca
bcordis <bcordis@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2012-09-16 15:12:33 +0000

    Fixed colspan error
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@3369 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- com_biblestudy/admin/views/mediaimages/tmpl/default.php
+++ com_biblestudy/admin/views/mediaimages/tmpl/default.php
@@ -48,7 +48,7 @@ $saveOrder = $listOrder == 'mediafile.ordering';
         </thead>
         <tfoot>
             <tr>
-                <td colspan="4">
+                <td colspan="5">
                     <?php echo $this->pagination->getListFooter(); ?>
                 </td>
             </tr>
@@ -83,20 +83,20 @@ $saveOrder = $listOrder == 'mediafile.ordering';
                 </td>
                 <td class="left">
                     <a href="<?php echo JRoute::_('index.php?option=com_biblestudy&task=mediaimage.edit&id=' . (int) $item->id); ?>">
-    <?php echo $this->escape($item->media_image_name); ?>
+                        <?php echo $this->escape($item->media_image_name); ?>
                     </a>
                 </td>
                 <td>
-    <?php echo $item->id; ?>
+                    <?php echo $item->id; ?>
                 </td>
             </tr>
-                <?php endforeach; ?>
+        <?php endforeach; ?>
     </table>
     <div>
         <input type="hidden" name="task" value="" />
         <input type="hidden" name="boxchecked" value="0" />
         <input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>" />
         <input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>" />
-<?php echo JHtml::_('form.token'); ?>
+        <?php echo JHtml::_('form.token'); ?>
     </div>
 </form>
