a77eec7740bf0923c41692f3e6d7a42c930a7ab9
bcordis <bcordis@a73faa7a-dd35-0410-af13-bd1c84eb1a9d>
2012-09-16 15:02:10 +0000

    Add Keepalive
    git-svn-id: http://joomlacode.org/svn/biblestudy/BibleStudy/Trunk@3366 a73faa7a-dd35-0410-af13-bd1c84eb1a9d

--- com_biblestudy/admin/views/mediafile/tmpl/edit.php
+++ com_biblestudy/admin/views/mediafile/tmpl/edit.php
@@ -9,8 +9,10 @@
 //No Direct Access
 defined('_JEXEC') or die;
 
+// Load the tooltip behavior.
 JHtml::_('behavior.tooltip');
 JHtml::_('behavior.formvalidation');
+JHtml::_('behavior.keepalive');
 
 $params = $this->form->getFieldsets('params');
 //Get the studyid if this is coming to us in a modal form
