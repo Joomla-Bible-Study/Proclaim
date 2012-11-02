7d684358dcd37112332f6494231ef64ab7d798a0
Brent Cordis <skeebrent@gmail.com>
2012-11-02 14:36:47 -0500

    reworking helpers

--- com_biblestudy/admin/views/comment/view.html.php
+++ com_biblestudy/admin/views/comment/view.html.php
@@ -2,10 +2,11 @@
 
 /**
  * View Html
+ *
  * @package BibleStudy.Admin
  * @Copyright (C) 2007 - 2011 Joomla Bible Study Team All rights reserved
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
- * @link http://www.JoomlaBibleStudy.org
+ * @link    http://www.JoomlaBibleStudy.org
  * */
 //No Direct Access
 defined('_JEXEC') or die;
@@ -13,97 +14,97 @@ defined('_JEXEC') or die;
 
 /**
  * View class for Comment
+ *
  * @package BibleStudy.Admin
- * @since 7.0.0
+ * @since   7.0.0
  */
-class BiblestudyViewComment extends JViewLegacy {
+class BiblestudyViewComment extends JViewLegacy
+{
 
-    /**
-     * Form Data
-     * @var array
-     */
-    protected $form;
+	/**
+	 * Form Data
+	 *
+	 * @var array
+	 */
+	protected $form;
 
-    /**
-     * Item
-     * @var array
-     */
-    protected $item;
+	/**
+	 * Item
+	 *
+	 * @var array
+	 */
+	protected $item;
 
-    /**
-     * State
-     * @var array
-     */
-    protected $state;
+	/**
+	 * State
+	 *
+	 * @var array
+	 */
+	protected $state;
 
-    /**
-     * Execute and display a template script.
-     *
-     * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
-     *
-     * @return  mixed  A string if successful, otherwise a JError object.
-     *
-     * @see     fetch()
-     * @since   11.1
-     */
-    public function display($tpl = null) {
-        $this->form = $this->get("Form");
-        $this->item = $this->get("Item");
-        $this->state = $this->get("State");
-        $this->canDo = BibleStudyHelper::getActions($this->item->id, 'comment');
+	/**
+	 * Display the view
+	 */
+	public function display($tpl = null)
+	{
+		$this->form = $this->get("Form");
+		$this->item = $this->get("Item");
+		$this->state = $this->get("State");
+		$this->canDo = JBSMHelper::getActions($this->item->id, 'comment');
 
-        if (!JFactory::getUser()->authorize('core.manage', 'com_biblestudy')) {
-            JError::raiseError(404, JText::_('JBS_CMN_NOT_AUTHORIZED'));
-            return false;
-        }
+		// Check for errors.
+		if (count($errors = $this->get('Errors'))) {
+			throw new Exception(implode("\n", $errors), 500);
+			return false;
+		}
+		// Set the toolbar
+		$this->addToolbar();
 
-        $this->setLayout("form");
-        // Set the toolbar
-        $this->addToolbar();
+		// Display the template
+		parent::display($tpl);
 
-        // Display the template
-        parent::display($tpl);
+		// Set the document
+		$this->setDocument();
+	}
 
-        // Set the document
-        $this->setDocument();
-    }
+	/**
+	 * Adds ToolBar
+	 *
+	 * @since 7.0
+	 */
+	protected function addToolbar()
+	{
+		JFactory::getApplication()->input->set('hidemainmenu', true);
+		$isNew = ($this->item->id == 0);
+		$title = $isNew ? JText::_('JBS_CMN_NEW') : JText::_('JBS_CMN_EDIT');
+		JToolBarHelper::title(JText::_('JBS_CMN_COMMENTS') . ': <small><small>[ ' . $title . ' ]</small></small>', 'comments.png');
 
-    /**
-     * Adds ToolBar
-     *
-     * @since 7.0
-     */
-    protected function addToolbar() {
-        JRequest::setVar('hidemainmenu', true);
-        $isNew = ($this->item->id == 0);
-        $title = $isNew ? JText::_('JBS_CMN_NEW') : JText::_('JBS_CMN_EDIT');
-        JToolBarHelper::title(JText::_('JBS_CMN_COMMENTS') . ': <small><small>[ ' . $title . ' ]</small></small>', 'comments.png');
+		if ($isNew && $this->canDo->get('core.create', 'com_biblestudy')) {
+			JToolBarHelper::apply('comment.apply');
+			JToolBarHelper::save('comment.save');
+			JToolBarHelper::save2new('comment.save2new');
+			JToolBarHelper::cancel('comment.cancel');
+		} else {
+			if ($this->canDo->get('core.edit', 'com_biblestudy')) {
+				JToolBarHelper::apply('comment.apply');
+				JToolBarHelper::save('comment.save');
+			}
+			JToolBarHelper::cancel('comment.cancel', 'JTOOLBAR_CLOSE');
+		}
+		JToolBarHelper::divider();
+		JToolBarHelper::help('biblestudy', true);
+	}
 
-        if ($isNew && $this->canDo->get('core.create', 'com_biblestudy')) {
-            JToolBarHelper::apply('comment.apply');
-            JToolBarHelper::save('comment.save');
-            JToolBarHelper::save2new('comment.save2new');
-            JToolBarHelper::cancel('comment.cancel');
-        } else {
-            if ($this->canDo->get('core.edit', 'com_biblestudy')) {
-                JToolBarHelper::apply('comment.apply');
-                JToolBarHelper::save('comment.save');
-            }
-            JToolBarHelper::cancel('comment.cancel', 'JTOOLBAR_CLOSE');
-        }
-        JToolBarHelper::divider();
-        JToolBarHelper::help('biblestudy', true);
-    }
-
-    /**
-     * Add the page title to browser.
-     *
-     * @since	7.1.0
-     */
-    protected function setDocument() {
-        $isNew = ($this->item->id < 1);
-        $document = JFactory::getDocument();
-        $document->setTitle($isNew ? JText::_('JBS_TITLE_COMMENT_CREATING') : JText::sprintf('JBS_TITLE_COMMENT_EDITING', $this->item->id));
-    }
+	/**
+	 * Add the page title to browser.
+	 *
+	 * @since    7.1.0
+	 */
+	protected function setDocument()
+	{
+		$isNew = ($this->item->id < 1);
+		$document = JFactory::getDocument();
+		$document->setTitle($isNew ? JText::_('JBS_TITLE_COMMENT_CREATING') : JText::sprintf('JBS_TITLE_COMMENT_EDITING', $this->item->id));
+	}
 
 }
