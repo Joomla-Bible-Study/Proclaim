06415817d3a68892196eb459bdaedb6cea12e5df
tomfuller2 <tomfuller2@gmail.com>
2012-11-15 16:27:09 -0800

    Working on folders batch. Not working yet.

--- com_biblestudy/admin/views/folders/tmpl/default_batch.php
+++ com_biblestudy/admin/views/folders/tmpl/default_batch.php
@@ -25,10 +25,10 @@ $published = $this->state->get('filter.published');
 
 	</div>
 	<div class="modal-footer">
-		<button class="btn" type="button" onclick="document.id('batch-series-id');document.id('batch-access').value='';document.id('batch-language-id').value=''" data-dismiss="modal">
+		<button class="btn" type="button" onclick="document.id('batch-folders-id');document.id('batch-access').value='';document.id('batch-language-id').value=''" data-dismiss="modal">
 			<?php echo JText::_('JCANCEL'); ?>
 		</button>
-		<button class="btn btn-primary" type="submit" onclick="Joomla.submitbutton('style.batch');">
+		<button class="btn btn-primary" type="submit" onclick="Joomla.submitbutton('folder.batch');">
 			<?php echo JText::_('JGLOBAL_BATCH_PROCESS'); ?>
 		</button>
 	</div>
--- com_biblestudy/admin/views/folders/tmpl/modal.php
+++ com_biblestudy/admin/views/folders/tmpl/modal.php
@@ -25,67 +25,6 @@ $function = $app->input->getCmd('function', 'jSelectStudy');
 $listOrder = $this->escape($this->state->get('list.ordering'));
 $listDirn = $this->escape($this->state->get('list.direction'));
 ?>
-<form action="<?php echo JRoute::_('index.php?option=com_biblestudy&view=folders&layout=modal&tmpl=component&function=' . $function . '&' . JSession::getFormToken() . '=1'); ?>"
-      method="post" name="adminForm" id="adminForm" class="form-inline">
-    <fieldset id="filter clearfix">
-        <div class="btn-toolbar">
-            <div class="btn-group pull-left">
-                <select name="filter_language" class="inputbox" onchange="this.form.submit()">
-                    <option value=""><?php echo JText::_('JOPTION_SELECT_LANGUAGE'); ?></option>
-					<?php echo JHtml::_('select.options', JHtml::_('contentlanguage.existing', true, true), 'value', 'text', $this->state->get('filter.language')); ?>
-                </select>
-            </div>
-    </fieldset>
-    <table class="table table-striped table-condensed">
-        <thead>
-        <tr>
-            <th class="title">
-				<?php echo JHtml::_('grid.sort', 'JBS_CMN_SERIES', 'series.series_text', $listDirn, $listOrder); ?>
-            </th>
-            <th width="5%" class="center nowrap">
-				<?php echo JHtml::_('grid.sort', 'JGRID_HEADING_LANGUAGE', 'language', $listDirn, $listOrder); ?>
-            </th>
-            <th width="1%" class="center nowrap">
-				<?php echo JHtml::_('grid.sort', 'JGRID_HEADING_ID', 'series.id', $listDirn, $listOrder); ?>
-            </th>
-        </tr>
-        </thead>
-        <tbody>
-		<?php foreach ($this->items as $i => $item) : ?>
-			<?php if ($item->language && JLanguageMultilang::isEnabled()) {
-				$tag = strlen($item->language);
-				if ($tag == 5) {
-					$lang = substr($item->language, 0, 2);
-				} elseif ($tag == 6) {
-					$lang = substr($item->language, 0, 3);
-				} else {
-					$lang = "";
-				}
-			} elseif (!JLanguageMultilang::isEnabled()) {
-				$lang = "";
-			}
-			?>
-        <tr class="row<?php echo $i % 2; ?>">
-            <td>
-                <a class="pointer"
-                   onclick="if (window.parent) window.parent.<?php echo $this->escape($function); ?>('<?php echo $item->id; ?>', '<?php echo $this->escape(addslashes($item->series_text)); ?>');">
-					<?php echo $this->escape($item->series_text); ?>
-                </a>
-            </td>
-            <td class="center">
-				<?php if ($item->language == '*'): ?>
-				<?php echo JText::alt('JALL', 'language'); ?>
-				<?php else: ?>
-				<?php echo $item->language_title ? $this->escape($item->language_title) : JText::_('JUNDEFINED'); ?>
-				<?php endif; ?>
-            </td>
-            <td align="center">
-				<?php echo (int)$item->id; ?>
-            </td>
-        </tr>
-			<?php endforeach; ?>
-        </tbody>
-    </table>
 
     <div>
         <input type="hidden" name="task" value=""/>
