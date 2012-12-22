cf8a3072758c4b78b0452d45330a5e6476db8d51
Brent Cordis <skeebrent@gmail.com>
2012-12-22 09:51:15 -0600

    Finish the site models folder. phpcs

--- com_biblestudy/site/models/fields/topics.php
+++ com_biblestudy/site/models/fields/topics.php
@@ -1,11 +1,9 @@
 <?php
-
 /**
- * Topics Field
- * @package BibleStudy.Site
- * @copyright (C) 2007 - 2011 Joomla Bible Study Team All rights reserved
- * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
- * @link http://www.JoomlaBibleStudy.org
+ * @package    BibleStudy.Site
+ * @copyright  (C) 2007 - 2011 Joomla Bible Study Team All rights reserved
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link       http://www.JoomlaBibleStudy.org
  * */
 // No Direct Access
 defined('_JEXEC') or die;
@@ -17,31 +15,34 @@ jimport('joomla.form.formfield');
 /**
  * Form Field class for the Joomla Framework.
  *
- * @package		BibleStudy.Site
- * @subpackage	Form
- * @since		7.0.0
+ * @package     BibleStudy.Site
+ * @subpackage  Form
+ * @since       7.0.0
  */
-class JFormFieldTopics extends JFormField {
+class JFormFieldTopics extends JFormField
+{
 
-    /**
-     * Topics
-     * @var type
-     */
-    public $type = 'Topics';
+	/**
+	 * Topics
+	 *
+	 * @var string
+	 */
+	public $type = 'Topics';
 
-    /**
-     * Method to get the field input markup.
-     *
-     * @return  string  The field input markup.
-     *
-     * @since   11.1
-     */
-    protected function getInput() {
+	/**
+	 * Method to get the field input markup.
+	 *
+	 * @return  string  The field input markup.
+	 *
+	 * @since   11.1
+	 */
+	protected function getInput()
+	{
 
-        return '
+		return '
             <input type="hidden" id="topics" name="jform[topics]"/>
             ';
-    }
+	}
 
 }
 
--- com_biblestudy/site/models/fields/topicslist.php
+++ com_biblestudy/site/models/fields/topicslist.php
@@ -1,11 +1,9 @@
 <?php
-
 /**
- * TopicsList
- * @package BibleStudy.Site
- * @copyright (C) 2007 - 2011 Joomla Bible Study Team All rights reserved
- * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
- * @link http://www.JoomlaBibleStudy.org
+ * @package    BibleStudy.Site
+ * @copyright  (C) 2007 - 2011 Joomla Bible Study Team All rights reserved
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link       http://www.JoomlaBibleStudy.org
  * */
 // No Direct Access
 defined('_JEXEC') or die;
@@ -13,42 +11,49 @@ defined('_JEXEC') or die;
 // import the list field type
 jimport('joomla.form.helper');
 JFormHelper::loadFieldClass('list');
-include_once (JPATH_ADMINISTRATOR . DIRECTORY_SEPARATOR . 'components' . DIRECTORY_SEPARATOR . 'com_biblestudy' . DIRECTORY_SEPARATOR . 'helpers' . DIRECTORY_SEPARATOR . 'translated.php');
+include_once JPATH_ADMINISTRATOR . '/components/com_biblestudy/helpers/translated.php';
 
 /**
  * Topics List Form Field class for the Joomla Bible Study component
  * Displays a topics list of ALL published topics
- * @package BibleStudy.Site
- * @since 7.0.0
+ *
+ * @package  BibleStudy.Site
+ * @since    7.0.0
  */
-class JFormFieldTopicslist extends JFormFieldList {
+class JFormFieldTopicslist extends JFormFieldList
+{
 
-    /**
-     * The field type.
-     *
-     * @var         string
-     */
-    protected $type = 'Topicslist';
+	/**
+	 * The field type.
+	 *
+	 * @var         string
+	 */
+	protected $type = 'Topicslist';
 
-    /**
-     * Method to get a list of options for a list input.
-     *
-     * @return      array           An array of JHtml options.
-     */
-    protected function getOptions() {
-        $db = JFactory::getDBO();
-        $query = "SELECT id, topic_text, params AS topic_params FROM #__bsms_topics WHERE published = 1 ORDER by topic_text ASC";
-        $db->setQuery((string) $query);
-        $topics = $db->loadObjectList();
-        $options = array();
-        if ($topics) {
-            foreach ($topics as $topic) {
-                $text = getTopicItemTranslated($topic);
-                $options[] = JHtml::_('select.option', $topic->id, $text);
-            }
-        }
-        $options = array_merge(parent::getOptions(), $options);
-        return $options;
-    }
+	/**
+	 * Method to get a list of options for a list input.
+	 *
+	 * @return      array           An array of JHtml options.
+	 */
+	protected function getOptions()
+	{
+		$db    = JFactory::getDBO();
+		$query = "SELECT id, topic_text, params AS topic_params FROM #__bsms_topics WHERE published = 1 ORDER by topic_text ASC";
+		$db->setQuery((string) $query);
+		$topics  = $db->loadObjectList();
+		$options = array();
+
+		if ($topics)
+		{
+			foreach ($topics as $topic)
+			{
+				$text      = JBSMTranslated::getTopicItemTranslated($topic);
+				$options[] = JHtml::_('select.option', $topic->id, $text);
+			}
+		}
+		$options = array_merge(parent::getOptions(), $options);
+
+		return $options;
+	}
 
 }
