a4ae31a44f1fd5a521558c645e94619eb8e53631
Tom Fuller <tomfuller2@gmail.com>
2013-02-12 15:48:41 -0800

    Fixed incorrect path to the defines file. Added a missing language string

--- mod_biblestudy/mod_biblestudy.php
+++ mod_biblestudy/mod_biblestudy.php
@@ -11,7 +11,7 @@
  * */
 defined('_JEXEC') or die;
 
-require_once JPATH_ADMINISTRATOR . 'components/com_biblestudy/lib/biblestudy.defines.php';
+require_once JPATH_ADMINISTRATOR . '/components/com_biblestudy/lib/biblestudy.defines.php';
 require_once BIBLESTUDY_PATH_LIB . '/biblestudy.pagebuilder.class.php';
 
 require_once __DIR__ . '/helper.php';
