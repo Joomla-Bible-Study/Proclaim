0ffd01b3727c545df185a0f521b57686d1f653b9
Brent Cordis <skeebrent@gmail.com>
2013-02-14 07:51:00 -0600

    Adding the start of Migration view.

---
+++ com_biblestudy/admin/views/migration/tmpl/default.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Default
+ *
+ * @package    BibleStudy.Admin
+ * @copyright  (C) 2007 - 2011 Joomla Bible Study Team All rights reserved
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link       http://www.JoomlaBibleStudy.org
+ * */
+
+// Protect from unauthorized access
+defined('_JEXEC') or die();
+
+if(version_compare(JVERSION, '3.0', 'ge')) {
+	JHTML::_('behavior.framework');
+	JHtml::_('behavior.modal');
+} else {
+	JHTML::_('behavior.mootools');
+}
+?>
+<?php if ($this->more): ?>
+<h1><?php echo JText::_('JBS_MIG_WORKING'); ?></h1>
+<?php else: ?>
+<h1><?php echo JText::_('JBS_MIG_MIGRATION_DONE'); ?></h1>
+<?php endif; ?>
+
+
+<div class="progress progress-striped active">
+    <div class="bar" style="width: <?php echo $this->percentage ?>%"></div>
+</div>
+
+<form action="index.php" name="adminForm" id="adminForm">
+    <input type="hidden" name="option" value="com_biblestudy"/>
+    <input type="hidden" name="view" value="migration"/>
+    <input type="hidden" name="task" value="run"/>
+    <input type="hidden" name="tmpl" value="component"/>
+</form>
+
+<?php if (!$this->more): ?>
+<div class="alert alert-info">
+    <p><?php echo JText::_('JBS_MIG__LBL_AUTOCLOSE_IN_3S'); ?></p>
+</div>
+<script type="text/javascript">
+    window.setTimeout('closeme();', 3000);
+    function closeme() {
+        parent.SqueezeBox.close();
+    }
+</script>
+<?php endif; ?>
---
+++ com_biblestudy/admin/views/migration/view.html.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * @package    BibleStudy.Admin
+ * @copyright  (C) 2007 - 2011 Joomla Bible Study Team All rights reserved
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link       http://www.JoomlaBibleStudy.org
+ * */
+// No Direct Access
+defined('_JEXEC') or die;
+
+
+/**
+ * View class for Migration
+ *
+ * @package  BibleStudy.Admin
+ * @since    8.0.0
+ */
+class BiblestudyViewMessagetypes extends JViewLegacy
+{
+
+	/**
+	 * Items
+	 *
+	 * @var object
+	 */
+	protected $items;
+
+	/**
+	 * Pagination
+	 *
+	 * @var object
+	 */
+	protected $pagination;
+
+	/**
+	 * State
+	 *
+	 * @var object
+	 */
+	protected $state;
+
+	/**
+	 * @var object
+	 */
+	protected $canDo;
+
+	/**
+	 * @var array
+	 */
+	public $f_levels;
+
+	public $sidebar;
+
+	/**
+	 * Execute and display a template script.
+	 *
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 *
+	 * @return  mixed  A string if successful, otherwise a JError object.
+	 *
+	 * @see     fetch()
+	 * @since   11.1
+	 */
+	public function display($tpl = null)
+	{
+		$this->items      = $this->get('Items');
+		$this->pagination = $this->get('Pagination');
+		$this->state      = $this->get('State');
+		$this->canDo      = JBSMBibleStudyHelper::getActions('', 'migration');
+
+		// Check for errors
+		if (count($errors = $this->get('Errors')))
+		{
+			JFactory::getApplication()->enqueueMessage(implode("\n", $errors), 'error');
+
+			return false;
+		}
+
+		// Set the document
+		$this->setDocument();
+
+		// Display the template
+		return parent::display($tpl);
+	}
+
+	/**
+	 * Add the page title to browser.
+	 *
+	 * @return void
+	 *
+	 * @since    7.1.0
+	 */
+	protected function setDocument()
+	{
+		$document = JFactory::getDocument();
+		$document->setTitle(JText::_('JBS_TITLE_MESSAGE_TYPES'));
+	}
+}
