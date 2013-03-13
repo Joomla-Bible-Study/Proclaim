48844c382b7d7142098a017904437bff1ff0902d
tomfuller2 <tomfuller2@gmail.com>
2013-03-13 07:59:26 -0700

    Changed one thing in servers batch but it didn't fix the problem.

--- com_biblestudy/admin/views/servers/tmpl/default_batch.php
+++ com_biblestudy/admin/views/servers/tmpl/default_batch.php
@@ -33,7 +33,7 @@ $published = $this->state->get('filter.published');
                 data-dismiss="modal">
 			<?php echo JText::_('JCANCEL'); ?>
         </button>
-        <button class="btn btn-primary" type="submit" onclick="Joomla.submitbutton('location.batch');">
+        <button class="btn btn-primary" type="submit" onclick="Joomla.submitbutton('server.batch');">
 			<?php echo JText::_('JGLOBAL_BATCH_PROCESS'); ?>
         </button>
     </div>
