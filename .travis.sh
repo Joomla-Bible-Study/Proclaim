b78a752a128f19886268f8638e7d6c910594a071
Eugen Istoc <eugenistoc@gmail.com>
2013-03-22 19:10:46 -0300

    hotfix

--- .travis.sh
+++ .travis.sh
@@ -5,14 +5,14 @@ pear channel-discover pear.pdepend.org
 pear channel-discover pear.phpmd.org
 pear channel-discover pear.phpdoc.org
 
-pear install phing/phing
-pear install pdepend/PHP_Depend-beta
-pear install pear.phpunit.de/PHPUnit
-pear install phpmd/PHP_PMD
-pear install PHP_CodeSniffer-1.5.0RC1
-pear install pear.phpunit.de/phpcpd
-pear install pear.phpunit.de/phploc
-pear install phpdoc/phpDocumentor-alpha
+#pear install phing/phing
+#pear install pdepend/PHP_Depend-beta
+#pear install pear.phpunit.de/PHPUnit
+#pear install phpmd/PHP_PMD
+#pear install PHP_CodeSniffer-1.5.0RC1
+#pear install pear.phpunit.de/phpcpd
+#pear install pear.phpunit.de/phploc
+#pear install phpdoc/phpDocumentor-alpha
 
 phpenv rehash
 
