22ceae5c108edd9d5495af173bf8a09ab439028f
Eugen Istoc <eugenistoc@gmail.com>
2013-03-22 18:38:44 -0300

    hotfix

--- .travis.sh
+++ .travis.sh
@@ -1,8 +1,16 @@
 echo "[Installing Phing and all required dependencies]"
+pear config-set auto_discover 1
 pear channel-discover pear.phing.info
-#pear channel-discover pear.pdepend.org
-pear install --alldeps phing/phing
-#pear install pdepend/PHP_Depend-beta
+pear channel-discover pear.pdepend.org
+pear channel-discover pear.phpmd.org
+
+pear install phing/phing
+pear install pdepend/PHP_Depend-beta
+pear install pear.phpunit.de/PHPUnit
+pear install phpmd/PHP_PMD
+pear install PHP_CodeSniffer-1.5.0RC1
+pear install pear.phpunit.de/phpcpd
+pear install pear.phpunit.de/phploc
 
 phpenv rehash
 
