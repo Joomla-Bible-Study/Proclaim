84ec743bf9570fc5990085b752c9218a670784f2
Eugen Istoc <eugenistoc@gmail.com>
2013-03-22 12:11:58 -0300

    Add phing compatibility

--- .travis-phing.sh
+++ .travis-phing.sh
@@ -8,24 +8,48 @@
 
 installPearTask ()
 {
+    sudo apt-get update -qq
+    sudo apt-get install -qq php5-xdebug
+    
     echo -e "\nAuto-discover pear channels and upgrade ..."
     pear config-set auto_discover 1
     pear -qq channel-update pear.php.net
     pear -qq upgrade
+    pear -qq channel-discover pear.phing.info
     echo "... OK"
 
-    echo -e "\nInstalling / upgrading phing ... "
-    which phing >/dev/null                      &&
-        pear upgrade pear.phing.info/phing ||
-        pear install --alldeps pear.phing.info/phing
+    echo -e "\nInstalling / upgrading phpcpd ... "
+    which phpcpd >/dev/null &&
+        pear upgrade pear.phpunit.de/phpcpd ||
+        pear install pear.phpunit.de/phpcpd
+    phpenv rehash
+
+    echo -e "\nInstalling / upgrading phploc ... "
+    which phploc >/dev/null &&
+        pear upgrade pear.phpunit.de/phploc ||
+        pear install pear.phpunit.de/phploc
+    phpenv rehash
+
+    echo -e "\nInstalling / upgrading phpcs ... "
+    which phpcs >/dev/null &&
+        pear upgrade pear.php.net/PHP_CodeSniffer ||
+        pear install pear.php.net/PHP_CodeSniffer
+    phpenv rehash
+    # re-test for phpcs:
+    phpcs --version 2>&1 >/dev/null &&
+        echo "... OK" ||
+        return 1
+
+    sudo apt-get install python-docutils
+    pear install VersionControl_Git-alpha
+    pear install VersionControl_SVN-alpha
+    pear install pear/XML_Serializer-beta
+    pear install --alldeps PEAR_PackageFileManager
+    pear install --alldeps PEAR_PackageFileManager2
+    pear install Net_Growl
 
     # update paths
     phpenv rehash
-
-    # re-test for phing:
-    phing -v 2>&1 >/dev/null    &&
-        echo "... OK"           ||
-        return 1
 }
 
 
@@ -35,6 +59,16 @@ installPearTask ()
         echo -e "\nSUCCESS - PHP ENVIRONMENT READY." ||
         ( echo "=== FAILED."; exit 1 )
 
-    phing $*
+    if [[ $TRAVIS_PHP_VERSION < 5.3 ]]; then
+pear install -f phpunit/File_Iterator-1.3.2
+     pear install -f phpunit/PHP_TokenStream-1.1.4
+     pear install -f phpunit/PHP_Timer-1.0.3
+     pear install -f phpunit/Text_Template-1.1.1
+    else
+composer install
+    fi
+
+../bin/phing
+
 
 #------------------------------------------------------- eof
