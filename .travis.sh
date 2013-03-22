3f7720301a98f64cd1a043c4c65f4e31f1a859bd
Eugen Istoc <eugenistoc@gmail.com>
2013-03-22 18:26:47 -0300

    hotfix

--- .travis.sh
+++ .travis.sh
@@ -1,8 +1,8 @@
 echo "[Installing Phing and all required dependencies]"
 pear channel-discover pear.phing.info
-pear channel-discover pear.pdepend.org
-pear install phing/phing
-pear install pdepend/PHP_Depend-beta
+#pear channel-discover pear.pdepend.org
+pear install --alldeps phing/phing
+#pear install pdepend/PHP_Depend-beta
 
 phpenv rehash
 
