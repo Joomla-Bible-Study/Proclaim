67a849553b721dc375be792ad077b4005992f013
Brent Cordis <skeebrent@gmail.com>
2013-09-07 11:10:22 -0500

    Add system to not show pageheader if using JCE

--- mod_biblestudy/tmpl/default_main.php
+++ mod_biblestudy/tmpl/default_main.php
@@ -19,7 +19,9 @@ JLoader::register('JBSMListing', BIBLESTUDY_PATH_LIB . '/biblestudy.listing.clas
 $JBSMListing = new JBSMListing;
 ?>
 <div id="biblestudy" class="noRefTagger">
-	<div id="jbsmoduleheader"><?php echo $params->get('pageheader'); ?></div>
+	<?php if($params->get('pageheader') != '<div>&nbsp;</div>') : ?>
+		<div id="jbsmoduleheader"><?php echo $params->get('pageheader'); ?></div>
+	<?php endif; ?>
 	<!-- This div is the container for the whole page -->
 	<table id="bsmsmoduletable">
 		<?php
