44ec915b5250e884f2bceaeb66fd3857669ccfe5
Eugen Istoc <eugenistoc@gmail.com>
2014-07-07 00:38:11 -0400

    Fix bug causing improper addon loanguage loading

--- com_biblestudy/admin/models/mediafile.php
+++ com_biblestudy/admin/models/mediafile.php
@@ -182,7 +182,7 @@ class BiblestudyModelMediafile extends JModelAdmin
 
         // Add language files
         $lang = JFactory::getLanguage();
-        if(!$lang->load('jbs_addon_legacy', JPATH_ADMINISTRATOR.'/components/com_biblestudy/addons/servers/'.$server_type))
+        if(!$lang->load('jbs_addon_'.$server_type, JPATH_ADMINISTRATOR.'/components/com_biblestudy/addons/servers/'.$server_type))
             throw new Exception(JText::_('JBS_ERR_ADDON_LANGUAGE_NOT_LOADED'));
 
         $form = $this->loadForm('com_biblestudy.mediafile.media', "media", array('control' => 'jform', 'load_data' => true), true, "/media");
