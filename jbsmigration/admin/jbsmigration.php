f0a02564d3f5c7076f6f23689dda2935d35d06fa
Brent Cordis <skeebrent@gmail.com>
2014-09-13 09:04:42 -0500

    Update Copyright and removed folder reference to migration.

--- jbsmigration/admin/jbsmigration.php
+++ jbsmigration/admin/jbsmigration.php
@@ -5,7 +5,7 @@
  *
  * @package    BibleStudy
  * @subpackage JBSMigration.Admin
- * @copyright  (C) 2007 - 2013 Joomla Bible Study Team All rights reserved
+ * @copyright  (C) 2007 - 2014 Joomla Bible Study Team All rights reserved
  * @license    http://www.gnu.org/copyleft/gpl.html GNU/GPL
  * @link       http://www.JoomlaBibleStudy.org
  * */
