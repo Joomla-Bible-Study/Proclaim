0f0f17871835883ca203e2c52b255d688d4105fd
Eugen Istoc <eugenistoc@gmail.com>
2014-09-19 23:58:45 -0400

    Create thumbnail before saving teacher

--- com_biblestudy/admin/models/teacher.php
+++ com_biblestudy/admin/models/teacher.php
@@ -21,180 +21,200 @@ jimport('joomla.application.component.modeladmin');
 class BiblestudyModelTeacher extends JModelAdmin
 {
 
-	/**
-	 * Controller Prefix
-	 *
-	 * @var        string    The prefix to use with controller messages.
-	 * @since    1.6
-	 */
-	protected $text_prefix = 'COM_BIBLESTUDY';
+    /**
+     * Controller Prefix
+     *
+     * @var        string    The prefix to use with controller messages.
+     * @since    1.6
+     */
+    protected $text_prefix = 'COM_BIBLESTUDY';
 
-	/**
-	 * Method to get a table object, load it if necessary.
-	 *
-	 * @param   string $name     The table name. Optional.
-	 * @param   string $prefix   The class prefix. Optional.
-	 * @param   array  $options  Configuration array for model. Optional.
-	 *
-	 * @return  JTable  A JTable object
-	 *
-	 * @since    1.7.0
-	 */
-	public function getTable($name = 'Teacher', $prefix = 'Table', $options = array())
-	{
-		return JTable::getInstance($name, $prefix, $options);
-	}
+    /**
+     * Method to get a table object, load it if necessary.
+     *
+     * @param   string $name The table name. Optional.
+     * @param   string $prefix The class prefix. Optional.
+     * @param   array $options Configuration array for model. Optional.
+     *
+     * @return  JTable  A JTable object
+     *
+     * @since    1.7.0
+     */
+    public function getTable($name = 'Teacher', $prefix = 'Table', $options = array())
+    {
+        return JTable::getInstance($name, $prefix, $options);
+    }
 
-	/**
-	 * Method to get a single record.
-	 *
-	 * @param   int $pk  The id of the primary key.
-	 *
-	 * @return    mixed    Object on success, false on failure.
-	 *
-	 * @since    1.7.0
-	 */
-	public function getItem($pk = null)
-	{
-		$item = parent::getItem($pk);
+    /**
+     * Method to get a single record.
+     *
+     * @param   int $pk The id of the primary key.
+     *
+     * @return    mixed    Object on success, false on failure.
+     *
+     * @since    1.7.0
+     */
+    public function getItem($pk = null)
+    {
+        $item = parent::getItem($pk);
 
-		if ($item)
-		{
-			// Convert the params field to an array.
-		}
+        if ($item) {
+            // Convert the params field to an array.
+        }
 
-		return $item;
-	}
+        return $item;
+    }
 
-	/**
-	 * Get the form data
-	 *
-	 * @param   array   $data      Data for the form.
-	 * @param   boolean $loadData  True if the form is to load its own data (default case), false if not.
-	 *
-	 * @return  mixed  A JForm object on success, false on failure
-	 *
-	 * @since 7.0
-	 */
-	public function getForm($data = array(), $loadData = true)
-	{
+    /**
+     * Get the form data
+     *
+     * @param   array $data Data for the form.
+     * @param   boolean $loadData True if the form is to load its own data (default case), false if not.
+     *
+     * @return  mixed  A JForm object on success, false on failure
+     *
+     * @since 7.0
+     */
+    public function getForm($data = array(), $loadData = true)
+    {
 
-		JForm::addFieldPath('JPATH_ADMINISTRATOR/components/com_users/models/fields');
+        JForm::addFieldPath('JPATH_ADMINISTRATOR/components/com_users/models/fields');
 
-		// Get the form.
-		$form = $this->loadForm('com_biblestudy.teacher', 'teacher', array('control' => 'jform', 'load_data' => $loadData));
+        // Get the form.
+        $form = $this->loadForm('com_biblestudy.teacher', 'teacher', array('control' => 'jform', 'load_data' => $loadData));
 
-		if (empty($form))
-		{
-			return false;
-		}
+        if (empty($form)) {
+            return false;
+        }
 
-		// Modify the form based on access controls.
-		if (!$this->canEditState((object) $data))
-		{
-			// Disable fields for display.
-			$form->setFieldAttribute('ordering', 'disabled', 'true');
-			$form->setFieldAttribute('published', 'disabled', 'true');
+        // Modify the form based on access controls.
+        if (!$this->canEditState((object)$data)) {
+            // Disable fields for display.
+            $form->setFieldAttribute('ordering', 'disabled', 'true');
+            $form->setFieldAttribute('published', 'disabled', 'true');
 
-			// Disable fields while saving.
-			// The controller has already verified this is a record you can edit.
-			$form->setFieldAttribute('ordering', 'filter', 'unset');
-			$form->setFieldAttribute('published', 'filter', 'unset');
-		}
+            // Disable fields while saving.
+            // The controller has already verified this is a record you can edit.
+            $form->setFieldAttribute('ordering', 'filter', 'unset');
+            $form->setFieldAttribute('published', 'filter', 'unset');
+        }
 
-		return $form;
-	}
+        return $form;
+    }
 
-	/**
-	 * Method to get the data that should be injected in the form.
-	 *
-	 * @return    mixed    The data for the form.
-	 *
-	 * @since   7.0
-	 */
-	protected function loadFormData()
-	{
-		// Check the session for previously entered form data.
-		$data = JFactory::getApplication()->getUserState('com_biblestudy.edit.teacher.data', array());
+    /**
+     * Method to get the data that should be injected in the form.
+     *
+     * @return    mixed    The data for the form.
+     *
+     * @since   7.0
+     */
+    protected function loadFormData()
+    {
+        // Check the session for previously entered form data.
+        $data = JFactory::getApplication()->getUserState('com_biblestudy.edit.teacher.data', array());
 
-		if (empty($data))
-		{
-			$data = $this->getItem();
-		}
+        if (empty($data)) {
+            $data = $this->getItem();
+        }
 
-		return $data;
-	}
+        return $data;
+    }
 
 
-	/**
-	 * Prepare and sanitise the table prior to saving.
-	 *
-	 * @param   JTable $table  A reference to a JTable object.
-	 *
-	 * @return    void
-	 *
-	 * @since    1.6
-	 */
-	protected function prepareTable($table)
-	{
-		jimport('joomla.filter.output');
-		$date = JFactory::getDate();
-		$user = JFactory::getUser();
+    /**
+     * Prepare and sanitise the table prior to saving.
+     *
+     * @param   JTable $table A reference to a JTable object.
+     *
+     * @return    void
+     *
+     * @since    1.6
+     */
+    protected function prepareTable($table)
+    {
+        jimport('joomla.filter.output');
+        $date = JFactory::getDate();
+        $user = JFactory::getUser();
 
-		$table->teachername = htmlspecialchars_decode($table->teachername, ENT_QUOTES);
-		$table->alias       = JApplication::stringURLSafe($table->alias);
+        $table->teachername = htmlspecialchars_decode($table->teachername, ENT_QUOTES);
+        $table->alias = JApplication::stringURLSafe($table->alias);
 
-		if (empty($table->alias))
-		{
-			$table->alias = JApplication::stringURLSafe($table->teachername);
-		}
+        if (empty($table->alias)) {
+            $table->alias = JApplication::stringURLSafe($table->teachername);
+        }
 
-		if (empty($table->id))
-		{
+        if (empty($table->id)) {
 
-			// Set ordering to the last item if not set
-			if (empty($table->ordering))
-			{
-				$db    = JFactory::getDbo();
-				$query = $db->getQuery(true);
-				$query->select('MAX(ordering)')->from('#__bsms_teachers');
-				$db->setQuery($query);
-				$max = $db->loadResult();
+            // Set ordering to the last item if not set
+            if (empty($table->ordering)) {
+                $db = JFactory::getDbo();
+                $query = $db->getQuery(true);
+                $query->select('MAX(ordering)')->from('#__bsms_teachers');
+                $db->setQuery($query);
+                $max = $db->loadResult();
 
-				$table->ordering = $max + 1;
-			}
-		}
-	}
+                $table->ordering = $max + 1;
+            }
+        }
+    }
 
-	/**
-	 * Method to check-out a row for editing.
-	 *
-	 * @param   integer $pk  The numeric id of the primary key.
-	 *
-	 * @return  boolean  False on failure or error, true otherwise.
-	 *
-	 * @since   11.1
-	 */
-	public function checkout($pk = null)
-	{
-		return $pk;
-	}
+    /**
+     * Method to check-out a row for editing.
+     *
+     * @param   integer $pk The numeric id of the primary key.
+     *
+     * @return  boolean  False on failure or error, true otherwise.
+     *
+     * @since   11.1
+     */
+    public function checkout($pk = null)
+    {
+        return $pk;
+    }
 
-	/**
-	 * Custom clean the cache of com_biblestudy and biblestudy modules
-	 *
-	 * @param   string  $group      The cache group
-	 * @param   integer $client_id  The ID of the client
-	 *
-	 * @return  void
-	 *
-	 * @since    1.6
-	 */
-	protected function cleanCache($group = null, $client_id = 0)
-	{
-		parent::cleanCache('com_biblestudy');
-		parent::cleanCache('mod_biblestudy');
-	}
+    /**
+     * Custom clean the cache of com_biblestudy and biblestudy modules
+     *
+     * @param   string $group The cache group
+     * @param   integer $client_id The ID of the client
+     *
+     * @return  void
+     *
+     * @since    1.6
+     */
+    protected function cleanCache($group = null, $client_id = 0)
+    {
+        parent::cleanCache('com_biblestudy');
+        parent::cleanCache('mod_biblestudy');
+    }
 
+    /**
+     * Saves data creating image thumbnails
+     *
+     * @param array $data
+     * @return bool
+     *
+     * @since 8.1.0
+     */
+    public function save($data)
+    {
+        $input = JFactory::getApplication()->input;
+        $data = $input->get('jform', false, 'array');
+        $files = $input->files->get('jform');
 
+        // If no image uploaded, just save data as usual
+        if (empty($files['image']['tmp_name'])) {
+            return parent::save($data);
+        }
+
+        $path = 'images/BibleStudy/teachers/' . JStringNormalise::toCamelCase($data['teachername']);
+        JBSMThumbnail::create($files['image'], $path);
+
+        // Modify model data
+        $data['teacher_image'] = $path . '/original_' . $files['image']['name'];
+        $data['teacher_thumbnail'] = $path . '/thumb_' . $files['image']['name'];
+
+        return parent::save($data);
+    }
 }
