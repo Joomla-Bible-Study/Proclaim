158f64bc4d53df5b8684630e2727f7ccf819b363
Eugen Istoc <eugenistoc@gmail.com>
2014-09-30 01:22:37 -0400

    Recreate all thumbnails when changing the global default size

--- com_biblestudy/admin/helpers/thumbnail.php
+++ com_biblestudy/admin/helpers/thumbnail.php
@@ -4,33 +4,48 @@ defined('_JEXEC') or die;
 class JBSMThumbnail
 {
 
-	/**
-	 * Creates a thumbnail for an uploaded image
-	 *
-	 * @param      $file
-	 * @param      $path
-	 * @param null $size
-	 *
-	 * @since 8.1.0
-	 */
-	public static function create($file, $path, $size = 100)
-	{
-		$original = JPATH_ROOT . '/' . $path . '/original_' . $file['name'];
-		$thumb    = JPATH_ROOT . '/' . $path . '/thumb_' . $file['name'];
+    /**
+     * Creates a thumbnail for an uploaded image
+     *
+     * @param      $file
+     * @param      $path
+     * @param null $size
+     *
+     * @since 8.1.0
+     */
+    public static function create($file, $path, $size = 100)
+    {
+        $original = JPATH_ROOT . '/' . $path . '/original_' . $file['name'];
+        $thumb = JPATH_ROOT . '/' . $path . '/thumb_' . $file['name'];
 
-		// Delete destination folder if it exists
-		if (JFolder::exists(JPATH_ROOT . '/' . $path))
-		{
-			JFolder::delete(JPATH_ROOT . '/' . $path);
-		}
+        // Delete destination folder if it exists
+        if (JFolder::exists(JPATH_ROOT . '/' . $path)) {
+            JFolder::delete(JPATH_ROOT . '/' . $path);
+        }
 
-		// Move uploaded image to destination
-		JFolder::create(JPATH_ROOT . '/' . $path);
-		JFile::move($file[tmp_name], $original);
+        // Move uploaded image to destination
+        JFolder::create(JPATH_ROOT . '/' . $path);
+        JFile::move($file[tmp_name], $original);
 
-		// Create thumbnail
-		$image     = new JImage($original);
-		$thumbnail = $image->resize($size, $size);
-		$thumbnail->toFile($thumb, IMAGETYPE_PNG);
-	}
+        // Create thumbnail
+        $image = new JImage($original);
+        $thumbnail = $image->resize($size, $size);
+        $thumbnail->toFile($thumb, IMAGETYPE_PNG);
+    }
+
+    public static function resize($path, $new_size)
+    {
+        $filename = str_replace('original_', '', basename($path));
+
+        // Delete existing thumbnail
+        $old_thumbs = JFolder::files(dirname($path), 'thumb_', true, true);
+        foreach ($old_thumbs as $thumb) {
+            Jfile::delete($thumb);
+        }
+
+        //Create new thumbnail
+        $image = new JImage($path);
+        $thumbnail = $image->resize($new_size, $new_size);
+        $thumbnail->toFile(dirname($path) . '/thumb_' . $filename, IMAGETYPE_PNG);
+    }
 }
