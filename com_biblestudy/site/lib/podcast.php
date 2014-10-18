d41151e28caef64eea9ee5932a628f9371626ab3
Brent Cordis <skeebrent@gmail.com>
2014-10-17 21:46:25 -0500

    Update system to remove Folder and ols server code.

--- com_biblestudy/site/lib/podcast.php
+++ com_biblestudy/site/lib/podcast.php
@@ -66,7 +66,7 @@ class JBSMPodcast
 
 				if ($checkresult)
 				{
-					$description       = $this->escapeHTML($podinfo->description);;
+					$description = $this->escapeHTML($podinfo->description);
 					$detailstemplateid = $podinfo->detailstemplateid;
 					$podcastimage      = $this->jimage($podinfo->image);
 					if (!$detailstemplateid)
@@ -322,38 +322,38 @@ class JBSMPodcast
 							) . ':' . sprintf("%02d", $episode->media_seconds) . '</itunes:duration>';
 
 						// Here is where we test to see if the link should be an article or docMan link, otherwise it is a mediafile
-						if ($episode->article_id > 1)
+						if ($episode->params->get('article_id') > 1)
 						{
 							$episodedetailtemp .=
 								'<enclosure url="http://' . $episode->server_path .
 								'/index.php?option=com_content&amp;view=article&amp;id=' .
-								$episode->article_id . '" length="' . $episode->size . '" type="' .
-								$episode->mimetype . '" />
+								$episode->params->get('article_id') . '" length="' . $episode->params->get('size') . '" type="' .
+								$episode->params->get('mimetype') . '" />
                         			<guid>http://' . $episode->server_path .
 								'/index.php?option=com_content&amp;view=article&amp;id=' .
-								$episode->article_id . '</guid>';
+								$episode->params->get('article_id') . '</guid>';
 						}
-						if ($episode->docMan_id > 1)
+						if ($episode->params->get('docMan_id') > 1)
 						{
 							$episodedetailtemp .=
 								'<enclosure url="http://' . $episode->server_path .
 								'/index.php?option=com_docman&amp;task=doc_download&amp;gid=' .
-								$episode->docMan_id . '" length="' . $episode->size . '" type="' .
-								$episode->mimetype . '" />
+								$episode->params->get('docMan_id') . '" length="' . $episode->params->get('size') . '" type="' .
+								$episode->params->get('mimetype') . '" />
                         			<guid>http://' . $episode->server_path .
 								'/index.php?option=com_docman&amp;task=doc_download&amp;gid=' .
-								$episode->docMan_id . '</guid>';
+								$episode->params->get('docMan_id') . '</guid>';
 						}
 						else
 						{
 							$episodedetailtemp .=
-								'<enclosure url="http://' . $episode->server_path . $episode->folderpath . str_replace(
+								'<enclosure url="http://' . str_replace(
 									' ',
 									"%20",
 									$episode->filename
-								) . '" length="' . $episode->size . '" type="'
+								) . '" length="' . $episode->params->get('size') . '" type="'
 								. $episode->mimetype . '" />
-                        			<guid>http://' . $episode->server_path . $episode->folderpath . str_replace(
+                        			<guid>http://' . str_replace(
 									' ',
 									"%20",
 									$episode->filename
@@ -464,15 +464,13 @@ class JBSMPodcast
 		$db    = JFactory::getDBO();
 		$query = $db->getQuery(true);
 		$query->select('p.id AS pid, p.podcastlimit,'
-		. ' mf.id AS mfid, mf.study_id, mf.server, mf.path, mf.filename, mf.size, mf.mime_type, mf.podcast_id,'
+			. ' mf.id AS mfid, mf.study_id, mf.server_id, mf.path, mf.filename, mf.podcast_id,'
 		. ' mf.published AS mfpub, mf.createdate, mf.params,'
-		. ' mf.docMan_id, mf.article_id,'
 		. ' s.id AS sid, s.studydate, s.teacher_id, s.booknumber, s.chapter_begin, s.verse_begin,'
 		. ' s.chapter_end, s.verse_end, s.studytitle, s.studyintro, s.published AS spub,'
 		. ' s.media_hours, s.media_minutes, s.media_seconds,'
 		. ' se.series_text,'
 		. ' sr.id AS srid, sr.server_path,'
-		. ' f.id AS fid, f.folderpath,'
 		. ' t.id AS tid, t.teachername,'
 		. ' b.id AS bid, b.booknumber AS bnumber, b.bookname,'
 		. ' mt.id AS mtid, mt.mimetype')
@@ -480,10 +478,8 @@ class JBSMPodcast
 			->leftJoin('#__bsms_studies AS s ON (s.id = mf.study_id)')
 			->leftJoin('#__bsms_series AS se ON (se.id = s.series_id)')
 			->leftJoin('#__bsms_servers AS sr ON (sr.id = mf.server)')
-			->leftJoin('#__bsms_folders AS f ON (f.id = mf.path)')
 			->leftJoin('#__bsms_books AS b ON (b.booknumber = s.booknumber)')
 			->leftJoin('#__bsms_teachers AS t ON (t.id = s.teacher_id)')
-			->leftJoin('#__bsms_mimetype AS mt ON (mt.id = mf.mime_type)')
 			->leftJoin('#__bsms_podcast AS p ON (p.id = mf.podcast_id)')
 			->where('mf.podcast_id LIKE ' . $db->q('%' . $id . '%'))->where('mf.published = ' . 1)->order('createdate desc');
 
@@ -494,6 +490,10 @@ class JBSMPodcast
 		$epis = array();
 		foreach ($episodes as $e)
 		{
+			$registry = new JRegistry;
+			$registry->loadString($e->params);
+			$e->params = $registry;
+
 			$e->podcast_id = str_replace('-1', '', $e->podcast_id);
 			if (substr_count($e->podcast_id, $id))
 			{
