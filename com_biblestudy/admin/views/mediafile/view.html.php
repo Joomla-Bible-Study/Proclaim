ed879c81b012a7b2b26bf0b6fb6238287bbefa13
Brent Cordis <skeebrent@gmail.com>
2015-01-10 17:25:43 -0600

    fixes filename in edit title.

--- com_biblestudy/admin/views/mediafile/view.html.php
+++ com_biblestudy/admin/views/mediafile/view.html.php
@@ -139,7 +139,7 @@ class BiblestudyViewMediafile extends JViewLegacy
 	{
 		$isNew    = ($this->item->id < 1);
 		$document = JFactory::getDocument();
-		$document->setTitle($isNew ? JText::_('JBS_TITLE_MEDIA_FILES_CREATING') : JText::sprintf('JBS_TITLE_MEDIA_FILES_EDITING', $this->item->filename));
+		$document->setTitle($isNew ? JText::_('JBS_TITLE_MEDIA_FILES_CREATING') : JText::_('JBS_TITLE_MEDIA_FILES_EDITING'));
 
 	}
 
