cba9eef2ce416cd00e5eb501a4eab04ed4e64de5
Brent Cordis <skeebrent@gmail.com>
2015-06-12 18:13:07 -0500

    Update jwplayer.php
    Extra ; in file.

--- com_biblestudy/admin/helpers/html/jwplayer.php
+++ com_biblestudy/admin/helpers/html/jwplayer.php
@@ -126,7 +126,7 @@ abstract class JHtmlJwplayer
 		}
 		$render .= "<script language=\"javascript\" type=\"text/javascript\">
 						jwplayer('placeholder" . $id . "').setup({
-							'file': '" . $media->path1 . "',;
+							'file': '" . $media->path1 . "',
 							";
 		if ($params->playerresponsive == 0)
 		{
