e002e6e8201f068a40496fa64bf0f2d48d215424
Tom Fuller <tomfuller2@gmail.com>
2016-04-01 13:58:11 -0700

    Fixed problem with PayPal donate button. Now works as simple a href.

--- com_biblestudy/admin/views/cpanel/tmpl/default.php
+++ com_biblestudy/admin/views/cpanel/tmpl/default.php
@@ -43,13 +43,7 @@ if ($msg)
 			</div>
 
 			<div id="jbspaypal">
-				<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
-					<input type="hidden" name="cmd" value="_s-xclick">
-					<input type="hidden" name="hosted_button_id" value="XMJT8SDW8RHZW">
-					<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-					<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-				</form>
-
+				<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7R9D3SCEYNAHE" target="_blank"><img src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" alt="PayPal - The safer, easier way to pay online!"> </a>
 			</div>
 			<hr/>
 			<?php echo $this->sidebar; ?>
