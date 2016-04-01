a3b618795e3924feda313a4c359e06c93804119f
Tom Fuller <tomfuller2@gmail.com>
2016-04-01 13:12:33 -0700

    Added _blank for form action of Donate button form. The form elements are being stripped out so this doesn't yet work.

--- com_biblestudy/admin/views/cpanel/tmpl/default.php
+++ com_biblestudy/admin/views/cpanel/tmpl/default.php
@@ -43,7 +43,7 @@ if ($msg)
 			</div>
 
 			<div id="jbspaypal">
-				<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+				<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
 					<input type="hidden" name="cmd" value="_s-xclick">
 					<input type="hidden" name="hosted_button_id" value="XMJT8SDW8RHZW">
 					<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
