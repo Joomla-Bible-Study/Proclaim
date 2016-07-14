2ea4f7c903cf581d86ec38bbc090ecbb7a14f4c4
Brent Cordis <skeebrent@gmail.com>
2016-07-13 19:26:18 -0500

    Joomla-Bible-Study-664 Series search fix. #664

--- com_biblestudy/admin/models/messagetypes.php
+++ com_biblestudy/admin/models/messagetypes.php
@@ -22,6 +22,7 @@ class BiblestudyModelMessagetypes extends JModelList
 	 * Number of Deletions
 	 *
 	 * @var int
+	 * @since 7.0.0
 	 */
 	private $_deletes;
 
@@ -29,6 +30,8 @@ class BiblestudyModelMessagetypes extends JModelList
 	 * Constructor.
 	 *
 	 * @param   array  $config  An optional associative array of configuration settings.
+	 *
+	 * @since 7.0.0
 	 */
 	public function __construct($config = array())
 	{
@@ -39,7 +42,7 @@ class BiblestudyModelMessagetypes extends JModelList
 				'published', 'messagetype.published',
 				'mesage_type', 'messagetype.message_type,',
 				'ordering', 'messagetype.ordering',
-				'access', 'messagetype.access',
+				'access', 'messagetype.access', 'access_level'
 			);
 		}
 
@@ -50,6 +53,8 @@ class BiblestudyModelMessagetypes extends JModelList
 	 * Get Deletes
 	 *
 	 * @return object
+	 *
+	 * @since 7.0.0
 	 */
 	public function getDeletes()
 	{
@@ -78,7 +83,7 @@ class BiblestudyModelMessagetypes extends JModelList
 	 *
 	 * @return  void
 	 *
-	 * @since   7.0
+	 * @since   7.0.0
 	 */
 	protected function populateState($ordering = null, $direction = null)
 	{
@@ -91,10 +96,44 @@ class BiblestudyModelMessagetypes extends JModelList
 			$this->context .= '.' . $layout;
 		}
 
+		$search = $this->getUserStateFromRequest($this->context . '.filter.search', 'filter_search');
+		$this->setState('filter.search', $search);
+
 		$published = $this->getUserStateFromRequest($this->context . '.filter.published', 'filter_published', '');
 		$this->setState('filter.published', $published);
 
-		parent::populateState('messagetype.message_type', 'ASC');
+		$language = $this->getUserStateFromRequest($this->context . '.filter.language', 'filter_language', '');
+		$this->setState('filter.language', $language);
+
+		$access = $this->getUserStateFromRequest($this->context . '.filter.access', 'filter_access', 0, 'int');
+		$this->setState('filter.access', $access);
+
+		parent::populateState('messagetype.message_type', 'asc');
+	}
+
+	/**
+	 * Method to get a store id based on model configuration state.
+	 *
+	 * This is necessary because the model is used by the component and
+	 * different modules that might need different sets of data or different
+	 * ordering requirements.
+	 *
+	 * @param   string  $id  A prefix for the store id.
+	 *
+	 * @return  string  A store id.
+	 *
+	 * @since   7.1.0
+	 */
+	protected function getStoreId($id = '')
+	{
+
+		// Compile the store id.
+		$id .= ':' . $this->getState('filter.search');
+		$id .= ':' . $this->getState('filter.messagetype');
+		$id .= ':' . $this->getState('filter.published');
+		$id .= ':' . $this->getState('filter.language');
+
+		return parent::getStoreId($id);
 	}
 
 	/**
@@ -102,12 +141,13 @@ class BiblestudyModelMessagetypes extends JModelList
 	 *
 	 * @return  JDatabaseQuery   A JDatabaseQuery object to retrieve the data set.
 	 *
-	 * @since   7.0
+	 * @since   7.0.0
 	 */
 	protected function getListQuery()
 	{
 		$db    = $this->getDbo();
 		$query = $db->getQuery(true);
+		$user  = JFactory::getUser();
 
 		$query->select(
 			$this->getState(
@@ -117,12 +157,37 @@ class BiblestudyModelMessagetypes extends JModelList
 
 		$query->from('#__bsms_message_type AS messagetype');
 
-		// Join over the asset groups.
-		$query->select('ag.title AS access_level');
-		$query->join('LEFT', '#__viewlevels AS ag ON ag.id = messagetype.access');
-
 		// Filter by published state
 		$published = $this->getState('filter.published');
+
+		// Filter by access level.
+		if ($access = $this->getState('filter.access'))
+		{
+			$query->where('messagetype.access = ' . (int) $access);
+		}
+
+		// Implement View Level Access
+		if (!$user->authorise('core.admin'))
+		{
+			$groups = implode(',', $user->getAuthorisedViewLevels());
+			$query->where('messagetype.access IN (' . $groups . ')');
+		}
+
+		// Filter by search in title.
+		$search = $this->getState('filter.search');
+
+		if (!empty($search))
+		{
+			if (stripos($search, 'id:') === 0)
+			{
+				$query->where('messagetype.id = ' . (int) substr($search, 3));
+			}
+			else
+			{
+				$search = $db->quote('%' . $db->escape($search, true) . '%');
+				$query->where('messagetype.message_type LIKE ' . $search);
+			}
+		}
 
 		if (is_numeric($published))
 		{
@@ -134,8 +199,8 @@ class BiblestudyModelMessagetypes extends JModelList
 		}
 
 		// Add the list ordering clause.
-		$orderCol  = $this->state->get('list.ordering');
-		$orderDirn = $this->state->get('list.direction');
+		$orderCol  = $this->state->get('list.ordering', 'messagetype.message_type');
+		$orderDirn = $this->state->get('list.direction', 'acs');
 		$query->order($db->escape($orderCol . ' ' . $orderDirn));
 
 		return $query;
