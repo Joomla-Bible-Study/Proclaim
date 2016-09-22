543afbffd42834527acb580216e85639950ee8bd
bcordis <skeebrent@gmail.com>
2016-09-22 17:52:16 -0500

    v9.0.5

--- com_biblestudy/admin/install/sql/uninstall-dbtables.sql
+++ com_biblestudy/admin/install/sql/uninstall-dbtables.sql
@@ -25,4 +25,3 @@ DROP TABLE IF EXISTS `#__bsms_update`;
 DROP TABLE IF EXISTS `#__bsms_share`;
 DROP TABLE IF EXISTS `#__bsms_storage`;
 DROP TABLE IF EXISTS `#__bsms_styles`;
-
---
+++ com_biblestudy/admin/install/sql/updates/mysql/9.0.5.sql
@@ -0,0 +1,2 @@
+INSERT INTO `#__bsms_update` (id, version) VALUES ('21', '9.0.5')
+ON DUPLICATE KEY UPDATE version = '9.0.5';
