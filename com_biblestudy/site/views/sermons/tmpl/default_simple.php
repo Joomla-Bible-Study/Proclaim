e87786f1348f3f4494dd1746da3c78f29ba0a89f
tomfuller2 <T&m121678$>
2018-03-30 11:33:16 -0700

    Adding styles and image width

--- com_biblestudy/site/views/sermons/tmpl/default_simple.php
+++ com_biblestudy/site/views/sermons/tmpl/default_simple.php
@@ -41,7 +41,7 @@ $columns   = 12;
 
 
 
-  <div class="row-fluid span12" style="background-color:#A9A9A9; margin:0 -5px; padding:8px 8px; border:1px solid #C5C1BE; position:relative; -webkit-border-radius:10px;">
+  <div class="row-fluid span12 dropdowns" style="background-color:#A9A9A9; margin:0 -5px; padding:8px 8px; border:1px solid #C5C1BE; position:relative; -webkit-border-radius:10px;">
 
     <?php
     echo $this->page->books;
@@ -57,8 +57,8 @@ $columns   = 12;
 	$oddeven = ($oddeven == $class1) ? $class2 : $class1;
 	?>
 	<div style="width:100%;">
-		<div class="span3"><div style="padding:12px 8px;line-height:22px;height:333px;">
-				<?php if ($study->study_thumbnail) {echo $study->study_thumbnail; echo '<br />';} ?>
+		<div class="span3"><div style="padding:12px 8px;line-height:22px;height:200px;">
+				<?php if ($study->study_thumbnail) {echo '<span style="max-width:250px; height:auto;">'.$study->study_thumbnail .'</span>'; echo '<br />';} ?>
 				<strong><?php echo $study->studytitle;?></strong><br />
 				<span style="color:#9b9b9b;"><?php echo $study->scripture1;?> | <?php echo $study->studydate;?></span><br />
 				<div style="font-size:85%;margin-bottom:-17px;max-height:122px;overflow:hidden;"><?php echo $study->teachername;?></div><br /><div style="background: rgba(0, 0, 0, 0) linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, white 100%) repeat scroll 0 0;bottom: 0;height: 32px;margin-top: -32px; position: relative;width: 100%;"></div>
@@ -68,6 +68,11 @@ $columns   = 12;
 
 	</div>
 <?php }?>
-<div class="row-fluid span12 pagination">
+<div class="row-fluid span12 pagination pagelinks" style="background-color: #A9A9A9;
+	margin: 0 -5px;
+	padding: 8px 8px;
+	border: 1px solid #C5C1BE;
+	position: relative;
+	-webkit-border-radius: 9px;">
 	<?php echo $this->pagination->getPageslinks();?>
 </div>
