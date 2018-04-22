e8bbe4512db788a5aa7822fafca785db572c5175
bcordis <skeebrent@gmail.com>
2018-04-22 17:51:35 -0500

    set default create element.

--- com_biblestudy/site/views/sermons/tmpl/default_main.php
+++ com_biblestudy/site/views/sermons/tmpl/default_main.php
@@ -21,24 +21,24 @@ $classelement = $listing->createelement($this->params->get('studies_element'));
 ?>
 
 <div class="container-fluid">
-	<div id="bsheader">
+    <div id="bsheader">
 		<?php
 		if ($this->params->get('showpodcastsubscribelist') == 1)
 		{
 			echo $this->subscribe;
 		}
 		?>
-	</div>
+    </div>
 	<?php if ($this->params->get('intro_show') > 0)
 	{
 		?>
-		<div class="hero-unit" style="padding-top:30px; padding-bottom:20px;">
+        <div class="hero-unit" style="padding-top:30px; padding-bottom:20px;">
 			<?php
 			if ($this->params->get('listteachers') && $this->params->get('list_teacher_show') > 0)
 			{
 				?>
-				<div class="row-fluid">
-					<ul class="thumbnails">
+                <div class="row-fluid">
+                    <ul class="thumbnails">
 						<?php $spans = 12 / count($teachers);
 						foreach ($teachers as $teacher)
 						{
@@ -55,7 +55,7 @@ $classelement = $listing->createelement($this->params->get('studies_element'));
 							if ($this->params->get('teacherlink') > 0)
 							{
 								echo '<div class="caption"><p><a href="index.php?option=com_biblestudy&view=teacher&id=' .
-										$teacher['id'] . '&t=' . $teacher['t'] . '">' . $teacher['name'] . '</a></p></div>';
+									$teacher['id'] . '&t=' . $teacher['t'] . '">' . $teacher['name'] . '</a></p></div>';
 							}
 							else
 							{
@@ -64,22 +64,35 @@ $classelement = $listing->createelement($this->params->get('studies_element'));
 							echo '</li>';
 						}
 						?>
-					</ul>
-				</div>
+                    </ul>
+                </div>
 			<?php } ?>
-			<div class="row-fluid">
-				<div class="span12">
+            <div class="row-fluid">
+                <div class="span12">
 					<?php if ($this->params->get('show_page_image') > 0)
+					{
+						?>
+                        <img class="imgcenter" src="<?php echo JUri::base() . $this->main->path; ?>" alt="">
+						<?php
+					}
+					if ($this->params->get('show_page_title') == 1)
 					{ ?>
-						<img class="imgcenter" src="<?php echo JUri::base() . $this->main->path; ?>" alt=""><?php } ?>
-					<?php if ($this->params->get('show_page_title') == 1)
-					{ ?><<?php echo $classelement; ?> style="<?php echo $this->params->get('list_title_align'); ?>
-					    "><?php echo $this->params->get('list_page_title'); ?></<?php echo $classelement; ?>><?php } ?>
-				<?php if ($this->params->get('list_intro'))
-				{ ?><p><?php echo $this->params->get('list_intro'); ?></p><?php } ?>
+                    <<?php echo $classelement; ?> style="<?php echo $this->params->get('list_title_align'); ?>
+                    ">
+					<?php echo $this->params->get('list_page_title'); ?>
+                </<?php echo $classelement; ?>>
+				<?php
+				}
+				if ($this->params->get('list_intro'))
+				{
+				    ?>
+                    <p>
+						<?php echo $this->params->get('list_intro'); ?>
+                    </p>
+				<?php } ?>
 
-			</div>
-		</div>
+            </div>
+        </div>
 	<?php } ?>
 </div>
 
@@ -88,18 +101,18 @@ $classelement = $listing->createelement($this->params->get('studies_element'));
 	// Search tools bar
 	echo JLayoutHelper::render('joomla.searchtools.default', array('view' => $this));
 	?>
-	<hr/>
+    <hr/>
 	<?php
-    if ($this->items)
-    {
-	    echo $listing->getFluidListing($this->items, $this->params, $this->template, $type = 'sermons');
-    }
-    ?>
+	if ($this->items)
+	{
+		echo $listing->getFluidListing($this->items, $this->params, $this->template, $type = 'sermons');
+	}
+	?>
 	<?php // Add pagination links ?>
 	<?php if (!empty($this->items)) : ?>
 		<?php if (($this->params->def('show_pagination', 2) == 1
-                || ($this->params->get('show_pagination') == 2))
-            && ($this->pagination->pagesTotal > 1)) : ?>
+				|| ($this->params->get('show_pagination') == 2))
+			&& ($this->pagination->pagesTotal > 1)) : ?>
             <div class="pagination">
 				<?php if ($this->params->def('show_pagination_results', 1)) : ?>
                     <p class="counter pull-right">
