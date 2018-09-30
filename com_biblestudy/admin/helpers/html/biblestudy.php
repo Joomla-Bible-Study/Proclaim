4b8bb44133d4771ed75cc80dc42b9191a9c15fd0
bcordis <skeebrent@gmail.com>
2018-09-29 20:01:12 -0500

    Update fontawesome js cnd info.

--- com_biblestudy/admin/helpers/html/biblestudy.php
+++ com_biblestudy/admin/helpers/html/biblestudy.php
@@ -105,7 +105,10 @@ abstract class JHtmlBiblestudy
 
 		if ($extra == 'font-awesome')
 		{
-			JHtml::script('https://use.fontawesome.com/ac3aa5180f.js');
+			JHtml::script('https://use.fontawesome.com/releases/v5.3.1/js/all.js',
+				[],
+				['integrity' => 'sha384-kW+oWsYx3YpxvjtZjFXqazFpA7UP/MbiY4jvs+RWZo2+N94PFZ36T6TFkc9O3qoB', 'crossorigin' => 'anonymous', 'defer' => 'defer']
+			);
 		}
 	}
 
