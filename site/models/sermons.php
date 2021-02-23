49a47fc9968cc0b289de739a62a3a4df72e67fc0
Brent Cordis <skeebrent@gmail.com>
2021-02-23 11:25:32 -0600

    Somthing to work on with Tom on #828

--- site/models/sermons.php
+++ site/models/sermons.php
@@ -306,16 +306,15 @@ class BiblestudyModelSermons extends JModelList
 
 		$this->setState('list.ordering', $orderCol);
 
+		// From landing page filter passing
 		$listOrder = $app->input->get('filter_order_Dir');
 
 		if (!in_array(strtoupper($listOrder), array('ASC', 'DESC', '')) && !empty($listOrder))
 		{
-			$listOrder = 'DESC';
+			$direction = 'DESC';
 		}
 
-		$this->setState('list.direction', $listOrder);
-
-		$this->setState('list.direction', $listOrder);
+		$this->setState('list.direction', $direction);
 
 		$this->setState('filter.location', $location);
 		$this->setState('filter.landinglocation', $location);
@@ -596,9 +595,9 @@ class BiblestudyModelSermons extends JModelList
 					{
 						foreach ($filtervalue as $filterid)
 						{
-							if ($filterid >= 1 && $filter !== 'study.booknumber')
+							if ((int) $filterid >= 1 && $filter !== 'study.booknumber')
 							{
-								if ($this->landing == 1)
+								if ((int) $this->landing === 1)
 								{
 									$$filterid = $this->getState($filter);
 								}
@@ -606,7 +605,7 @@ class BiblestudyModelSermons extends JModelList
 								$query->where($filter . ' = ' . (int) $filterid);
 							}
 
-							if ($filterid >= 1 && $filter === 'study.booknumber')
+							if ((int) $filterid >= 1 && $filter === 'study.booknumber')
 							{
 								$book = $filterid;
 								$chb  = $this->input->get('minChapt', '', 'int');
@@ -649,9 +648,6 @@ class BiblestudyModelSermons extends JModelList
 			}
 		}
 
-		// Set the secondary order
-		$this->setState('secondaryorderstate', 1);
-
 		// Filter by language
 		$language = $params->get('language', '*');
 
@@ -659,7 +655,7 @@ class BiblestudyModelSermons extends JModelList
 		{
 			$query->where('study.language in (' . $db->quote($this->getState('filter.languages')) . ',' . $db->quote('*') . ')');
 		}
-		elseif ($this->getState('filter.language') || $language !== '*')
+		elseif ($language !== '*' || $this->getState('filter.language'))
 		{
 			$query->where('study.language in (' . $db->quote(JFactory::getLanguage()->getTag()) . ',' . $db->quote('*') . ')');
 		}
@@ -683,14 +679,18 @@ class BiblestudyModelSermons extends JModelList
 		if (empty($orderCol) || $orderCol === " ")
 		{
 			$orderCol  = $this->state->get('list.ordering', 'study.studydate');
-			$orderDirn = $this->state->get('list.direction', 'DESC');
 
-			$secondaryorderstate = $this->getState('secondaryorderstate');
-
-			if (!empty($secondaryorderstate))
+			// Set order by menu if set. New Default is blank as of 9.2.5
+			if ($this->state->params->get('order') === '2')
 			{
-				$orderDirn = $params->get('default_order_secondary');
+				$this->state->set('list.direction', 'ASC');
 			}
+			elseif ($this->state->params->get('order') === '1')
+			{
+				$this->state->set('list.direction', 'DESC');
+			}
+
+			$orderDirn = $this->state->get('list.direction', 'DESC');
 		}
 
 		$query->order($db->escape($orderCol) . ' ' . $db->escape($orderDirn));
