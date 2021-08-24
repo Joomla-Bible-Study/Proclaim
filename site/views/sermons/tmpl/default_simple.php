1ffabe6cb78087f04215e6be783f7cd628321a46
Tom Fuller <tomfuller2@gmail.com>
2021-08-24 14:50:32 -0700

    Update default_simple.php

--- site/views/sermons/tmpl/default_simple.php
+++ site/views/sermons/tmpl/default_simple.php
@@ -3,7 +3,7 @@
 * Part of Proclaim Package
 *
  * @package    Proclaim.Admin
-* @copyright  2007 - 2019 (C) CWM Team All rights reserved
+* @copyright  2007 - 2021 (C) CWM Team All rights reserved
 * @license    http://www.gnu.org/copyleft/gpl.html GNU/GPL
  * @link       https://www.christianwebministries.org
  * */
