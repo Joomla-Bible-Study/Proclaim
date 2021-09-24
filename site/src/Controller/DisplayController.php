1e84625b7a168bb6f684cbe3d26153ccc6938554
Tom Fuller <tomfuller2@gmail.com>
2021-09-24 13:23:18 -0700

    Views deleted

--- site/src/Controller/DisplayController.php
+++ site/src/Controller/DisplayController.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Proclaim/Site
- * @subpackage  com_mywalks
+ * @subpackage  com_proclaim
  *
  * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
