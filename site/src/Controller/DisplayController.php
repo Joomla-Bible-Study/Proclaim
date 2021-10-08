24a16c4466fee0c34e8e7584294df0ee6cdbe866
Brent Cordis <skeebrent@gmail.com>
2021-10-08 17:14:25 -0500

    Local Changes

--- site/src/Controller/DisplayController.php
+++ site/src/Controller/DisplayController.php
@@ -20,26 +20,27 @@ use Joomla\CMS\MVC\Controller\BaseController;
  */
 class DisplayController extends BaseController
 {
-    /**
-     * The default view.
-     *
-     * @var    string
-     * @since  1.6
-     */
-    protected $default_view = 'CWMLandingPage';
+	/**
+	 * The default view.
+	 *
+	 * @var    string
+	 * @since  1.6
+	 */
+	protected $default_view = 'CWMLandingPage';
 
-    /**
-     * Method to display a view.
-     *
-     * @param   boolean  $cachable   If true, the view output will be cached
-     * @param   array    $urlparams  An array of safe URL parameters and their variable types, for valid values see {@link \JFilterInput::clean()}.
-     *
-     * @return  static  This object to support chaining.
-     *
-     * @since   1.5
-     */
-    public function display($cachable = false, $urlparams = array())
-    {
-        return parent::display();
-    }
+	/**
+	 * Method to display a view.
+	 *
+	 * @param   boolean  $cachable   If true, the view output will be cached
+	 * @param   array    $urlparams  An array of safe URL parameters and their variable types, for valid values see {@link \JFilterInput::clean()}.
+	 *
+	 * @return  static  This object to support chaining.
+	 *
+	 * @throws \Exception
+	 * @since   1.5
+	 */
+	public function display($cachable = false, $urlparams = array())
+	{
+		return parent::display();
+	}
 }
