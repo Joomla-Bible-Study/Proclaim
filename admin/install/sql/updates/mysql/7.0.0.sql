2686d1b5cd4a64894d74af898b6163222bcb07a2
Brent Cordis <skeebrent@gmail.com>
2022-03-09 18:42:17 -0600

    Only allowing upgrade or migration from 9.0.0 and up. So removing old code for migration.

--- admin/install/sql/updates/mysql/7.0.0.sql
+++
@@ -1,9 +0,0 @@
--- This is don in upgrade biblestudy.700.upgrade.php
-CREATE TABLE IF NOT EXISTS `#__bsms_update` (
-  id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  version VARCHAR(255) DEFAULT NULL,
-  PRIMARY KEY (id)
-) DEFAULT CHARSET=utf8;
-
-INSERT INTO `#__bsms_update` (id, version) VALUES (7, '7.0.0')
-ON DUPLICATE KEY UPDATE version= '7.0.0';
--- admin/install/sql/updates/mysql/7.0.3.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `#__bsms_update` (
-  id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  version VARCHAR(255) DEFAULT NULL,
-  PRIMARY KEY (id)
-) DEFAULT CHARSET=utf8;
-
-INSERT INTO `#__bsms_update` (id, version) VALUES (11, '7.0.3')
-ON DUPLICATE KEY UPDATE version= '7.0.3';
