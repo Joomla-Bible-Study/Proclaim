e938f9751bf1b01ae3c0f936d3f67179884ccca5
Tom Fuller <tomfuller2@gmail.com>
2022-10-11 12:54:45 -0700

    Adding some style to make icons work

--- site/tmpl/cwmsermon/default_simple.php
+++ site/tmpl/cwmsermon/default_simple.php
@@ -20,15 +20,15 @@ defined('_JEXEC') or die;
 <dl class="article-info text-muted">
 
     <dd class="createdby" itemprop="author" itemscope="" itemtype="https://schema.org/Person">
-        <span class="icon-user icon-fw" aria-hidden="true"></span>
+        <i class="icon-user icon-fw" aria-hidden="true"></i>
         <?php echo Text::_('JBS_CMN_TEACHER'); ?> <span itemprop="name"><?php echo $this->item->teachername;?></span>	</dd>
 
     <dd class="category-name">
-        <span class="fas fa-bible" aria-hidden="true"></span>
+        <i class="fas fa-bible" aria-hidden="true"></i>
         <?php echo Text::_('JBS_CMN_SCRIPTURE'); ?>: <?php echo $this->item->scripture1;?>	</dd>
 
     <dd class="published">
-        <span class="icon-calendar icon-fw" aria-hidden="true"></span>
+        <i class="icon-calendar icon-fw" aria-hidden="true"></i>
         <time datetime="2022-07-14T12:19:37-07:00" itemprop="datePublished">
             <?php echo Text::_('JBS_CMN_ITEM_PUBLISHED'); ?>: <?php echo $this->item->studydate;?>	</time>
     </dd>
--- site/tmpl/cwmsermons/default_simple.php
+++ site/tmpl/cwmsermons/default_simple.php
@@ -44,18 +44,18 @@ $columns   = 12;
         <h4 itemprop="headline">
             <?php echo $this->item->studytitle; ?>		</h4>
     </div>
-    <dl class="article-info text-muted">
+    <dl class="article-info text-muted" style="display: grid;">
 
         <dd class="createdby" itemprop="author" itemscope="" itemtype="https://schema.org/Person">
-            <span class="icon-user icon-fw" aria-hidden="true"></span>
+            <i class="icon-user icon-fw" aria-hidden="true"></i>
             <?php echo Text::_('JBS_CMN_TEACHER'); ?> <span itemprop="name"><?php echo $this->item->teachername;?></span>	</dd>
 
         <dd class="category-name">
-            <span class="fas fa-bible" aria-hidden="true"></span>
+            <i class="fas fa-bible" aria-hidden="true"></i>
             <?php echo Text::_('JBS_CMN_SCRIPTURE'); ?>: <?php echo $this->item->scripture1;?>	</dd>
 
         <dd class="published">
-            <span class="icon-calendar icon-fw" aria-hidden="true"></span>
+            <i class="icon-calendar icon-fw" aria-hidden="true"></i>
             <time datetime="2022-07-14T12:19:37-07:00" itemprop="datePublished">
                 <?php echo Text::_('JBS_CMN_ITEM_PUBLISHED'); ?>: <?php echo $this->item->studydate;?>	</time>
         </dd>
