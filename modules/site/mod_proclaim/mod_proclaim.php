af5973aece5ce6b38ae08cfb0dc0eba3790af1b1
Brent Cordis <skeebrent@gmail.com>
2022-12-30 11:38:43 -0800

    Merge from Development

--- modules/site/mod_proclaim/mod_proclaim.php
+++ modules/site/mod_proclaim/mod_proclaim.php
@@ -11,14 +11,15 @@
  * */
 defined('_JEXEC') or die;
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Router\Route;
-use Joomla\Registry\Registry;
-use Joomla\CMS\Helper\ModuleHelper;
 use CWM\Component\Proclaim\Administrator\Helper\CWMParams;
 use CWM\Component\Proclaim\Site\Helper\CWMPagebuilder;
 use CWM\Module\Proclaim\Site\Helper\ModProclaimHelper;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Helper\ModuleHelper;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Router\Route;
+use Joomla\Registry\Registry;
+
 // Always load JBSM API if it exists.
 $api = JPATH_ADMINISTRATOR . '/components/com_proclaim/api.php';
 
@@ -32,12 +33,28 @@ else
 }
 
 // Need for inline player
-$document = Factory::getApplication()->getDocument();
+try
+{
+	$document = Factory::getApplication()->getDocument();
+}
+catch (Exception $e)
+{
+	Factory::getApplication()->enqueueMessage($e, 'error');
+}
 
 /** @var $params Registry */
 $templatemenuid = $params->get('t');
-$template       = CWMParams::getTemplateparams($templatemenuid);
-$pagebuilder    = new CWMPageBuilder;
+
+try
+{
+	$template = CWMParams::getTemplateparams($templatemenuid);
+}
+catch (\Exception $e)
+{
+	Factory::getApplication()->enqueueMessage($e, 'error');
+}
+
+$pagebuilder = new CWMPageBuilder;
 
 $admin        = CWMParams::getAdmin();
 /** @var Registry $admin_params */
@@ -59,7 +76,16 @@ if ($params->get('useexpert_module') > 0 || is_string($params->get('moduletempla
 	{
 		$item->slug       = $item->alias ? ($item->id . ':' . $item->alias) : $item->id . ':'
 			. str_replace(' ', '-', htmlspecialchars_decode($item->studytitle, ENT_QUOTES));
-		$pelements        = $pagebuilder->buildPage($item, $params, $template);
+
+		try
+		{
+			$pelements = $pagebuilder->buildPage($item, $params, $template);
+		}
+		catch (Exception $e)
+		{
+			Factory::getApplication()->enqueueMessage($e, 'error');
+		}
+
 		$item->scripture1 = $pelements->scripture1;
 		$item->scripture2 = $pelements->scripture2;
 		$item->media      = $pelements->media;
@@ -110,28 +136,28 @@ $list      = $items;
 $link_text = $params->get('pagetext');
 
 $input = Factory::getApplication()->input;
+
 if (!$templatemenuid)
 {
 	$templatemenuid = $input->getInt('templatemenuid', 1);
 }
 
-$linkurl  = Route::_('index.php?option=com_proclaim&view=cwmsermons&t=' . $templatemenuid);
-$link     = '<a href="' . $linkurl . '"><button class="btn">' . $link_text . '</button></a>';
-$document = Factory::getApplication()->getDocument();
+$linkurl = Route::_('index.php?option=com_proclaim&view=cwmsermons&t=' . $templatemenuid);
+$link    = '<a href="' . $linkurl . '"><button class="btn">' . $link_text . '</button></a>';
+
 HtmlHelper::_('proclaim.framework');
 
 $config = JComponentHelper::getParams('com_proclaim');
 
 // We need to load the path to the helper files
-//$path1 = JPATH_BASE . '/components/com_proclaim/helpers/';
-$url   = $params->get('stylesheet');
+// $path1 = JPATH_BASE . '/components/com_proclaim/helpers/';
+$url = $params->get('stylesheet');
 
 if ($url)
 {
 	$document->addStyleSheet($url);
 }
 
-//HtmlHelper::_('biblestudy.loadCss', $params, null, 'font-awesome');
 $pageclass_sfx = $params->get('pageclass_sfx');
 
 if ($params->get('simple_mode') === '1')
