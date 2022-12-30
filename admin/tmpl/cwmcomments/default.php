1fd4009a47d686280921e57a7e39fb16c2a41126
Brent Cordis <skeebrent@gmail.com>
2022-12-30 14:06:51 -0800

    Updated Comments Code view.

--- admin/tmpl/cwmcomments/default.php
+++ admin/tmpl/cwmcomments/default.php
@@ -14,6 +14,7 @@ use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Layout\LayoutHelper;
+use Joomla\CMS\Language\Multilanguage;
 use Joomla\CMS\Router\Route;
 
 $app        = Factory::getApplication();
@@ -52,63 +53,63 @@ $wa->useScript('keepalive')
 ?>
 <form action="<?php echo Route::_('index.php?option=com_proclaim&view=cwmcomments'); ?>" method="post" name="adminForm"
       id="adminForm">
-	<?php if (!empty($this->sidebar)): ?>
-	<div id="j-sidebar-container" class="span2">
-		<?php echo $this->sidebar; ?>
-		<hr/>
-	</div>
-	<div id="j-main-container" class="span10">
-		<?php else : ?>
-		<div id="j-main-container">
-			<?php endif; ?>
-			<?php
-			// Search tools bar
-			echo LayoutHelper::render('joomla.searchtools.default', array('view' => $this));
-			?>
-			<?php if (empty($this->items)) : ?>
-				<div class="alert alert-no-items">
-					<?php echo Text::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
-				</div>
-			<?php else : ?>
-				<table class="table table-striped adminlist" id="comments">
-					<thead>
-					<tr>
-						<th width="1%" class="hidden-phone">
-							<?php echo HTMLHelper::_('grid.checkall'); ?>
-						</th>
-						<th width="1%" style="min-width:55px;" class="nowrap center">
-							<?php echo HTMLHelper::_('grid.sort', 'JBS_CMN_PUBLISHED', 'comment.published', $listDirn, $listOrder); ?>
-						</th>
-						<th>
-							<?php echo HTMLHelper::_('grid.sort', 'JBS_CMN_TITLE', 'study.studytitle', $listDirn, $listOrder); ?>
-						</th>
-						<th width="10%" class="nowrap hidden-phone">
-							<?php echo HTMLHelper::_('grid.sort', 'JGRID_HEADING_ACCESS', 'comment.access', $listDirn, $listOrder); ?>
-						</th>
-						<th class="nowrap hidden-phone">
-							<?php echo HTMLHelper::_('grid.sort', 'JBS_CMT_FULL_NAME', 'comment.full_name', $listDirn, $listOrder); ?>
-						</th>
-						<th class="nowrap hidden-phone">
-							<?php echo Text::_('JBS_CMT_TEXT'); ?>
-						</th>
-						<th class="nowrap hidden-phone">
-							<?php echo HTMLHelper::_('grid.sort', 'JBS_CMT_CREATE_DATE', 'comment.studydate', $listDirn, $listOrder); ?>
-						</th>
-						<th width="5%" class="nowrap hidden-phone">
-							<?php echo HTMLHelper::_('grid.sort', 'JGRID_HEADING_LANGUAGE', 'language', $listDirn, $listOrder); ?>
-						</th>
-						<th width="1%" class="nowrap hidden-phone">
-							<?php echo HTMLHelper::_('grid.sort', 'JGRID_HEADING_ID', 'comment.id', $listDirn, $listOrder); ?>
-						</th>
-					</tr>
-					</thead>
-					<tfoot>
-					<tr>
-						<td colspan="<?php echo $columns; ?>">
-						</td>
-					</tr>
+	<div class="row">
+		<div class="col-md-12">
+			<div id="j-main-container" class="j-main-container">
+				<?php echo LayoutHelper::render('joomla.searchtools.default', array('view' => $this)); ?>
+				<?php if (empty($this->items)) : ?>
+					<div class="alert alert-info">
+						<span class="icon-info-circle" aria-hidden="true"></span><span
+								class="visually-hidden"><?php echo Text::_('INFO'); ?></span>
+						<?php echo Text::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
+					</div>
+				<?php else : ?>
+					<table class="table table-striped itemlist" id="comments">
+						<caption class="visually-hidden">
+							<?php echo Text::_('JBS_STY_TABLE_CAPTION'); ?>,
+							<span id="orderedBy"><?php echo Text::_('JGLOBAL_SORTED_BY'); ?> </span>,
+							<span id="filteredBy"><?php echo Text::_('JGLOBAL_FILTERED_BY'); ?></span>
+						</caption>
+						<thead>
+						<tr>
+							<th class="w-1 text-center">
+								<?php echo HTMLHelper::_('grid.checkall'); ?>
+							</th>
+							<th scope="col" class="w-1 text-center">
+								<?php echo HTMLHelper::_('grid.sort', 'JBS_CMN_PUBLISHED', 'comment.published', $listDirn, $listOrder); ?>
+							</th>
+							<th scope="col" style="min-width:100px">
+								<?php echo HTMLHelper::_('grid.sort', 'JBS_CMN_TITLE', 'study.studytitle', $listDirn, $listOrder); ?>
+							</th>
+							<th scope="col" class="w-1 text-center">
+								<?php echo HTMLHelper::_('grid.sort', 'JGRID_HEADING_ACCESS', 'comment.access', $listDirn, $listOrder); ?>
+							</th>
+							<th scope="col" class="w-1 text-center">
+								<?php echo HTMLHelper::_('grid.sort', 'JBS_CMT_FULL_NAME', 'comment.full_name', $listDirn, $listOrder); ?>
+							</th>
+							<th scope="col" class="w-1 text-center">
+								<?php echo Text::_('JBS_CMT_TEXT'); ?>
+							</th>
+							<th scope="col" class="w-1 text-center">
+								<?php echo HTMLHelper::_('grid.sort', 'JBS_CMT_CREATE_DATE', 'comment.studydate', $listDirn, $listOrder); ?>
+							</th>
+							<?php if (Multilanguage::isEnabled()) : ?>
+								<th scope="col" class="w-10 d-none d-md-table-cell">
+									<?php echo HTMLHelper::_('searchtools.sort', 'JGRID_HEADING_LANGUAGE', 'language', $listDirn, $listOrder); ?>
+								</th>
+							<?php endif; ?>
+							<th scope="col" class="w-3 d-none d-lg-table-cell">
+								<?php echo HTMLHelper::_('grid.sort', 'JGRID_HEADING_ID', 'comment.id', $listDirn, $listOrder); ?>
+							</th>
+						</tr>
+						</thead>
+						<tfoot>
+						<tr>
+							<td colspan="<?php echo $columns; ?>">
+							</td>
+						</tr>
 					</tfoot>
-					<tbody>
+					<tbody<?php if ($saveOrder) : ?> class="js-draggable" data-url="<?php echo $saveOrderingUrl; ?>" data-direction="<?php echo strtolower($listDirn); ?>" data-nested="true"<?php endif; ?>>
 					<?php
 					foreach ($this->items as $i => $item) :
 						$link = Route::_('index.php?option=com_proclaim&task=cwmcomment.edit&id=' . (int) $item->id);
@@ -118,27 +119,14 @@ $wa->useScript('keepalive')
 						$canChange = $user->authorise('core.edit.state', 'com_proclaim.comment.' . $item->id);
 						?>
 						<tr class="row<?php echo $i % 2; ?>" data-draggable-group="<?php echo '1' ?>">
-
-							<td class="center hidden-phone">
+							<td class="text-center">
 								<?php echo HTMLHelper::_('grid.id', $i, $item->id); ?>
 							</td>
-							<td class="center">
+							<td class="text-center d-none d-md-table-cell">
 								<div class="btn-group">
-									<?php echo HTMLHelper::_('jgrid.published', $item->published, $i, 'cwmcomments.', $canChange, 'cb', '', ''); ?><?php
-
-									// Create dropdown items
-									$action = $archived ? 'unarchive' : 'archive';
-									HTMLHelper::_('actionsdropdown.' . $action, 'cb' . $i, 'cwmcomments');
-
-									$action = $trashed ? 'untrash' : 'trash';
-									HTMLHelper::_('actionsdropdown.' . $action, 'cb' . $i, 'cwmcomments');
-
-									// Render dropdown list
-									echo HTMLHelper::_('actionsdropdown.render', $this->escape($item->studytitle));
-									?>
+									<?php echo HTMLHelper::_('jgrid.published', $item->published, $i, 'cwmcomments.', $canChange, 'cb', '', ''); ?>
 								</div>
 							</td>
-
 							<td class="nowrap has-context" style="width:10%;">
 								<div class="pull-left">
 									<?php if ($canEdit || $canEditOwn) : ?>
@@ -149,54 +137,55 @@ $wa->useScript('keepalive')
 									<?php endif; ?>
 								</div>
 							</td>
-							<td class="small hidden-phone">
+							<td class="small d-none d-md-table-cell">
 								<?php echo $this->escape($item->access_level); ?>
 							</td>
-							<td class="nowrap has-context">
+							<td class="small d-none d-md-table-cell">
 								<?php echo $item->full_name; ?>
 							</td>
-							<td>
+							<td class="small d-none d-md-table-cell">
 								<?php echo substr($item->comment_text, 0, 50); ?>
 							</td>
-							<td class="nowrap has-context">
+							<td class="small d-none d-md-table-cell">
 								<?php echo $item->comment_date; ?>
 							</td>
-							<td class="small hidden-phone">
-								<?php if ($item->language == '*'): ?>
-									<?php echo Text::alt('JALL', 'language'); ?>
-								<?php else: ?>
-									<?php echo $item->language_title ? $this->escape($item->language_title) : Text::_('JUNDEFINED'); ?>
-								<?php endif; ?>
-							</td>
-							<td class="center hidden-phone">
+							<?php if (Multilanguage::isEnabled()) : ?>
+								<td class="small d-none d-md-table-cell">
+									<?php echo LayoutHelper::render('joomla.content.language', $item); ?>
+								</td>
+							<?php endif; ?>
+							<td class="d-none d-lg-table-cell">
 								<?php echo (int) $item->id; ?>
 							</td>
 						</tr>
 					<?php endforeach; ?>
 					</tbody>
-				</table>
-				<?php // Load the batch processing form. ?>
-				<?php if ($user->authorise('core.create', 'com_proclaim')
-					&& $user->authorise('core.edit', 'com_proclaim')
-					&& $user->authorise('core.edit.state', 'com_proclaim')
-				) : ?>
-					<?php echo HTMLHelper::_(
-						'bootstrap.renderModal',
-						'collapseModal',
-						array(
-							'title'  => Text::_('COM_CONTENT_BATCH_OPTIONS'),
-							'footer' => $this->loadTemplate('batch_footer'),
-						),
-						$this->loadTemplate('batch_body')
-					); ?>
+					</table>
+
+					<?php // load the pagination. ?>
+					<?php echo $this->pagination->getListFooter(); ?>
+
+					<?php // Load the batch processing form. ?>
+					<?php if ($user->authorise('core.create', 'com_proclaim')
+						&& $user->authorise('core.edit', 'com_proclaim')
+						&& $user->authorise('core.edit.state', 'com_proclaim')
+					) : ?>
+						<?php echo HTMLHelper::_(
+							'bootstrap.renderModal',
+							'collapseModal',
+							array(
+								'title'  => Text::_('COM_CONTENT_BATCH_OPTIONS'),
+								'footer' => $this->loadTemplate('batch_footer'),
+							),
+							$this->loadTemplate('batch_body')
+						); ?>
+					<?php endif; ?>
 				<?php endif; ?>
-			<?php endif; ?>
-			<?php echo $this->pagination->getListFooter(); ?>
-			<?php // Load the batch processing form. ?>
-			<input type="hidden" name="task" value=""/>
-			<input type="hidden" name="boxchecked" value="0"/>
-			<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>"/>
-			<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>"/>
-			<?php echo HTMLHelper::_('form.token'); ?>
+				<?php // Load the batch processing form. ?>
+				<input type="hidden" name="task" value=""/>
+				<input type="hidden" name="boxchecked" value="0"/>
+				<?php echo HTMLHelper::_('form.token'); ?>
+			</div>
 		</div>
+	</div>
 </form>
