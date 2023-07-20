ab4971e8428bce8741bc488c68c414f564752bb3
Brent Cordis <skeebrent@gmail.com>
2023-07-20 18:30:59 -0500

    Fix problem with Assets fix and controller.

--- admin/src/Controller/CWMAssetsController.php
+++ admin/src/Controller/CWMAssetsController.php
@@ -21,7 +21,6 @@ use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\Controller\BaseController;
 use Joomla\CMS\MVC\Model\BaseDatabaseModel;
-use Joomla\CMS\Router\Route;
 use Joomla\CMS\Session\Session;
 
 /**
@@ -47,7 +46,7 @@ class CWMAssetsController extends BaseController
 	 * @var    string
 	 * @since  3.0
 	 */
-	protected $default_view = 'cwmassets';
+	protected $default_view = 'CWMAssets';
 
 	/**
 	 * Constructor.
@@ -59,7 +58,7 @@ class CWMAssetsController extends BaseController
 	 * @throws \Exception
 	 * @since 1.5
 	 */
-	public function execute($task)
+	public function execute($task): void
 	{
 		if ($task !== 'run' && $task !== 'checkassets' && $task !== 'clear')
 		{
@@ -87,6 +86,7 @@ class CWMAssetsController extends BaseController
 		$session = Factory::getApplication()->getSession();
 		$session->set('assat_stack', '', 'CWM');
 		$session->set('checkassets', $checkassets, 'CWM');
+		$this->input->set('view', 'CWMAssets');
 
 		$this->display(false);
 	}
@@ -99,7 +99,7 @@ class CWMAssetsController extends BaseController
 	 * @throws \Exception
 	 * @since 8.0.0
 	 */
-	public function browse()
+	public function browse(): void
 	{
 		// Check for request forgeries.
 //		Session::checkToken('get') or jexit(Text::_('JINVALID_TOKEN'));
@@ -117,7 +117,7 @@ class CWMAssetsController extends BaseController
 			$model = new CWMAssetsModel;
 			$state = $model->startScanning();
 			$app->input->set('scanstate', $state);
-			$app->input->set('view', 'cwmassets');
+			$app->input->set('view', 'CWMAssets');
 
 			$this->display(false);
 		}
@@ -135,7 +135,7 @@ class CWMAssetsController extends BaseController
 	 * @throws \Exception
 	 * @since 9.0.2
 	 */
-	public function clear()
+	public function clear(): void
 	{
 		// Check for request forgeries.
 		Session::checkToken('get') or jexit(Text::_('JINVALID_TOKEN'));
@@ -145,7 +145,7 @@ class CWMAssetsController extends BaseController
 		$session = Factory::getSession();
 		$session->set('assat_stack', '', 'CWM');
 		$app = Factory::getApplication();
-		$app->input->set('view', 'cwmassets');
+		$app->input->set('view', 'CWMAssets');
 		$this->display(false);
 	}
 
@@ -157,7 +157,7 @@ class CWMAssetsController extends BaseController
 	 * @throws \Exception
 	 * @since 8.0.0
 	 */
-	public function run()
+	public function run(): void
 	{
 		// Check for request forgeries.
 		Session::checkToken('get') || Session::checkToken() or jexit(Text::_('JINVALID_TOKEN'));
@@ -166,7 +166,7 @@ class CWMAssetsController extends BaseController
 		$model = new CWMAssetsModel;
 		$state = $model->run();
 		$app->input->set('scanstate', $state);
-		$app->input->set('view', 'cwmassets');
+		$app->input->set('view', 'CWMAssets');
 
 		$this->display(false);
 	}
@@ -182,7 +182,7 @@ class CWMAssetsController extends BaseController
 	 *
 	 * @since   1.6
 	 */
-	public function getModel($name = 'CWMAssets', $prefix = '', $config = array('ignore_request' => true))
+	public function getModel($name = 'CWMAssets', $prefix = '', $config = array('ignore_request' => true)): BaseDatabaseModel
 	{
 		return parent::getModel($name, $prefix, $config);
 	}
