b37aeeeaea8167453d0f523c665adc6a47f6e3e8
Tom Fuller <tomfuller2@gmail.com>
2024-07-31 14:15:52 -0700

    Added procedure to add subtitle field is not exists. Not able to build so can't test.

--- admin/sql/updates/mysql/10.0.0-20220921.sql
+++ admin/sql/updates/mysql/10.0.0-20220921.sql
@@ -6,3 +6,13 @@ alter table `#__bsms_podcast`
     modify image VARCHAR(255) null;
 alter table `#__bsms_series`
     modify series_thumbnail VARCHAR(255) null;
+DROP PROCEDURE IF EXISTS `addit`;
+DELIMITER //
+CREATE PROCEDURE `addit`()
+BEGIN
+    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION BEGIN END;
+    ALTER TABLE `#__bsms_series` ADD COLUMN `subtitle` TEXT;
+END //
+DELIMITER ;
+CALL `addit`();
+DROP PROCEDURE `addit`;
