f27878adc198ccb10744e0b3c187e5b14016bab4
Brent Cordis <994259+bcordis@users.noreply.github.com>
2025-08-26 22:24:23 -0500

    Update the PHP scripts and add the podcastlink to the SQL file.

--- admin/sql/updates/mysql/10.0.0-20220921.sql
+++ admin/sql/updates/mysql/10.0.0-20220921.sql
@@ -3,10 +3,12 @@ VALUES ('42', '10.0.0-20220921')
 ON DUPLICATE KEY UPDATE version = '10.0.0-20220921';
 
 alter table `#__bsms_podcast`
-    modify podcast_image_subscribe varchar(255) null;
+    modify podcast_image_subscribe varchar(255) DEFAULT NULL;
 alter table `#__bsms_podcast`
-    modify podcastimage varchar(255) null;
+    modify podcastimage varchar(255) DEFAULT NULL;
 alter table `#__bsms_podcast`
-    modify image VARCHAR(255) null;
+    modify image VARCHAR(255) DEFAULT NULL;
 alter table `#__bsms_series`
-    modify series_thumbnail VARCHAR(255) null;
+    modify series_thumbnail VARCHAR(255) DEFAULT NULL;
+alter table `#__bsms_podcast`
+    add column `podcastlink` VARCHAR(100) DEFAULT NULL AFTER `website`;
