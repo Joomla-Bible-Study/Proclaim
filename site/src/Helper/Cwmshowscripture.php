759fc29628872e3a3c1ef32100ef9cfa0cb5d570
Brent Cordis <994259+bcordis@users.noreply.github.com>
2025-12-24 18:40:50 -0800

    Remove outdated code with optimization of how sql and the functions work.

--- site/src/Helper/Cwmshowscripture.php
+++ site/src/Helper/Cwmshowscripture.php
@@ -43,25 +43,20 @@ class Cwmshowscripture
      *
      * @since    7.1
      */
-    public function buildPassage($row, Registry $params)
+    public function buildPassage($row, Registry $params): string|bool
     {
-        if (!$row->bookname) {
+        if (empty($row->bookname)) {
             return false;
         }
 
         $reference  = $this->formReference($row);
         $version    = $params->get('bible_version', '77');
         $this->link = $this->getBiblegateway($reference, $version, $params);
-        $choice     = $params->get('show_passage_view');
-        $passage    = null;
+        $choice     = (int) $params->get('show_passage_view');
+        $passage    = '';
         $css        = false;
 
         switch ($choice) {
-            case 0:
-                $passage = '';
-
-                break;
-
             case 1:
                 $passage = $this->getHideShow();
                 $css     = true;
@@ -74,12 +69,11 @@ class Cwmshowscripture
 
             case 3:
                 $passage = $this->getLink($params);
-
                 break;
         }
 
         if ($css) {
-            HtmlHelper::_('stylesheet', Uri::base() . 'media/com_proclaim/css/biblegateway-print.css');
+            HtmlHelper::_('stylesheet', 'media/com_proclaim/css/biblegateway-print.css', ['relative' => true]);
         }
 
         return $passage;
@@ -96,20 +90,16 @@ class Cwmshowscripture
      */
     public function formReference($row): string
     {
-        $book      = Text::_($row->bookname);
-        $book      = str_replace(' ', '+', $book);
-        $book .= '+';
-        $reference = $book . $row->chapter_begin;
+        $book = str_replace(' ', '+', Text::_($row->bookname));
+        $reference = $book . '+' . $row->chapter_begin;
 
-        if ($row->verse_begin) {
+        if (!empty($row->verse_begin)) {
             $reference .= ':' . $row->verse_begin;
         }
 
-        if ($row->chapter_end && $row->verse_end) {
+        if (!empty($row->chapter_end) && !empty($row->verse_end)) {
             $reference .= '-' . $row->chapter_end . ':' . $row->verse_end;
-        }
-
-        if ($row->verse_end && !$row->chapter_end) {
+        } elseif (!empty($row->verse_end)) {
             $reference .= '-' . $row->verse_end;
         }
 
@@ -129,7 +119,7 @@ class Cwmshowscripture
      */
     public function getBiblegateway($reference, $version, $params): string
     {
-        return "https://classic.biblegateway.com/passage/index.php?search=" . $reference . ";&version=" . $version . ";&interface=print";
+        return "https://www.biblegateway.com/passage/?search=" . $reference . "&version=" . $version . "&interface=print";
     }
 
     /**
@@ -141,13 +131,12 @@ class Cwmshowscripture
      */
     public function getHideShow(): string
     {
-        $contents = '<iframe id="scripture" src="' . $this->link . '" width="100%" height="400px;"></iframe>';
-        $passage  = '<div class = "fluid-row"><div class="col-12"></div>';
-        $passage .= '<a class="heading" href="javascript:ReverseDisplay(\'scripture\')">>>' . Text::_(
-            'JBS_CMN_SHOW_HIDE_SCRIPTURE'
-        ) . '<<</a>';
-        $passage .= '<div id="scripture" style="display: none;">';
-        $passage .= $contents;
+        $id = 'scripture_' . uniqid('', true);
+        $passage = '<div class="fluid-row"><div class="col-12"></div>';
+        $passage .= '<a class="heading" href="#" onclick="var e = document.getElementById(\'' . $id . '\'); e.style.display = (e.style.display == \'none\' ? \'block\' : \'none\'); return false;">' 
+            . Text::_('JBS_CMN_SHOW_HIDE_SCRIPTURE') . '</a>';
+        $passage .= '<div id="' . $id . '" style="display: none;">';
+        $passage .= '<iframe src="' . $this->link . '" width="100%" height="400" style="border:0;"></iframe>';
         $passage .= '</div>';
         $passage .= '</div>';
 
@@ -163,9 +152,7 @@ class Cwmshowscripture
      */
     public function getShow(): string
     {
-        $contents = '<iframe id = "scripture" src = "' . $this->link . '" width = "100%" height = "400px;" ></iframe >';
-
-        return '<div class = "passage">' . $contents . '</div>';
+        return '<div class="passage"><iframe src="' . $this->link . '" width="100%" height="400" style="border:0;"></iframe></div>';
     }
 
     /**
@@ -179,23 +166,16 @@ class Cwmshowscripture
      */
     public function getLink(Registry $params): string
     {
-        $passage = '<div class = passage>';
-
-        // $passage .= '<a href="#" onclick="';
+        $passage = '<div class="passage">';
         $passage .= '<a href="' . $this->link . '" ';
         $passage .= "onclick=\"window.open(this.href,'mywindow','toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,";
-        $passage .= "resizable=yes,width=800,height=500');";
-        $passage .= "return false;";
+        $passage .= "resizable=yes,width=800,height=500'); return false;\"";
+        $passage .= ' title="' . Text::_('JBS_STY_CLICK_TO_OPEN_PASSAGE') . '">';
 
-        // $rel = "{handler: 'iframe', size: {x: 800, y: 500}}";
-        $passage .= '" title="' . Text::_('JBS_STY_CLICK_TO_OPEN_PASSAGE') . '">'; ?>
-        <?php
-        if ($params->get('showpassage_icon') > 0) {
-            if ((int)$params->get('showpassage_icon') === 1) {
-                $passage .= '<i class="fas fa-bible fa-3x" style="display: flex; margin-right: 10px;"></i>';
-            } else {
-                $passage .= Text::_('JBS_STY_CLICK_TO_OPEN_PASSAGE');
-            }
+        if ((int) $params->get('showpassage_icon') === 1) {
+            $passage .= '<i class="fas fa-bible fa-3x" style="display: flex; margin-right: 10px;"></i>';
+        } elseif ($params->get('showpassage_icon') > 0) {
+            $passage .= Text::_('JBS_STY_CLICK_TO_OPEN_PASSAGE');
         }
 
         $passage .= '</a></div>';
@@ -214,6 +194,6 @@ class Cwmshowscripture
      */
     public function bodyOnly(string $html): string
     {
-        return '<iframe id = "scripture" src = "' . $this->link . '" width = "100%" height = "400px;" ></iframe >';
+        return '<div class="passage"><iframe src="' . $this->link . '" width="100%" height="400" style="border:0;"></iframe></div>';
     }
 }
