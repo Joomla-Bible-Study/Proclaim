02fcf1c01c61f23cc20193e7fba2efef04f21d27
Brent Cordis <994259+bcordis@users.noreply.github.com>
2026-01-10 10:12:37 -0600

    Update help links

--- admin/src/View/Cwmmediafiles/HtmlView.php
+++ admin/src/View/Cwmmediafiles/HtmlView.php
@@ -230,8 +230,7 @@ class HtmlView extends BaseHtmlView
                 ->message('JGLOBAL_CONFIRM_DELETE')
                 ->listCheck(true);
         }
-        $help_url = 'https://www.christianwebministries.org/index.php?option=com_content&view=article&id=28:admin-messages-list-help-screen&catid=20&Itemid=315&tmpl=component';
-        $toolbar->help('proclaim', false, $url = $help_url, 'com_proclaim');
+        $toolbar->help('mediafiles', true);
     }
 
     /**
